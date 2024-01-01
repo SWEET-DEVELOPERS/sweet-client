b81e3e42e8937ff5ff114dffd779daf49991634d
장정안 <wjddks96@naver.com>
2024-01-01 15:11:23 +0900

    fix: reset 라이브러리 설치로 GlobalStyle 수정

--- src/style/GlobalStyle.ts
+++ src/style/GlobalStyle.ts
@@ -1,43 +1,31 @@
-import { createGlobalStyle, css } from "styled-components";
-
-export const reset = css`
-  * {
-    margin: 0;
-    padding: 0;
-    font-size: 62.5%;
-  }
-
-  *,
-  :after,
-  :before {
-    box-sizing: border-box;
-  }
-
-  html,
-  body {
-    height: 100%;
-  }
-
-  :root {
-    --vh: 100%;
-  }
-
-  button {
-    cursor: pointer;
-  }
-
-  a {
-    text-decoration: none;
-  }
-`;
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
 
 export const GlobalStyle = createGlobalStyle`
 
 ${reset}
+
+ a{
+  text-decoration: none;
+ }
+
+ * {
+  box-sizing: border-box;
+ }
+
+ button {
+  cursor: pointer;
+ }
+
+ :root {
+  --vh: 100%
+ }
 #root, body, html {
     scrollbar-width: none; /* 파이어폭스 스크롤바 숨김 */
+    
     max-width: 43rem;
     margin: 0 auto;
+    font-size: 62.5%;
     -ms-overflow-style: none; /* 인터넷 익스플로러  스크롤바 숨김 */
     scrollbar-width: none; /* 파이어폭스 스크롤바 숨김 */
 }
