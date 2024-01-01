e844528f3f55b92835906347c3c7027a4e0a4516
장정안 <wjddks96@naver.com>
2024-01-01 17:03:40 +0900

    feat: vw관련 설정 추가

--- utils/setScreenSize.ts
+++ utils/setScreenSize.ts
@@ -2,4 +2,14 @@ export const setScreenSize = () => {
   const vh = window.innerHeight * 0.01;
 
   document.documentElement.style.setProperty("--vh", `${vh}px`);
+
+  const windowWidth =
+    window.innerWidth ||
+    document.documentElement.clientWidth ||
+    document.body.clientWidth;
+  const maxWidth = Math.min(375, windowWidth);
+  document.documentElement.style.setProperty(
+    "--app-max-width",
+    `${maxWidth}px`
+  );
 };
