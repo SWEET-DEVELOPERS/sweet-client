3fc8ec3dceaa494cba0b1f88c9aef7c6797875fa
Wang HoEun <hoeunwang@gmail.com>
2024-01-02 18:32:07 +0900

    chore: vite svgr 세팅 및 custom 파일 생성

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
-import svgr from "vite-plugin-svgr";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import svgr from 'vite-plugin-svgr';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [
+    react(),
+    svgr({
+      svgrOptions: {
+        icon: true,
+      },
+    }),
+  ],
 });
