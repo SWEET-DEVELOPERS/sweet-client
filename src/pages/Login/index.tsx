9534e9233ed3d1c15ed7fb218c78e2139d809fee
Wang HoEun <hoeunwang@gmail.com>
2024-01-03 18:17:39 +0900

    feat: Login 페이지 생성

---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as S from './Login.style';
+
+const Login = () => {
+  return (
+    <S.Container>
+      <p> 로그인 </p>
+    </S.Container>
+  );
+};
+
+export default Login;
