d3d9b5998cad7d1cae6bce1f4f697e0a2f0832ea
imeureka <lky062@naver.com>
2024-01-03 19:39:46 +0900

    feat: useEffect 제거 후 스타일 고정

--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -1,27 +1,9 @@
-import { useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 import styled from 'styled-components';
-import { setScreenSize } from '../../../utils/setScreenSize';
 import Footer from './Footer';
 import Header from './Header';
 
 export default function Layout() {
-  useEffect(() => {
-    setScreenSize(); // 화면 크기 조절 함수 호출
-  }, []);
-
-  useEffect(() => {
-    const onResize = () => {
-      setScreenSize(); // 화면 크기가 변경될 때마다 함수 호출
-    };
-
-    window.addEventListener('resize', onResize);
-
-    return () => {
-      window.removeEventListener('resize', onResize);
-    };
-  }, []);
-
   return (
     <LayoutWrapper>
       <Header />
@@ -35,4 +17,6 @@ const LayoutWrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+
+  width: Math.min(375, windowWidth);
 `;
