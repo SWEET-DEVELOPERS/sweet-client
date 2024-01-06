459600e7ee775fd6bda57e2a06f81293dfb7fabf
urjimyu <92876819+urjimyu@users.noreply.github.com>
2024-01-06 21:49:35 +0900

    feat: 임시로 선물 등록 테스트 페이지 추가

--- src/components/common/Router.tsx
+++ src/components/common/Router.tsx
@@ -1,20 +1,25 @@
-import { createBrowserRouter } from "react-router-dom";
-import Main from "../../pages/Main";
-import Mypage from "../../pages/Mypage";
-import DashBoard from "./DashBoard";
+import { createBrowserRouter } from 'react-router-dom';
+import Main from '../../pages/Main';
+import Mypage from '../../pages/Mypage';
+import DashBoard from './DashBoard';
+import AddGift from '../../pages/AddGift';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <DashBoard />,
     children: [
       {
-        path: "/",
+        path: '/',
         element: <Main />,
       },
       {
-        path: "/mypage",
+        path: '/mypage',
         element: <Mypage />,
+      },
+      {
+        path: '/addgift',
+        element: <AddGift />,
       },
     ],
   },
