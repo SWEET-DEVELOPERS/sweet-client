ddb041d73828fa73f8c3b261ec9caa2e14d1c910
장정안 <wjddks96@naver.com>
2024-01-07 00:06:14 +0900

    fix: import 이슈 해결을 위한 중괄호 제거

--- src/assets/fonts/index.ts
+++ src/assets/fonts/index.ts
@@ -1,13 +1,13 @@
 import { css } from 'styled-components';
-import { SUIT_Bold } from '../../assets/fonts/SUIT-Bold.ttf';
-import { SUIT_ExtraBold } from '../../assets/fonts/SUIT-ExtraBold.ttf';
-import { SUIT_ExtraLight } from '../../assets/fonts/SUIT-ExtraLight.ttf';
-import { SUIT_Heavy } from '../../assets/fonts/SUIT-Heavy.ttf';
-import { SUIT_Light } from '../../assets/fonts/SUIT-Light.ttf';
-import { SUIT_Medium } from '../../assets/fonts/SUIT-Medium.ttf';
-import { SUIT_Regular } from '../../assets/fonts/SUIT-Regular.ttf';
-import { SUIT_Thin } from '../../assets/fonts/SUIT-Thin.ttf';
-import { SUIT_SemiBold } from '../../assets/fonts/SUIT-SemiBold.ttf';
+import SUIT_Bold from '../../assets/fonts/SUIT-Bold.ttf';
+import SUIT_ExtraBold from '../../assets/fonts/SUIT-ExtraBold.ttf';
+import SUIT_ExtraLight from '../../assets/fonts/SUIT-ExtraLight.ttf';
+import SUIT_Heavy from '../../assets/fonts/SUIT-Heavy.ttf';
+import SUIT_Light from '../../assets/fonts/SUIT-Light.ttf';
+import SUIT_Medium from '../../assets/fonts/SUIT-Medium.ttf';
+import SUIT_Regular from '../../assets/fonts/SUIT-Regular.ttf';
+import SUIT_Thin from '../../assets/fonts/SUIT-Thin.ttf';
+import SUIT_SemiBold from '../../assets/fonts/SUIT-SemiBold.ttf';
 
 export default css`
   @font-face {
