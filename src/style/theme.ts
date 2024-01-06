2aa7129ff678009ad4d432cb99aa2a42b4138286
장정안 <wjddks96@naver.com>
2024-01-06 14:02:07 +0900

    feat: theme color 설정

--- src/style/theme.ts
+++ src/style/theme.ts
@@ -1,5 +1,34 @@
 import mixin from './mixin';
 
-const theme = { mixin };
+const colors = {
+  white: '#FFFFFF',
+  black: '#222222',
+  gray_01: '#F8F8F8',
+  gray_02: '#EBE9EA',
+  gray_03: '#E1DEDF',
+  gray_04: '#D3CFD1',
+  gray_05: '#CAC6C8',
+  gray_06: '#BDB8BA',
+  gray_07: '#ACA7A9',
+  gray_08: '#868384',
+  gray_09: '#686566',
+  gray_10: '#4F4D4E',
+  pink_01: '#FFE9F1',
+  pink_02: '#FFBAD5',
+  pink_03: '#FF99C0',
+  pink_04: '#FF6AA3',
+  pink_05: '#FF4D91',
+  pink_06: '#FF2176',
+  pink_07: '#E81E6B',
+  pink_08: '#B51754',
+  pink_09: '#8C1241',
+  pink_10: '#6B0E32',
+  semantic_red_01: '#FFEDED',
+  semantic_red_02: '#FF4A4A',
+  semantic_blue_01: 'E5F9E6',
+  semantic_blue_02: '2CCB32',
+};
+
+const theme = { mixin, colors };
 
 export default theme;
