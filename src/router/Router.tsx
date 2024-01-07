08f1dbbe3ced8ef074af8998518cdbb241bca6a6
urjimyu <92876819+urjimyu@users.noreply.github.com>
2024-01-07 23:36:48 +0900

    feat: 선물 홈 라우터 연결

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom';
 import Main from '../pages/Main';
 import Mypage from '../pages/Mypage';
 import Layout from '../layouts/Layout';
+import GiftHome from '../pages/GiftHome/GiftHome';
 
 const router = createBrowserRouter([
   {
@@ -15,6 +16,11 @@ const router = createBrowserRouter([
       {
         path: '/mypage',
         element: <Mypage />,
+      },
+      {
+        // 임의로 선물방 홈 페이지 연결
+        path: '/gifthome',
+        element: <GiftHome />,
       },
     ],
   },
