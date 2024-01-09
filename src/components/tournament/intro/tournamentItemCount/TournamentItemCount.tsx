3ee0d32f06e179f5781002afa5e6bd8bc13f3527
imeureka <lky062@naver.com>
2024-01-09 06:54:13 -0800

    chore: 아이템 개수 변수명 변경

--- src/components/tournament/intro/tournamentItemCount/TournamentItemCount.tsx
+++ src/components/tournament/intro/tournamentItemCount/TournamentItemCount.tsx
@@ -1,10 +1,10 @@
 import * as S from './TournamentItemCount.style';
 
 export default function TournamentItemCount() {
-  const price = 16;
+  const count = 16;
   return (
     <S.TournamentItemCountWrapper>
-      <S.TotalItems>총 등록된 선물 {price}개</S.TotalItems>
+      <S.TotalItems>총 등록된 선물 {count}개</S.TotalItems>
     </S.TournamentItemCountWrapper>
   );
 }
