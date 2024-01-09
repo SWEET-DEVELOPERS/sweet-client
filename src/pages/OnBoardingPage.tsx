26c2340796d8e928e5f3d992d62042cf08d42290
장정안 <wjddks96@naver.com>
2024-01-09 18:00:28 +0900

    style: 최상위 컴포넌트에서 margin 값 주는 Wrapper 작성

--- src/pages/OnBoardingPage.tsx
+++ src/pages/OnBoardingPage.tsx
@@ -1,7 +1,18 @@
 //온보딩 모든 컴포넌트를 funnel로 관리하는 최상위 페이지
 
+import styled from 'styled-components';
+import NameInput from '../components/onboarding/step01/Step01';
+
 const OnBoardingPage = () => {
-  return <div></div>;
+  return (
+    <OnBoardingPageWrapper>
+      <NameInput />
+    </OnBoardingPageWrapper>
+  );
 };
 
 export default OnBoardingPage;
+
+const OnBoardingPageWrapper = styled.div`
+  margin: 2rem;
+`;
