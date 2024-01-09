6ca22ce6d246bacc78a86f7f9e2941834a2dd420
Wang HoEun <hoeunwang@gmail.com>
2024-01-09 17:05:22 +0900

    feat: btn next style 코드 작업 완료

---
+++ src/components/common/Button/Next/BtnNext.style.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.button`
+  ${({ theme: { mixin } }) => mixin.flexCenter({})}
+
+  height: 4.4rem;
+  padding: 1rem 1.1rem 1rem 2rem;
+  border-radius: 9.9rem;
+
+  background-color: ${({ theme: { colors } }) => colors.G_02};
+  color: ${({ theme: { colors } }) => colors.G_07};
+  border-color: ${({ theme: { colors } }) => colors.G_07};
+
+  ${({ theme: { fonts } }) => fonts.body_09}
+
+  &:focus {
+    background-color: ${({ theme: { colors } }) => colors.P_06};
+    color: ${({ theme: { colors } }) => colors.white};
+    border: none;
+    outline: none;
+  }
+`;
--- src/components/common/Button/Next/index.tsx
+++ src/components/common/Button/Next/index.tsx
@@ -1,5 +1,18 @@
-const BtnNext = () => {
-  return <></>;
+import { ButtonHTMLAttributes } from 'react';
+import * as S from './BtnNext.style';
+
+type BtnNextProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  disabled?: boolean;
+  children: React.ReactNode;
+  onClick?: () => void;
+};
+
+const BtnNext = ({ disabled, children, onClick }: BtnNextProps) => {
+  return (
+    <S.Wrapper disabled={disabled} onClick={onClick}>
+      {children}
+    </S.Wrapper>
+  );
 };
 
 export default BtnNext;
