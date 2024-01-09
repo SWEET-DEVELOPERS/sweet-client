6816a532c1ecb4ee69e97ce7836ec1bddf529fcf
imeureka <lky062@naver.com>
2024-01-09 05:35:08 -0800

    feat: 토너먼트 인트로 컨테이너 컴포넌트 구현

---
+++ src/components/tournament/intro/TournamentContainer.tsx
@@ -0,0 +1,9 @@
+import TournamentStartText from './TournamentStartText';
+
+export default function TournamentConatainer() {
+  return (
+    <div>
+      <TournamentStartText />
+    </div>
+  );
+}
--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom';
 import Main from '../pages/Main';
 import Mypage from '../pages/Mypage';
 import Layout from '../layouts/Layout';
+import TournamentConatainer from '../components/tournament/intro/TournamentContainer';
 
 const router = createBrowserRouter([
   {
@@ -15,6 +16,10 @@ const router = createBrowserRouter([
       {
         path: '/mypage',
         element: <Mypage />,
+      },
+      {
+        path: '/tournament',
+        element: <TournamentConatainer />,
       },
     ],
   },
