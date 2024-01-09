b84b80b35bb810fd6badc80f129afa578a0006dc
urjimyu <92876819+urjimyu@users.noreply.github.com>
2024-01-09 14:31:49 +0900

    feat: 선물홈 2030, 친구 UI 구현

--- src/components/GiftHome/GiftHome2030Gifts/GiftHome2030Gifts.tsx
+++ src/components/GiftHome/GiftHome2030Gifts/GiftHome2030Gifts.tsx
@@ -1,5 +1,45 @@
 import React from 'react';
+import * as S from '../GiftHomeShowcase.styled';
 
 export default function GiftHome2030Gifts() {
-  return <div></div>;
+  const isGift = true;
+  const price = 42000;
+  const user = '강국희';
+
+  return (
+    <S.GiftHomeShowcaseWrapper>
+      {/* 공통 헤더 넣을 예정 */}
+      {isGift ? (
+        <S.GiftsWrapper>
+          <S.GiftsItemWrapper>
+            <S.GiftsItemImage></S.GiftsItemImage>
+            <S.GiftsItemTitle>오로라 레터링 케이크(소)</S.GiftsItemTitle>
+            <S.GiftsItemPrice>{price}원</S.GiftsItemPrice>
+          </S.GiftsItemWrapper>
+          <S.GiftsItemWrapper>
+            <S.GiftsItemImage></S.GiftsItemImage>
+            <S.GiftsItemTitle>오로라 레터링 케이크(소)</S.GiftsItemTitle>
+            <S.GiftsItemPrice>{price}원</S.GiftsItemPrice>
+          </S.GiftsItemWrapper>
+          <S.GiftsItemWrapper>
+            <S.GiftsItemImage></S.GiftsItemImage>
+            <S.GiftsItemTitle>오로라 레터링 케이크(소)</S.GiftsItemTitle>
+            <S.GiftsItemPrice>{price}원</S.GiftsItemPrice>
+          </S.GiftsItemWrapper>
+          <S.GiftsItemWrapper>
+            <S.GiftsItemImage></S.GiftsItemImage>
+            <S.GiftsItemTitle>오로라 레터링 케이크(소)</S.GiftsItemTitle>
+            <S.GiftsItemPrice>{price}원</S.GiftsItemPrice>
+          </S.GiftsItemWrapper>
+          <S.GiftsItemWrapper>
+            <S.GiftsItemImage></S.GiftsItemImage>
+            <S.GiftsItemTitle>오로라 레터링 케이크(소)</S.GiftsItemTitle>
+            <S.GiftsItemPrice>{price}원</S.GiftsItemPrice>
+          </S.GiftsItemWrapper>
+        </S.GiftsWrapper>
+      ) : (
+        <S.NoGiftsWrapper>아직 선물이 등록되지 않았어요</S.NoGiftsWrapper>
+      )}
+    </S.GiftHomeShowcaseWrapper>
+  );
 }
--- src/components/GiftHome/GiftHomeFriendsGifts/GiftHomeFriendsGifts.tsx
+++ src/components/GiftHome/GiftHomeFriendsGifts/GiftHomeFriendsGifts.tsx
@@ -1,4 +1,4 @@
-import * as S from './GiftHomeFriendsGifts.styled';
+import * as S from '../GiftHomeShowcase.styled';
 
 export default function GiftHomeFriendsGifts() {
   const isGift = true;
@@ -6,10 +6,34 @@ export default function GiftHomeFriendsGifts() {
   const user = '강국희';
 
   return (
-    <S.GiftHomeFriendsGiftsWrapper>
+    <S.GiftHomeShowcaseWrapper>
       {/* 공통 헤더 넣을 예정 */}
       {isGift ? (
         <S.GiftsWrapper>
+          <S.GiftsItemWrapper>
+            <S.GiftsItemImage></S.GiftsItemImage>
+            <S.GiftsItemTitle>오로라 레터링 케이크(소)</S.GiftsItemTitle>
+            <S.GiftsItemPrice>{price}원</S.GiftsItemPrice>
+            <S.GiftsItemUser>{user}</S.GiftsItemUser>
+          </S.GiftsItemWrapper>
+          <S.GiftsItemWrapper>
+            <S.GiftsItemImage></S.GiftsItemImage>
+            <S.GiftsItemTitle>오로라 레터링 케이크(소)</S.GiftsItemTitle>
+            <S.GiftsItemPrice>{price}원</S.GiftsItemPrice>
+            <S.GiftsItemUser>{user}</S.GiftsItemUser>
+          </S.GiftsItemWrapper>
+          <S.GiftsItemWrapper>
+            <S.GiftsItemImage></S.GiftsItemImage>
+            <S.GiftsItemTitle>오로라 레터링 케이크(소)</S.GiftsItemTitle>
+            <S.GiftsItemPrice>{price}원</S.GiftsItemPrice>
+            <S.GiftsItemUser>{user}</S.GiftsItemUser>
+          </S.GiftsItemWrapper>
+          <S.GiftsItemWrapper>
+            <S.GiftsItemImage></S.GiftsItemImage>
+            <S.GiftsItemTitle>오로라 레터링 케이크(소)</S.GiftsItemTitle>
+            <S.GiftsItemPrice>{price}원</S.GiftsItemPrice>
+            <S.GiftsItemUser>{user}</S.GiftsItemUser>
+          </S.GiftsItemWrapper>
           <S.GiftsItemWrapper>
             <S.GiftsItemImage></S.GiftsItemImage>
             <S.GiftsItemTitle>오로라 레터링 케이크(소)</S.GiftsItemTitle>
@@ -20,6 +44,6 @@ export default function GiftHomeFriendsGifts() {
       ) : (
         <S.NoGiftsWrapper>아직 선물이 등록되지 않았어요</S.NoGiftsWrapper>
       )}
-    </S.GiftHomeFriendsGiftsWrapper>
+    </S.GiftHomeShowcaseWrapper>
   );
 }
