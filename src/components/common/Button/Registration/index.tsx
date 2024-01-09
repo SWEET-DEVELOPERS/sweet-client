660dd83e6c322540272c821d70e33b12da420539
Wang HoEun <hoeunwang@gmail.com>
2024-01-09 22:35:34 +0900

    feat: registration 버튼 style 구현

--- src/components/common/Button/Registration/index.tsx
+++ src/components/common/Button/Registration/index.tsx
@@ -1,4 +1,17 @@
-const BtnRegistration = () => {
-  return <></>;
+import { ButtonHTMLAttributes } from 'react';
+import * as S from './BtnRegistration.style';
+
+type BtnRegistrationProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  disabled?: boolean;
+  children: React.ReactNode;
+  onClick?: () => void;
+};
+
+const BtnRegistration = ({ disabled, children, onClick }: BtnRegistrationProps) => {
+  return (
+    <S.Wrapper disabled={disabled} onClick={onClick}>
+      {children}
+    </S.Wrapper>
+  );
 };
 export default BtnRegistration;
