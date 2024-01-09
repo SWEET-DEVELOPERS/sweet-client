b5427c82e2f576ec5ed5a2c2acc56e3e1a0233ab
장정안 <wjddks96@naver.com>
2024-01-09 14:24:46 +0900

    style: STEP02~05까지 우선적으로 타이틀 구현
    세부적인 인풋창과 같은 속성들은 추후 구현하겠습니다

---
+++ src/components/onboarding/Step03.tsx
@@ -0,0 +1,13 @@
+import * as S from './Onboarding.style';
+
+const GiftDelivery = () => {
+  return (
+    <S.TitleWrapper>
+      <S.Title>님께</S.Title>
+      <S.Title>언제 선물을</S.Title>
+      <S.Title>전달하실 예정인가요?</S.Title>
+    </S.TitleWrapper>
+  );
+};
+
+export default GiftDelivery;
---
+++ src/components/onboarding/Step04.tsx
@@ -0,0 +1,13 @@
+import * as S from './Onboarding.style';
+
+const SetTournamentSchedule = () => {
+  return (
+    <S.TitleWrapper>
+      <S.Title>선물 토너먼트</S.Title>
+      <S.Title>시작 일정을 설정해주세요</S.Title>
+      <S.SubTitle>토너먼트 시작 전까지 선물을 등록할 수 있어요.</S.SubTitle>
+    </S.TitleWrapper>
+  );
+};
+
+export default SetTournamentSchedule;
