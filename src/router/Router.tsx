12410b5a0bec2fafcff46aea1d365de5ab5ec674
urjimyu <92876819+urjimyu@users.noreply.github.com>
2024-01-09 22:48:09 +0900

    feat: 라우터에 페이지 임시 추가

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -3,6 +3,7 @@ import Main from '../pages/Main';
 import Mypage from '../pages/Mypage';
 import Layout from '../layouts/Layout';
 import OnBoardingPage from '../pages/OnBoardingPage';
+import GiftAddPage from '../pages/GiftAddPage/GiftAddPage';
 
 const router = createBrowserRouter([
   {
@@ -20,6 +21,10 @@ const router = createBrowserRouter([
       {
         path: '/onboarding',
         element: <OnBoardingPage />,
+      },
+      {
+        path: '/giftadd',
+        element: <GiftAddPage />,
       },
     ],
   },
