58b256615f9f8610813aa67ac13b53081e3ea06c
Wang HoEun <hoeunwang@gmail.com>
2024-01-10 03:06:10 +0900

    feat: subTitle 기능 구현

--- src/components/common/Button/Subtitle/index.tsx
+++ src/components/common/Button/Subtitle/index.tsx
@@ -1,5 +1,24 @@
-const BtnSubtitle = () => {
-  return <></>;
+import { ButtonHTMLAttributes } from 'react';
+import * as S from './BtnSubtitle.style';
+import { IcRight } from '../../../../assets/svg';
+
+type BtnSubtitleProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  disabled?: boolean;
+  state: boolean;
+  onClick?: () => void;
+};
+
+const BtnSubtitle = ({ disabled, state, onClick }: BtnSubtitleProps) => {
+  return (
+    <S.Wrapper disabled={disabled} onClick={onClick}>
+      <span>진행 중인 선물방</span>
+      {state ? (
+        <IcRight style={{ width: '2.4rem', color: 'black' }} />
+      ) : (
+        <S.EditText>수정하기</S.EditText>
+      )}
+    </S.Wrapper>
+  );
 };
 
 export default BtnSubtitle;
