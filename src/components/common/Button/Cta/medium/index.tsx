4280495cd2a7a85910497bdd2dcf9005704536d0
Wang HoEun <hoeunwang@gmail.com>
2024-01-09 15:48:01 +0900

    feat: btn medium 구현

--- src/components/common/Button/Cta/medium/index.tsx
+++ src/components/common/Button/Cta/medium/index.tsx
@@ -1,5 +1,18 @@
-const BtnMedium = () => {
-  return <></>;
+import { ButtonHTMLAttributes } from 'react';
+import * as S from './BtnMedium.style';
+
+type BtnFillProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  disabled?: boolean;
+  children: React.ReactNode;
+  onClick?: () => void;
+};
+
+const BtnMedium = ({ disabled, children, onClick }: BtnFillProps) => {
+  return (
+    <S.Wrapper disabled={disabled} onClick={onClick}>
+      {children}
+    </S.Wrapper>
+  );
 };
 
 export default BtnMedium;
