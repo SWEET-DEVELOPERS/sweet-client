90412d0b52bc10427c8841a652ad785469a97ac2
urjimyu <92876819+urjimyu@users.noreply.github.com>
2024-01-09 11:45:49 +0900

    feat: 임시로 세부 페이지 라우터 추가

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -2,6 +2,8 @@ import { createBrowserRouter } from 'react-router-dom';
 import Main from '../pages/Main';
 import Mypage from '../pages/Mypage';
 import Layout from '../layouts/Layout';
+import GiftHomeDetailFriends from '../pages/GiftHomeDetail/GiftHomeDetailFriends';
+import GiftHomeDetail2030 from '../pages/GiftHomeDetail/GiftHomeDetail2030';
 
 const router = createBrowserRouter([
   {
@@ -15,6 +17,16 @@ const router = createBrowserRouter([
       {
         path: '/mypage',
         element: <Mypage />,
+      },
+      {
+        // 임의로 세부페이지 추가
+        path: '/giftdetailfriends',
+        element: <GiftHomeDetailFriends />,
+      },
+      {
+        // 임의로 세부페이지 추가
+        path: '/giftdetail2030',
+        element: <GiftHomeDetail2030 />,
       },
     ],
   },
