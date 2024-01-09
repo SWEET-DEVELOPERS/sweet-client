8da78dab478bf0f69635a8c9e91a60910a447eda
장정안 <wjddks96@naver.com>
2024-01-09 14:21:37 +0900

    style: 공통 스타일 분리

---
+++ src/components/onboarding/Onboarding.style.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+import theme from '../../style/theme';
+
+export const TitleWrapper = styled.div`
+  margin-left: 2rem;
+  margin-right: 2rem;
+`;
+
+export const Title = styled.p`
+  color: ${theme.colors.B_01};
+  ${theme.fonts.heading_01};
+`;
+
+export const SubTitle = styled.p`
+  color: ${theme.colors.G_10};
+  ${theme.fonts.caption_02};
+`;
