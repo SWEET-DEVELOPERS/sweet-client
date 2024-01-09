4b80b1ba8c5c665fc98886a5970ed98e58f93272
장정안 <wjddks96@naver.com>
2024-01-09 23:47:02 +0900

    style: Step02 썸네일 등록 뷰 구현

--- src/components/onboarding/step02/Step02.tsx
+++ src/components/onboarding/step02/Step02.tsx
@@ -1,7 +1,26 @@
+import styled from 'styled-components';
 import Title from '../../common/title/Title';
+import { IcPlusImageFinal } from '../../../assets/svg';
 
 const ThumbnailInput = () => {
-  return <Title title='썸네일을 등록해주세요' />;
+  return (
+    <>
+      <Title title='썸네일을 등록해주세요' />
+      <div style={{ width: '100%', marginTop: '11rem' }}>
+        <ThumbnailWrapper>
+          <IcPlusImageFinal style={{ width: '5rem', height: '5rem' }} />
+        </ThumbnailWrapper>
+      </div>
+    </>
+  );
 };
 
 export default ThumbnailInput;
+
+const ThumbnailWrapper = styled.div`
+  ${({ theme }) => theme.mixin.flexCenter({})}
+  background-color: ${({ theme }) => theme.colors.G_01};
+  width: 24rem;
+  height: 24rem;
+  margin: 0 auto;
+`;
