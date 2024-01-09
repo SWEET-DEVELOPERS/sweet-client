33ce82bedc5357cfb64d9b49e3f862da3279a646
장정안 <wjddks96@naver.com>
2024-01-09 21:05:57 +0900

    style: 오타 수정

--- src/components/onboarding/step04/Step04.tsx
+++ src/components/onboarding/step04/Step04.tsx
@@ -6,7 +6,7 @@ const SetTournamentSchedule = () => {
     <>
       <Title title='선물 토너먼트' />
       <Title title='시작 일정을 설정해주세요' />
-      <SubTitle subTitle='너먼트 시작 전까지 선물을 등록할 수 있어요.' />
+      <SubTitle subTitle='토너먼트 시작 전까지 선물을 등록할 수 있어요.' />
     </>
   );
 };
