277256829d65b4c11a0e51e6a290d18b6f98879e
Wang HoEun <hoeunwang@gmail.com>
2024-01-10 02:30:09 +0900

    feat: btn edit  구현

--- src/components/common/Button/Edit/index.tsx
+++ src/components/common/Button/Edit/index.tsx
@@ -1,5 +1,18 @@
-const BtnEdit = () => {
-  return <></>;
+import { ButtonHTMLAttributes } from 'react';
+import * as S from './BtnEdit.style';
+import { IcEdit2 } from '../../../../assets/svg';
+type BtnEditProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  disabled?: boolean;
+  children: React.ReactNode;
+  onClick?: () => void;
+};
+const BtnEdit = ({ disabled, children, onClick }: BtnEditProps) => {
+  return (
+    <S.Wrapper disabled={disabled} onClick={onClick}>
+      <IcEdit2 style={{ width: '2.4rem', color: 'white' }} />
+      <S.Text>{children}</S.Text>
+    </S.Wrapper>
+  );
 };
 
 export default BtnEdit;
