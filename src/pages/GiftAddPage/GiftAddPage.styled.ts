859db93dc204b46d51ed47712a469d08a8f36a9f
urjimyu <92876819+urjimyu@users.noreply.github.com>
2024-01-09 22:17:20 +0900

    feat: 내가 추가한 선물 페이지 UI 구현중

---
+++ src/pages/GiftAddPage/GiftAddPage.styled.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const GiftAddPageWrapper = styled.div`
+  width: 100%;
+  padding: 0 2.2rem;
+`;
+
+export const AddButtonsWrapper = styled.div`
+display: flex;
+
+  width: 100%;
+`;
---
+++ src/pages/GiftAddPage/GiftAddPage.tsx
@@ -0,0 +1,13 @@
+import * as S from './GiftAddPage.styled';
+
+function GiftAddPage() {
+  return (
+    <S.GiftAddPageWrapper>
+      {/* 헤더 추가 예정 */}
+      {/* <MiniTimer/> */}
+      <S.AddButtonsWrapper></S.AddButtonsWrapper>
+    </S.GiftAddPageWrapper>
+  );
+}
+
+export default GiftAddPage;
