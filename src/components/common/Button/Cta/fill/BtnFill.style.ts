3dc7014b0110d73f16746bbd808de5fa66d4e475
Wang HoEun <hoeunwang@gmail.com>
2024-01-09 15:10:09 +0900

    feat: btnfill 제작 완료

---
+++ src/components/common/Button/Cta/fill/BtnFill.style.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.button`
+  ${({ theme: { mixin } }) => mixin.flexCenter({})}
+
+  width: 33.5rem;
+  padding: 1.4rem 8.1rem;
+  gap: 1rem;
+  border-radius: 5.5rem;
+
+  background-color: ${({ theme: { colors } }) => colors.black};
+  color: ${({ theme: { colors } }) => colors.white};
+  ${({ theme: { fonts } }) => fonts.body_09}
+`;
--- src/components/common/Button/Cta/fill/index.tsx
+++ src/components/common/Button/Cta/fill/index.tsx
@@ -1,4 +1,17 @@
-const BtnFill = () => {
-  return <></>;
+import { ButtonHTMLAttributes } from 'react';
+import * as S from './BtnFill.style';
+
+type BtnFillProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  disabled?: boolean;
+  children: React.ReactNode;
+  onClick?: () => void;
+};
+
+const BtnFill = ({ disabled, children, onClick }: BtnFillProps) => {
+  return (
+    <S.Wrapper disabled={disabled} onClick={onClick}>
+      {children}
+    </S.Wrapper>
+  );
 };
 export default BtnFill;
