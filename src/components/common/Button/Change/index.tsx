fd01198c0c1b0a64ee7d29c6b2889b13f468055a
Wang HoEun <hoeunwang@gmail.com>
2024-01-10 00:59:42 +0900

    feat: change btn 구현

--- src/components/common/Button/Change/index.tsx
+++ src/components/common/Button/Change/index.tsx
@@ -1,4 +1,19 @@
-const BtnChange = () => {
-  return <></>;
+import { ButtonHTMLAttributes } from 'react';
+import { IcRefresh } from '../../../../assets/svg';
+import * as S from './BtnChange.style';
+
+type BtnChangeProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  disabled?: boolean;
+  children: React.ReactNode;
+  onClick?: () => void;
+};
+
+const BtnChange = ({ disabled, children, onClick }: BtnChangeProps) => {
+  return (
+    <S.Wrapper disabled={disabled} onClick={onClick}>
+      <IcRefresh style={{ width: '2rem' }} />
+      <S.Text>{children}</S.Text>
+    </S.Wrapper>
+  );
 };
 export default BtnChange;
