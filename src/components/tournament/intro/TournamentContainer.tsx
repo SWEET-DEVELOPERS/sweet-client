51676154fb5ad804a08d3d3c5bc2ae95a3962cfe
imeureka <lky062@naver.com>
2024-01-09 06:31:00 -0800

    feat: 토너먼트 인트로 컨테이너 컴포넌트 구현

--- src/components/tournament/intro/TournamentContainer.tsx
+++ src/components/tournament/intro/TournamentContainer.tsx
@@ -1,9 +1,24 @@
 import TournamentStartText from './TournamentStartText';
+import TournamentItemCount from './TournamentItemCount';
+import styled from 'styled-components';
+import { Svg3Dicons } from '../../../assets/svg';
 
 export default function TournamentConatainer() {
   return (
-    <div>
-      <TournamentStartText />
-    </div>
+    <>
+    <TournamentStartText />
+      <TournamentItemCount />
+      <TournamentImg>
+        <Svg3Dicons />
+      </TournamentImg>
+      {/* 공통버튼 btn_cta_fill 들어갈 예정*/}
+    </>
+      
   );
 }
+
+const TournamentImg = styled.div`
+  width: 20rem;
+  height: 20rem;
+  margin: 0 auto;
+`;
