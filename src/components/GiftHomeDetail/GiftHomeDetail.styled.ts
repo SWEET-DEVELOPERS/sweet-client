59b072f87eafa90c546e3104d343bcc3532ead3b
urjimyu <92876819+urjimyu@users.noreply.github.com>
2024-01-09 14:59:21 +0900

    feat: 미니 타이머 UI 구현

--- src/components/GiftHomeDetail/GiftHomeDetail.styled.ts
+++ src/components/GiftHomeDetail/GiftHomeDetail.styled.ts
@@ -11,6 +11,24 @@ export const GiftHomeDetailWrapper = styled.div`
   background-color: ${({ theme }) => theme.colors.white};
 `;
 
+export const GiftsDetailTimerWrapper = styled.div`
+  padding: 0.3rem 1rem;
+  margin: 2.2rem 0 1.2rem 0;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  column-gap: 0.4rem;
+
+  border-radius: 1.4rem;
+  background: ${({ theme }) => theme.colors.G_01};
+`;
+
+export const GiftsDetailTimerNumbers = styled.span`
+  ${({ theme }) => theme.fonts.body_09};
+  color: ${({ theme }) => theme.colors.P_06};
+`;
 export const GiftsItemWrapper = styled.div`
   width: 16rem;
   height: 25.7rem;
--- src/pages/GiftHomeDetail/GiftHomeDetailFriends.tsx
+++ src/pages/GiftHomeDetail/GiftHomeDetailFriends.tsx
@@ -4,9 +4,13 @@ function GiftHomeDetail() {
   const price = 42000;
   const name = '왕호은';
   const people = 3;
+  const time = '00:00:00';
 
   return (
     <S.GiftHomeDetailWrapper>
+      <S.GiftsDetailTimerWrapper>
+        <S.GiftsDetailTimerNumbers>{time}</S.GiftsDetailTimerNumbers>
+      </S.GiftsDetailTimerWrapper>
       {/* 공통 헤더 추가 예정 */}
       {/* 미니 타이머 추가 예정 */}
       <S.GiftsItemWrapper>
