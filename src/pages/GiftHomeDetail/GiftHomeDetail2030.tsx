1024842874547353002c79e348724266ceb5cdc6
urjimyu <92876819+urjimyu@users.noreply.github.com>
2024-01-09 11:48:00 +0900

    feat: 선물홈 친구, 2030 세부 페이지 UI 구현

---
+++ src/pages/GiftHomeDetail/GiftHomeDetail2030.tsx
@@ -0,0 +1,38 @@
+import * as S from '../../components/GiftHomeDetail/GiftHomeDetail.styled';
+
+function GiftHomeDetail() {
+  const price = 42000;
+  const name = '왕호은';
+  return (
+    <S.GiftHomeDetailWrapper>
+      {/* 공통 헤더 추가 예정 */}
+      {/* 미니 타이머 추가 예정 */}
+      <S.GiftsItemWrapper>
+        <S.GiftsItemImage></S.GiftsItemImage>
+        <S.GiftsItemTitle>어센틱 로고 후디 멜란지 그레이</S.GiftsItemTitle>
+        <S.GiftsItemPrice>{price}원</S.GiftsItemPrice>
+        <S.GiftsItemUser>{name}</S.GiftsItemUser>
+      </S.GiftsItemWrapper>
+      <S.GiftsItemWrapper>
+        <S.GiftsItemImage></S.GiftsItemImage>
+        <S.GiftsItemTitle>어센틱 로고 후디 멜란지 그레이</S.GiftsItemTitle>
+        <S.GiftsItemPrice>{price}원</S.GiftsItemPrice>
+        <S.GiftsItemUser>{name}</S.GiftsItemUser>
+      </S.GiftsItemWrapper>
+      <S.GiftsItemWrapper>
+        <S.GiftsItemImage></S.GiftsItemImage>
+        <S.GiftsItemTitle>어센틱 로고 후디 멜란지 그레이</S.GiftsItemTitle>
+        <S.GiftsItemPrice>{price}원</S.GiftsItemPrice>
+        <S.GiftsItemUser>{name}</S.GiftsItemUser>
+      </S.GiftsItemWrapper>
+      <S.GiftsItemWrapper>
+        <S.GiftsItemImage></S.GiftsItemImage>
+        <S.GiftsItemTitle>어센틱 로고 후디 멜란지 그레이</S.GiftsItemTitle>
+        <S.GiftsItemPrice>{price}원</S.GiftsItemPrice>
+        <S.GiftsItemUser>{name}</S.GiftsItemUser>
+      </S.GiftsItemWrapper>
+    </S.GiftHomeDetailWrapper>
+  );
+}
+
+export default GiftHomeDetail;
