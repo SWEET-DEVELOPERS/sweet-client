3815994001de09f3f61e0334a40bdb0baf2cc082
장정안 <wjddks96@naver.com>
2024-01-10 17:09:34 +0900

    feat: 반복되는 부분 map 함수로 변경

--- src/components/onboarding/step05/Step05.tsx
+++ src/components/onboarding/step05/Step05.tsx
@@ -5,6 +5,14 @@ import * as S from './Step05.style';
 
 const SetTournamentDuration = () => {
   // TODO 오늘 기준 날짜로 수정 & map함수로 수정
+
+  const timeOptions = [
+    { text: '6시간', dateType: 'today' },
+    { text: '12시간', dateType: 'today' },
+    { text: '18시간', dateType: 'nottoday' },
+    { text: '24시간', dateType: 'nottoday' },
+  ];
+
   return (
     <div>
       <div>
@@ -15,34 +23,19 @@ const SetTournamentDuration = () => {
         <SubTitle subTitle='토너먼트가 아래 시간 동안 진행돼요.' />
       </div>
       <S.Wrapper>
-        <S.DetailBox>
-          <S.RadioBox>
-            <IcGroup style={{ width: '2.4rem', height: '2.4rem' }} />
-            <S.TimeText>6시간</S.TimeText>
-          </S.RadioBox>
-          <S.InTodayDate>6월 23일 당일</S.InTodayDate>
-        </S.DetailBox>
-        <S.DetailBox>
-          <S.RadioBox>
-            <IcGroup style={{ width: '2.4rem', height: '2.4rem' }} />
-            <S.TimeText>12시간</S.TimeText>
-          </S.RadioBox>
-          <S.InTodayDate>6월 23일 당일</S.InTodayDate>
-        </S.DetailBox>
-        <S.DetailBox>
-          <S.RadioBox>
-            <IcGroup style={{ width: '2.4rem', height: '2.4rem' }} />
-            <S.TimeText>18시간</S.TimeText>
-          </S.RadioBox>
-          <S.NotTodayDate>6월 23일 이후</S.NotTodayDate>
-        </S.DetailBox>
-        <S.DetailBox>
-          <S.RadioBox>
-            <IcGroup style={{ width: '2.4rem', height: '2.4rem' }} />
-            <S.TimeText>24시간</S.TimeText>
-          </S.RadioBox>
-          <S.NotTodayDate>6월 23일 이후</S.NotTodayDate>
-        </S.DetailBox>
+        {timeOptions.map((option, index) => (
+          <S.DetailBox key={index}>
+            <S.RadioBox>
+              <IcGroup style={{ width: '2.4rem', height: '2.4rem' }} />
+              <S.TimeText>{option.text}</S.TimeText>
+            </S.RadioBox>
+            {option.dateType === 'today' ? (
+              <S.InTodayDate>6월 23일 당일</S.InTodayDate>
+            ) : (
+              <S.NotTodayDate>6월 23일 이후</S.NotTodayDate>
+            )}
+          </S.DetailBox>
+        ))}
       </S.Wrapper>
     </div>
   );
