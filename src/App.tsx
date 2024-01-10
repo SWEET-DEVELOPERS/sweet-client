c1f33a3be5138b577084346677d90e157234c9af
urjimyu <92876819+urjimyu@users.noreply.github.com>
2024-01-10 23:29:48 +0900

    feat: 높이 설정 추가

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,18 @@ import { RouterProvider } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 import router from './router/Router';
 import GlobalStyle from './style/GlobalStyle';
+import { styled } from 'styled-components';
 
 function App() {
+  const Wrapper = styled.div`
+    background-color: white;
+    border: none;
+    min-height: calc(var(--vh, 1vh) * 100);
+    margin-left: auto;
+    margin-right: auto;
+    position: relative;
+  `;
+
   const [queryClient] = useState(
     () =>
       new QueryClient({
@@ -17,12 +27,14 @@ function App() {
       }),
   );
   return (
-    <QueryClientProvider client={queryClient}>
-      <RecoilRoot>
-        <RouterProvider router={router} />
-        <GlobalStyle />
-      </RecoilRoot>
-    </QueryClientProvider>
+    <Wrapper>
+      <QueryClientProvider client={queryClient}>
+        <RecoilRoot>
+          <RouterProvider router={router} />
+          <GlobalStyle />
+        </RecoilRoot>
+      </QueryClientProvider>
+    </Wrapper>
   );
 }
 
