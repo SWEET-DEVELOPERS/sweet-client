331544c3925963d78913e00d3c24876a9f7bc4ff
imeureka <lky062@naver.com>
2024-01-10 06:31:33 -0800

    feat: 토너먼트 title 컴포넌트 구현

--- src/components/tournament/tournamentFlow/tournamentTitle/TournamentTitle.tsx
+++ src/components/tournament/tournamentFlow/tournamentTitle/TournamentTitle.tsx
@@ -1,5 +1,15 @@
+import * as S from './TournamernTitle.style';
+
 const TournamentTitle = () => {
-  return <div></div>;
+  const round = 1;
+  return (
+    <>
+      {/* 라운드 컴포넌트 */}
+      <S.Title>Round {round}</S.Title>
+      <S.SetCount>1/6</S.SetCount>
+      {/* 세트 수 현재 상황 */}
+    </>
+  );
 };
 
 export default TournamentTitle;
---
+++ src/components/tournament/tournamentFlow/tournamentTitle/TournamernTitle.style.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Title = styled.p`
+  ${({ theme }) => theme.fonts.suit};
+  font-size: 3.4rem;
+  color: ${({ theme }) => theme.colors.P_06};
+`;
+
+export const SetCount = styled.p`
+  margin-bottom: 4rem;
+  font-size: 18px;
+  color: ${({ theme }) => theme.colors.G_05};
+  ${({ theme }) => theme.fonts.body_02};
+`;
