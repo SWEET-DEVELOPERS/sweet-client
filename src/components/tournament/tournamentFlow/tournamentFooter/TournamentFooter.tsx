a78fcf37e982b09fef2edeb0763b2b24b01c0765
imeureka <lky062@naver.com>
2024-01-10 06:31:16 -0800

    feat: 토너먼트 footer 컴포넌트 구현

--- src/components/tournament/tournamentFlow/tournamentFooter/TournamentFooter.tsx
+++ src/components/tournament/tournamentFlow/tournamentFooter/TournamentFooter.tsx
@@ -1,8 +1,18 @@
+import { IcLeft } from '../../../../assets/svg';
+import BtnNext from '../../../common/Button/Next/BtnNext';
+import * as S from './TournamentFooter.style';
+
 const TournamentFooter = () => {
   return (
-    <div>
-      <></>
-    </div>
+    <>
+      <S.TournamentFooterWrapper>
+        <S.Btnpre>
+          이전
+          <IcLeft style={{ width: '2.4rem', height: '2.4rem' }} />
+        </S.Btnpre>
+        <BtnNext>다음</BtnNext>
+      </S.TournamentFooterWrapper>
+    </>
   );
 };
 
