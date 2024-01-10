9ef769eb63860bee775f678306d74d99bdacae31
장정안 <wjddks96@naver.com>
2024-01-11 03:52:38 +0900

    feat: 아이콘 클릭 시 이미지 업로드 창 변경

--- src/components/onboarding/step02/Step02.tsx
+++ src/components/onboarding/step02/Step02.tsx
@@ -16,19 +16,24 @@ const ThumbnailInput = (props: ThumbnailInputProps) => {
       <Title title='썸네일을 등록해주세요' />
       <div style={{ width: '100%' }}>
         <S.ThumbnailWrapper>
-          <IcPlusImageFinal style={{ width: '5rem', height: '5rem' }} />
+          <input type='file' accept='image/*' style={{ display: 'none' }} id='imgInput' />
+          <label htmlFor='imgInput'>
+            <IcPlusImageFinal style={{ width: '5rem', height: '5rem' }} />
+          </label>
         </S.ThumbnailWrapper>
         <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
-          <BtnNext
-            type='button'
-            onClick={onNext}
-            customStyle={{
-              position: 'absolute',
-              bottom: '0',
-            }}
-          >
-            다음
-          </BtnNext>
+          <S.BtnWrapper>
+            <BtnNext
+              type='button'
+              onClick={onNext}
+              customStyle={{
+                position: 'absolute',
+                bottom: '0',
+              }}
+            >
+              다음
+            </BtnNext>
+          </S.BtnWrapper>
         </div>
       </div>
     </>
