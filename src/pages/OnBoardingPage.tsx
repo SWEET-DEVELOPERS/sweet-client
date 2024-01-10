375ef8b81175505851233dd896f2094fef17e32e
장정안 <wjddks96@naver.com>
2024-01-10 22:48:07 +0900

    feat: 최상위 온보딩 컴포넌트 퍼널 수정

--- src/pages/OnBoardingPage.tsx
+++ src/pages/OnBoardingPage.tsx
@@ -1,11 +1,12 @@
 //온보딩 모든 컴포넌트를 funnel로 관리하는 최상위 페이지
 
 import styled from 'styled-components';
-import NameInput from '../components/onboarding/step01/Step01';
 import { ONBOARDING_FORM_STEP } from '../core/onboarding';
 import { useFunnel } from '../hooks/useFunnel';
+import NameInput from '../components/onboarding/step01/Step01';
 import ThumbnailInput from '../components/onboarding/step02/Step02';
 import GiftDelivery from '../components/onboarding/step03/Step03';
+import SetTournamentSchedule from '../components/onboarding/step04/Step04';
 import SetTournamentDuration from '../components/onboarding/step05/Step05';
 
 const OnBoardingPage = () => {
@@ -21,10 +22,10 @@ const OnBoardingPage = () => {
           <ThumbnailInput onNext={() => setStep((prev) => 'PRESENT')} />
         </Funnel.Step>
         <Funnel.Step name='PRESENT'>
-          <GiftDelivery onNext={() => setStep((prev) => 'TOURNAMENT_PROCEEDING')} />
+          <GiftDelivery onNext={() => setStep((prev) => 'TOURNAMENT_SCHEDULE_REGISTRATION')} />
         </Funnel.Step>
         <Funnel.Step name='TOURNAMENT_SCHEDULE_REGISTRATION'>
-          <SetTournamentDuration onNext={() => setStep((prev) => 'GIFT_ROOM_FIX')} />
+          <SetTournamentSchedule onNext={() => setStep((prev) => 'TOURNAMENT_PROCEEDING')} />
         </Funnel.Step>
         <Funnel.Step name='TOURNAMENT_PROCEEDING'>
           <SetTournamentDuration onNext={() => setStep((prev) => 'GIFT_ROOM_FIX')} />
