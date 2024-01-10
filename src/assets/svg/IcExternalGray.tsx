73a4ef940763c6a8d022f061b7ce54db5df74bbc
imeureka <lky062@naver.com>
2024-01-10 11:02:35 -0800

    chore: 아이콘 추가

---
+++ src/assets/svg/IcExternalGray.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import type { SVGProps } from 'react';
+const SvgIcExternalGray = (props: SVGProps<SVGSVGElement>) => (
+  <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 16 16' {...props}>
+    <path
+      stroke='#ACA7A9'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      d='M7.25 2h-3A2.25 2.25 0 0 0 2 4.25v7.5A2.25 2.25 0 0 0 4.25 14h7.5A2.25 2.25 0 0 0 14 11.75v-3M10.25 2H14m0 0v3.375M14 2 7.624 8.375'
+    />
+  </svg>
+);
+export default SvgIcExternalGray;
