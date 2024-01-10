500ca2566c1599b3de916cdadde27aa2f2cbf126
장정안 <wjddks96@naver.com>
2024-01-10 15:22:47 +0900

    feat: 아이콘 클릭 시 인풋 값 지우기

--- src/components/onboarding/step01/Step01.tsx
+++ src/components/onboarding/step01/Step01.tsx
@@ -13,6 +13,10 @@ const NameInput = () => {
     inputValue.length + unicodeChars <= 10 ? setText(inputValue) : e.preventDefault();
   };
 
+  const handleBtnClick = () => {
+    setText('');
+  };
+
   return (
     <>
       <Title title='선물 받을 분의' />
@@ -21,13 +25,19 @@ const NameInput = () => {
         <S.TextField>
           <S.Input
             type='text'
+            value={text}
             onChange={onChange}
             maxLength={10}
             placeholder='이름을 입력해주세요'
           />
         </S.TextField>
         <S.IconField>
-          {text.length > 0 && <IcCancelCircleFinal style={{ width: '2.4rem', height: '2.4rem' }} />}
+          {text.length > 0 && (
+            <IcCancelCircleFinal
+              style={{ width: '2.4rem', height: '2.4rem' }}
+              onClick={handleBtnClick}
+            />
+          )}
         </S.IconField>
       </S.Wrapper>
       <S.LetterLength>({text.length}/10)</S.LetterLength>
