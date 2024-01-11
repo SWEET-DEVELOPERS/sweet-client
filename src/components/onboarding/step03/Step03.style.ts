ac0d3e6e8de87a5242514292bfe037421a3c1280
장정안 <wjddks96@naver.com>
2024-01-11 13:04:00 +0900

    style: 날짜 선택 시 밑줄 빨간색 처리

--- src/components/onboarding/step03/Step03.style.ts
+++ src/components/onboarding/step03/Step03.style.ts
@@ -1,11 +1,17 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Wrapper = styled.div`
+export const Wrapper = styled.div<{ hasContent: boolean }>`
   ${({ theme }) => theme.mixin.flexBox({ align: 'center', justify: 'center' })}
   width: 100%;
   height: 3.6rem;
   margin-top: 7.2rem;
   border-bottom: 0.1rem solid ${({ theme }) => theme.colors.G_02};
+
+  ${(props) =>
+    props.hasContent &&
+    css`
+      border-bottom: 0.1rem solid ${({ theme }) => theme.colors.P_06};
+    `}
 `;
 
 export const TextField = styled.div`
@@ -22,4 +28,9 @@ export const Input = styled.input`
   height: 3.5rem;
   border: none;
   outline: none;
+
+  input::placeholder {
+    color: ${({ theme }) => theme.colors.G_07};
+    ${({ theme }) => theme.fonts.body_06};
+  }
 `;
