6495d45786f93e25d053cb432aca76061a5daaba
imeureka <lky062@naver.com>
2024-01-11 03:26:31 -0800

    feat: modal 버튼 함수 프롭 전달

--- src/components/common/Modal/Modal.tsx
+++ src/components/common/Modal/Modal.tsx
@@ -4,15 +4,18 @@ import BtnMedium from '../Button/Cta/medium/BtnMedium';
 
 interface ModalProps {
   children: React.ReactNode;
+  onConfirmClick?: () => void; // 클릭 이벤트 함수를 받을 수 있는 프롭 추가
 }
 
-const Modal: React.FC<ModalProps> = ({ children }) => {
+const Modal: React.FC<ModalProps> = ({ children, onConfirmClick }) => {
   return (
     <>
       <S.Overlay />
       <S.ModalWrapper>
         <S.ModalContent>{children}</S.ModalContent>
-        <BtnMedium customStyle={{ width: '21.3rem' }}>확인</BtnMedium>
+        <BtnMedium customStyle={{ width: '21.3rem' }} onClick={onConfirmClick}>
+          확인
+        </BtnMedium>
       </S.ModalWrapper>
     </>
   );
