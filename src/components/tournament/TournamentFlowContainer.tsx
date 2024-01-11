1e97d05232f3ec3a9b29941e8152f013a5c8b442
LEE KAYOUNG / KATIE <104339899+imeureka@users.noreply.github.com>
2024-01-11 00:20:37 -0800

    [토너먼트] 토너먼트 인트로 화면 퍼블리싱 및 랜더링 (#67)
    * feat: 토너먼트 공통 버튼 도입
    
    * docs: 토너먼트 컨테이너 폴더변경
    
    * style: 토너먼트 패딩 값 변경
    
    * feat: 공통 컴포넌트 시작하기 버튼 구현
    
    * feat: 시작하기 버튼 후 라운드 시작 로직 구현!
    
    * chore: 네이밍 오타 수정
    
    * style: 스타일 코드 분리
    
    * style: arrow 함수로 변경
    
    * fix: 아이콘 오류 수정

---
+++ src/components/tournament/TournamentFlowContainer.tsx
@@ -0,0 +1,12 @@
+import TournamentCard from './tournamentFlow/tournamentCard/TournamentCard';
+import TournamentFooter from './tournamentFlow/tournamentFooter/TournamentFooter';
+const TournamentFlowContainer = () => {
+  return (
+    <>
+      <TournamentCard />
+      <TournamentFooter />
+    </>
+  );
+};
+
+export default TournamentFlowContainer;
