038cf68068e1039c49b61d361a8e8449e8709a8b
Wang HoEun <hoeunwang@gmail.com>
2024-01-12 01:35:02 +0900

    fix: router browserRouter로 수정

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,55 +1,63 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Mypage from '../pages/Mypage';
-import Layout from '../layouts/Layout';
-import GiftHomeDetailFriends from '../pages/GiftHomeDetail/GiftHomeDetailFriends';
-import GiftHomeDetail2030 from '../pages/GiftHomeDetail/GiftHomeDetail2030';
-import OnBoardingPage from '../pages/OnBoardingPage';
-import TournamentOngoing from '../pages/TournamentOngoing';
-import TournamentPage from '../pages/TournamentPage';
 import Start from '../pages/Start/Start';
 import Login from '../components/Login';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Layout />,
-    children: [
-      {
-        path: '/',
-        element: <Start />,
-      },
-      {
-        // 임의로 세부페이지 추가
-        path: '/giftdetailfriends',
-        element: <GiftHomeDetailFriends />,
-      },
-      {
-        // 임의로 세부페이지 추가
-        path: '/giftdetail2030',
-        element: <GiftHomeDetail2030 />,
-      },
-      {
-        path: '/onboarding',
-        element: <OnBoardingPage />,
-      },
-      {
-        path: '/tournamentPro',
-        element: <TournamentOngoing />,
-      },
-      {
-        path: '/tournament',
-        element: <TournamentPage />,
-      },
-    ],
-  },
-  {
-    path: '/api/oauth/kakao/login',
-    element: <Login />,
-  },
-  {
-    path: '/mypage',
-    element: <Mypage />,
-  },
-]);
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Start />} />
+        <Route path='/ipad' element={<Login />} />
+        <Route path='/shop-ipad' element={<Mypage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default router;
+export default Router;
+
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <Layout />,
+//     children: [
+//       {
+//         path: '/',
+//         element: <Start />,
+//       },
+//       {
+//         path: '/api/oauth/kakao/login',
+//         element: <Login />,
+//       },
+//       {
+//         path: '/mypage',
+//         element: <Mypage />,
+//       },
+//       {
+//         // 임의로 세부페이지 추가
+//         path: '/giftdetailfriends',
+//         element: <GiftHomeDetailFriends />,
+//       },
+//       {
+//         // 임의로 세부페이지 추가
+//         path: '/giftdetail2030',
+//         element: <GiftHomeDetail2030 />,
+//       },
+//       {
+//         path: '/onboarding',
+//         element: <OnBoardingPage />,
+//       },
+//       {
+//         path: '/tournamentPro',
+//         element: <TournamentOngoing />,
+//       },
+//       {
+//         path: '/tournament',
+//         element: <TournamentPage />,
+//       },
+//     ],
+//   },
+// ]);
+//
+//export default router;
