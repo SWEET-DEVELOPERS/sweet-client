6f1567f002d1ddb02fea74a79bf1195572b9f076
imeureka <lky062@naver.com>
2024-01-11 04:31:27 -0800

    feat: 공통 컴포넌트 수정

--- src/components/common/Button/Cta/fill/BtnFill.style.ts
+++ src/components/common/Button/Cta/fill/BtnFill.style.ts
@@ -3,8 +3,8 @@ import styled from 'styled-components';
 export const Wrapper = styled.button`
   ${({ theme: { mixin } }) => mixin.flexCenter({})}
 
-  width: 33.5rem;
-  padding: 1.5rem 8.1rem 1.6rem 8.1rem;
+  /* width: 33.5rem;
+  padding: 1.5rem 8.1rem 1.6rem 8.1rem; */
   gap: 1rem;
   border-radius: 5.5rem;
 
--- src/components/tournament/TournamentResult/TournamentResultFooter/TournamentResultFooter.tsx
+++ src/components/tournament/TournamentResult/TournamentResultFooter/TournamentResultFooter.tsx
@@ -3,15 +3,14 @@ import styled from 'styled-components';
 const TournamentResultFooter = () => {
   return (
     <TournamentResultFooterWrapper>
-      <BtnFill>다시하기</BtnFill>
-      <BtnFill>결과 제출하기</BtnFill>
+      <BtnFill customStyle={{ width: '16.4rem' }}>다시하기</BtnFill>
+      <BtnFill customStyle={{ width: '16.4rem' }}>결과 제출하기</BtnFill>
     </TournamentResultFooterWrapper>
   );
 };
 
 const TournamentResultFooterWrapper = styled.footer`
   display: flex;
-  width: 16.4rem;
   height: 5.2rem;
 `;
 
