ef0d154cf5222c66427905ff75bf87f2e78e6599
Wang Hoeun <38005874+hoeun0723@users.noreply.github.com>
2024-01-11 20:30:55 +0900

    kakao 배포 페이지 확인을 위한 pr (#78)
    * feat: Login 페이지 생성
    
    * style: style코드 mixin으로 변경
    
    * feat: query key 세팅 및 instance 생성 (type 수정 필요)
    
    * chore: axios 설치
    
    * refactor: queryKeys 파일 삭제
    
    * feat: react-query user login api 코드 작성
    
    * feat: Login 로직 코드와 서버 연결 코드 작성
    
    * fix: baseurl import 수정 및 code queryString 으로 구현
    
    * style: 안 쓰는 코드 정리 및 로직 코드 정리
    
    * fix: 안 쓰는 import문 삭제
    
    * fix: 안쓰는 import 문 삭제
    
    ---------
    
    Co-authored-by: Wang HoEun <hoeunwang@gmail.com>

---
+++ src/hooks/queries/user/useLogin.tsx
@@ -0,0 +1,30 @@
+import { instance } from '../../../apis/client';
+import { LoginDataType } from '../../../types/user';
+
+interface LoginResponseType {
+  data: LoginDataType;
+}
+
+export const fetchAuth = (AUTHORIZE_CODE: string): Promise<LoginResponseType> =>
+  instance.post(`/oauth/kakao/login?code=${AUTHORIZE_CODE}`);
+
+export const useLogin = (AUTHORIZE_CODE: string) => {
+  // const setNickName = useSetRecoilState(nickNameState);
+  // const setMemberId = useSetRecoilState(memberIdState);
+  // const setProfileImage = useSetRecoilState(profileImageState);
+
+  fetchAuth(AUTHORIZE_CODE).then((response: LoginResponseType) => {
+    const data = response.data;
+    const JWT = response.data.memberToken.accessToken;
+    if (JWT) {
+      localStorage.setItem('EXIT_LOGIN_TOKEN', JWT);
+    }
+    // if (data.userInfo) {
+    //   setNickName(data.userInfo.nickname);
+    //   setMemberId(data.userInfo.memberId);
+    //   setProfileImage(data.userInfo.profileImage);
+    // }
+    console.log(JWT);
+    console.log(data);
+  });
+};
