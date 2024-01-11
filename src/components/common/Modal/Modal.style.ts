b21a5e8ccc73552a67a7638107e0f34715380b7c
imeureka <lky062@naver.com>
2024-01-11 03:28:03 -0800

    style: 스타일 속성 순서

--- src/components/common/Modal/Modal.style.ts
+++ src/components/common/Modal/Modal.style.ts
@@ -6,22 +6,24 @@ export const Overlay = styled.div`
   left: 0;
   width: 100%;
   height: 100%;
-  background: rgba(0, 0, 0, 0.5); /* 어두운 색상으로 설정 */
-  z-index: 999; /* 모달보다 낮은 z-index */
+  background: rgba(0, 0, 0, 0.5);
+  z-index: 999;
 `;
 
 export const ModalWrapper = styled.div`
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
   display: flex;
   justify-content: flex-end;
   align-items: center;
   flex-direction: column;
+  position: fixed;
+
   width: 283px;
   height: 195px;
   padding: 43px 35px 20px 35px;
+
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
   border-radius: 12px;
   gap: 28px;
   flex-shrink: 0;
