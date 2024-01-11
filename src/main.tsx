98e5a4ba2f7a9e7ac2de13b7dae5c60dabb581c0
Wang HoEun <hoeunwang@gmail.com>
2024-01-11 23:42:20 +0900

    fix: strict mode 삭제

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,7 @@ import App from './App.tsx';
 import Styles from './style/index.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <Styles>
-      <App />
-    </Styles>
-  </React.StrictMode>,
+  <Styles>
+    <App />
+  </Styles>,
 );
