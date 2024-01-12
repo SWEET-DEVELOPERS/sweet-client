f8ae05a32d7e370802f1519f27fc912ba0474c44
imeureka <lky062@naver.com>
2024-01-11 21:13:07 -0800

    chore: 메달아이콘 추가

---
+++ src/assets/svg/IcMedal1.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import type { SVGProps } from 'react';
+const SvgIcMedal1 = (props: SVGProps<SVGSVGElement>) => (
+  <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 25 38' {...props}>
+    <path
+      fill='#F57C75'
+      d='M6 15.042c0-.61.494-1.104 1.104-1.104h10.792c.61 0 1.104.494 1.104 1.104v20.905c0 .867-.953 1.396-1.689.937l-4.226-2.636a1.1 1.1 0 0 0-1.17 0L7.69 36.884A1.104 1.104 0 0 1 6 35.947z'
+    />
+    <path
+      fill='#FFDD2B'
+      d='M12.5 25.063c6.904 0 12.5-5.597 12.5-12.5 0-6.904-5.596-12.5-12.5-12.5S0 5.659 0 12.563c0 6.903 5.596 12.5 12.5 12.5'
+    />
+    <path fill='#000' d='M13.554 7.927v10.178l-1.484-.015V9.51l-1.792.993-.616-1.245 2.422-1.33z' />
+  </svg>
+);
+export default SvgIcMedal1;
