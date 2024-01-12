875ab62eef20661e958704c5c4065861f570d365
urjimyu <92876819+urjimyu@users.noreply.github.com>
2024-01-12 22:56:10 +0900

    fix: 빌드 에러 해결

--- src/assets/svg/IcTimeCount.tsx
+++ src/assets/svg/IcTimeCount.tsx
@@ -1,4 +1,3 @@
-import * as React from 'react';
 import type { SVGProps } from 'react';
 const SvgIcTimeCount = (props: SVGProps<SVGSVGElement>) => (
   <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 74 82' {...props}>
