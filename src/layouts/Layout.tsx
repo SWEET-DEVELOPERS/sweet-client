b9eb56c292d314378153b620709faf78aca0b97b
urjimyu <92876819+urjimyu@users.noreply.github.com>
2024-01-12 14:18:28 +0900

    feat: haeder, footer 제거

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,16 +1,12 @@
 import { useMobile } from '../hooks/useMobile';
 import { Outlet } from 'react-router-dom';
-import Footer from '../components/common/Footer';
-import Header from '../components/common/Header';
 
 export default function Layout() {
   useMobile();
 
   return (
     <>
-      <Header />
       <Outlet />
-      <Footer />
     </>
   );
 }
