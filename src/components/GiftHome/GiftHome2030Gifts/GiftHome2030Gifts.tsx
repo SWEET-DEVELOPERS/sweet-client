756c1ff532df2c5a17733ff1cbdb9b7d0077c175
urjimyu <92876819+urjimyu@users.noreply.github.com>
2024-01-13 00:03:18 +0900

    feat: 컴포넌트 분리

--- src/components/GiftHome/GiftHome2030Gifts/GiftHome2030Gifts.tsx
+++ src/components/GiftHome/GiftHome2030Gifts/GiftHome2030Gifts.tsx
@@ -1,41 +1,29 @@
-import React from 'react';
-import * as S from '../GiftHomeShowcase.styled';
+import * as S from '../common/GiftHomeShowcase.styled';
+import GiftHome2030Item from './GiftHome2030Item';
 
-export default function GiftHome2030Gifts() {
-  const isGift = true;
-  const price = 42000;
-  const user = '강국희';
+interface GiftHome2030GiftsProps {
+  data: {
+    price: string;
+    title: string;
+    user: string;
+    imgUrl: string;
+  }[];
+}
 
+export default function GiftHome2030Gifts({ data }: GiftHome2030GiftsProps) {
   return (
     <S.GiftHomeShowcaseWrapper>
       {/* 공통 헤더 넣을 예정 */}
-      {isGift ? (
+      {data.length > 0 ? (
         <S.GiftsWrapper>
-          <S.GiftsItemWrapper>
-            <S.GiftsItemImage></S.GiftsItemImage>
-            <S.GiftsItemTitle>오로라 레터링 케이크(소)</S.GiftsItemTitle>
-            <S.GiftsItemPrice>{price}원</S.GiftsItemPrice>
-          </S.GiftsItemWrapper>
-          <S.GiftsItemWrapper>
-            <S.GiftsItemImage></S.GiftsItemImage>
-            <S.GiftsItemTitle>오로라 레터링 케이크(소)</S.GiftsItemTitle>
-            <S.GiftsItemPrice>{price}원</S.GiftsItemPrice>
-          </S.GiftsItemWrapper>
-          <S.GiftsItemWrapper>
-            <S.GiftsItemImage></S.GiftsItemImage>
-            <S.GiftsItemTitle>오로라 레터링 케이크(소)</S.GiftsItemTitle>
-            <S.GiftsItemPrice>{price}원</S.GiftsItemPrice>
-          </S.GiftsItemWrapper>
-          <S.GiftsItemWrapper>
-            <S.GiftsItemImage></S.GiftsItemImage>
-            <S.GiftsItemTitle>오로라 레터링 케이크(소)</S.GiftsItemTitle>
-            <S.GiftsItemPrice>{price}원</S.GiftsItemPrice>
-          </S.GiftsItemWrapper>
-          <S.GiftsItemWrapper>
-            <S.GiftsItemImage></S.GiftsItemImage>
-            <S.GiftsItemTitle>오로라 레터링 케이크(소)</S.GiftsItemTitle>
-            <S.GiftsItemPrice>{price}원</S.GiftsItemPrice>
-          </S.GiftsItemWrapper>
+          {data.map((gift, index) => (
+            <GiftHome2030Item
+              key={index}
+              imgUrl={gift.imgUrl}
+              title={gift.title}
+              price={gift.price}
+            />
+          ))}
         </S.GiftsWrapper>
       ) : (
         <S.NoGiftsWrapper>아직 선물이 등록되지 않았어요</S.NoGiftsWrapper>
--- src/components/GiftHome/GiftHomeFriendsGifts/GiftHomeFriendsGifts.tsx
+++ src/components/GiftHome/GiftHomeFriendsGifts/GiftHomeFriendsGifts.tsx
@@ -1,45 +1,30 @@
-import * as S from '../GiftHomeShowcase.styled';
+import * as S from '../common/GiftHomeShowcase.styled';
+import GiftHomeFriendsItem from './GiftHomeFriendsItem';
 
-export default function GiftHomeFriendsGifts() {
-  const isGift = true;
-  const price = 42000;
-  const user = '강국희';
+interface GiftHomeFriendsGiftsProps {
+  data: {
+    price: string;
+    title: string;
+    user: string;
+    imgUrl: string;
+  }[];
+}
 
+export default function GiftHomeFriendsGifts({ data }: GiftHomeFriendsGiftsProps) {
   return (
     <S.GiftHomeShowcaseWrapper>
       {/* 공통 헤더 넣을 예정 */}
-      {isGift ? (
+      {data.length > 0 ? (
         <S.GiftsWrapper>
-          <S.GiftsItemWrapper>
-            <S.GiftsItemImage></S.GiftsItemImage>
-            <S.GiftsItemTitle>오로라 레터링 케이크(소)</S.GiftsItemTitle>
-            <S.GiftsItemPrice>{price}원</S.GiftsItemPrice>
-            <S.GiftsItemUser>{user}</S.GiftsItemUser>
-          </S.GiftsItemWrapper>
-          <S.GiftsItemWrapper>
-            <S.GiftsItemImage></S.GiftsItemImage>
-            <S.GiftsItemTitle>오로라 레터링 케이크(소)</S.GiftsItemTitle>
-            <S.GiftsItemPrice>{price}원</S.GiftsItemPrice>
-            <S.GiftsItemUser>{user}</S.GiftsItemUser>
-          </S.GiftsItemWrapper>
-          <S.GiftsItemWrapper>
-            <S.GiftsItemImage></S.GiftsItemImage>
-            <S.GiftsItemTitle>오로라 레터링 케이크(소)</S.GiftsItemTitle>
-            <S.GiftsItemPrice>{price}원</S.GiftsItemPrice>
-            <S.GiftsItemUser>{user}</S.GiftsItemUser>
-          </S.GiftsItemWrapper>
-          <S.GiftsItemWrapper>
-            <S.GiftsItemImage></S.GiftsItemImage>
-            <S.GiftsItemTitle>오로라 레터링 케이크(소)</S.GiftsItemTitle>
-            <S.GiftsItemPrice>{price}원</S.GiftsItemPrice>
-            <S.GiftsItemUser>{user}</S.GiftsItemUser>
-          </S.GiftsItemWrapper>
-          <S.GiftsItemWrapper>
-            <S.GiftsItemImage></S.GiftsItemImage>
-            <S.GiftsItemTitle>오로라 레터링 케이크(소)</S.GiftsItemTitle>
-            <S.GiftsItemPrice>{price}원</S.GiftsItemPrice>
-            <S.GiftsItemUser>{user}</S.GiftsItemUser>
-          </S.GiftsItemWrapper>
+          {data.map((gift, index) => (
+            <GiftHomeFriendsItem
+              key={index}
+              imgUrl={gift.imgUrl}
+              title={gift.title}
+              price={gift.price}
+              user={gift.user}
+            />
+          ))}
         </S.GiftsWrapper>
       ) : (
         <S.NoGiftsWrapper>아직 선물이 등록되지 않았어요</S.NoGiftsWrapper>
