388b57259ccf4fce8bf2567211fa2d24aa33cda2
imeureka <lky062@naver.com>
2024-01-11 21:21:39 -0800

    style: 공통 priceTag 적용

--- src/components/tournament/tournamentFlow/tournamentCard/TournamentCard.tsx
+++ src/components/tournament/tournamentFlow/tournamentCard/TournamentCard.tsx
@@ -2,13 +2,10 @@ import { useState } from 'react';
 import exampleItemImg from '../../../../assets/img/Rectangle.png';
 import * as S from './TournamentCard.style';
 import { Logo } from '../../../../assets/svg';
+import PriceTag from '../../../common/title/Price/PriceTag';
 
 const TournamentCard = () => {
   const [isClicked, setIsClicked] = useState(false);
-
-  const formatPrice = (price: number) => {
-    return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-  };
 
   const handleClick = () => {
     setIsClicked(!isClicked);
@@ -26,7 +23,7 @@ const TournamentCard = () => {
 
         <S.ItemInfo>
           <S.Title>임영웅 콘서트 Gold열</S.Title>
-          <S.Price>{formatPrice(8130000)}원</S.Price>
+          <PriceTag price={8130000} />
           {/* 보러가기 컴포넌트 분리할까 말까 링크있는 
     아이템이면 생성 조건 렌더링 */}
         </S.ItemInfo>
@@ -36,5 +33,3 @@ const TournamentCard = () => {
 };
 
 export default TournamentCard;
-
-
