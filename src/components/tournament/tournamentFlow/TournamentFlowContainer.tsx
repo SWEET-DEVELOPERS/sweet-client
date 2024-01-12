3b8d7fe31f1cefd95c3f7acd4e0e3ce049ca83a6
imeureka <lky062@naver.com>
2024-01-12 00:17:29 -0800

    fix: 토너먼트 컨테이너 충동 해결

--- src/components/tournament/tournamentFlow/TournamentFlowContainer.tsx
+++ src/components/tournament/tournamentFlow/TournamentFlowContainer.tsx
@@ -1,9 +1,15 @@
 import TournamentCard from './TournamentCard/TournamentCard';
+import { TournamentCardWrapper } from './TournamentFlowContainer.style';
 import TournamentFooter from './TournamentFooter/TournamentFooter';
+import TournamentTitle from './TournamentTitle/TournamentTitle';
+
 const TournamentFlowContainer = () => {
   return (
     <>
-      <TournamentCard />
+      <TournamentTitle />
+      <TournamentCardWrapper>
+        <TournamentCard /> <TournamentCard />
+      </TournamentCardWrapper>
       <TournamentFooter />
     </>
   );
