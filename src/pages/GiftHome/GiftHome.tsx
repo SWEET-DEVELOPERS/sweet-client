f6cea64758921fe45c44daa3719338a4b02c55b2
urjimyu <92876819+urjimyu@users.noreply.github.com>
2024-01-14 02:14:29 +0900

    feat: 플로팅 버튼 구현

--- src/pages/GiftHome/GiftHome.tsx
+++ src/pages/GiftHome/GiftHome.tsx
@@ -1,10 +1,14 @@
+import { useNavigate } from 'react-router-dom';
 import GiftHome2030Gifts from '../../components/GiftHome/GiftHome2030Gifts/GiftHome2030Gifts';
 import GiftHomeFriendsGifts from '../../components/GiftHome/GiftHomeFriendsGifts/GiftHomeFriendsGifts';
 import GiftHomeHeader from '../../components/GiftHome/GiftHomeHeader/GiftHomeHeader';
 import GiftHomeSummary from '../../components/GiftHome/GiftHomeSummary/GiftHomeSummary';
+import BtnFill from '../../components/common/Button/Cta/fill/BtnFill';
 import * as S from './GiftHome.styled';
 
 export default function GiftHome() {
+  const navigate = useNavigate();
+
   const userData = {
     friendsNumber: 42,
     giftOwner: '가나다라마바사아자차',
@@ -38,12 +42,21 @@ export default function GiftHome() {
     },
   ];
 
+  const handleClickBtn = () => {
+    navigate('/addGift');
+  };
+
   return (
     <S.GiftHomeWrapper>
-      <GiftHomeHeader/>
+      <GiftHomeHeader />
       <GiftHomeSummary data={userData} />
       <GiftHomeFriendsGifts data={exampleData} />
       <GiftHome2030Gifts data={exampleData} />
+      <BtnFill
+        children={'선물 등록하기'}
+        onClick={handleClickBtn}
+        customStyle={{ position: 'fixed', bottom: '0', marginBottom: '2rem' }}
+      />
     </S.GiftHomeWrapper>
   );
 }
