988d3cd8bc546358be87dcfd222bc64e70895f2c
장정안 <wjddks96@naver.com>
2024-01-14 15:06:26 +0900

    feat: step04 -05 props 내려 받기

--- src/pages/OnBoardingPage.tsx
+++ src/pages/OnBoardingPage.tsx
@@ -6,18 +6,18 @@ import { useFunnel } from '../hooks/useFunnel';
 import NameInput from '../components/OnBoardingSteps/Step01/Step01';
 import ThumbnailInput from '../components/OnBoardingSteps/Step02/Step02';
 import GiftDelivery from '../components/OnBoardingSteps/Step03/Step03';
-import SetTournamentSchedule from '../components/OnBoardingSteps/Step04/Step04';
 import SetTournamentDuration from '../components/OnBoardingSteps/Step05/Step05';
 import OnboardingFinal from '../components/OnBoardingSteps/Step06/Step06';
 import { useState } from 'react';
+import SetTournamentSchedule from '../components/OnBoardingSteps/Step04/Step04';
 
 const OnBoardingPage = () => {
   const { Funnel, setStep } = useFunnel(ONBOARDING_FORM_STEP, ONBOARDING_FORM_STEP[0]);
   const [gifteeName, setGifteeName] = useState<string>('');
   const [imageUrl, setImageUrl] = useState<string>('');
   const [deliveryDate, setDeliveryDate] = useState<string>('');
-  const [tournamentStartDate, SetTournamentStartDate] = useState<string>('');
-  const [tournamentDurantion, setTournamentDurantion] = useState<string>('');
+  const [tournamentStartDate, setTournamentStartDate] = useState<string>('');
+  const [tournamentDuration, setTournamentDuration] = useState<string>('');
 
   return (
     <OnBoardingPageWrapper>
@@ -52,12 +52,16 @@ const OnBoardingPage = () => {
 
         <Funnel.Step name='TOURNAMENT_SCHEDULE_REGISTRATION'>
           <div style={{ margin: '2rem' }}>
-            <SetTournamentSchedule onNext={() => setStep(() => 'TOURNAMENT_PROCEEDING')} />/
+            <SetTournamentSchedule onNext={() => setStep(() => 'TOURNAMENT_PROCEEDING')} />
           </div>
         </Funnel.Step>
         <Funnel.Step name='TOURNAMENT_PROCEEDING'>
           <div style={{ margin: '2rem' }}>
-            <SetTournamentDuration onNext={() => setStep(() => 'GIFT_ROOM_FIX')} />
+            <SetTournamentDuration
+              onNext={() => setStep(() => 'GIFT_ROOM_FIX')}
+              tournamentDuration={tournamentDuration}
+              setTournamentDuration={setTournamentDuration}
+            />
           </div>
         </Funnel.Step>
         <Funnel.Step name='GIFT_ROOM_FIX'>
