01250abf906365db576910eab85028065752bf59
imeureka <lky062@naver.com>
2024-01-15 05:34:53 -0800

    feat: 헤더 삭제

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,13 +1,11 @@
 import { useMobile } from '../hooks/useMobile';
 import { Outlet } from 'react-router-dom';
-import Header from '../components/common/Header';
 
 export default function Layout() {
   useMobile();
 
   return (
     <>
-      <Header />
       <Outlet />
     </>
   );
