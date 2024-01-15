ca2e4571ea8acc177c0add80b0ce70075726b022
Wang HoEun <hoeunwang@gmail.com>
2024-01-15 23:56:20 +0900

    feat: data 존재 여부에 따른 view 전환

--- src/pages/MyPage/GiftRoomView/DoneGiftView/DoneGiftView.tsx
+++ src/pages/MyPage/GiftRoomView/DoneGiftView/DoneGiftView.tsx
@@ -1,19 +1,25 @@
 import DoneCardRoom from '../../../../components/CardRoom/DoneCardRoom';
 import GiftRoomHeader from '../GiftRoomHeader/GiftRoomHeader';
+import { ClosedRoomType } from '../../../../types/user';
 import * as S from './DoneGiftView.style';
 
-const DoneGiftView = () => {
-  const giftData: boolean = true;
+interface DoneGiftViewType {
+  data: ClosedRoomType | undefined;
+}
+
+const DoneGiftView = ({ data }: DoneGiftViewType) => {
+  //console.log(data[0]);
+
   return (
     <S.DoneGiftViewWrapper>
       <GiftRoomHeader name='종료된 선물방' page='detail-done' />
-      {giftData ? (
-        <S.RoomWrapper>
-          <DoneCardRoom />
-        </S.RoomWrapper>
-      ) : (
-        <S.NoneRoom>준비했던 선물이 없어요</S.NoneRoom>
-      )}
+      <S.RoomWrapper>
+        {!data ? (
+          <>{/* <DoneCardRoom user={data.done} imgSrc= userCount/> */}</>
+        ) : (
+          <S.NoneRoom>준비했던 선물이 없어요</S.NoneRoom>
+        )}
+      </S.RoomWrapper>
     </S.DoneGiftViewWrapper>
   );
 };
--- src/pages/MyPage/GiftRoomView/ProgressGiftView/ProgressGiftView.tsx
+++ src/pages/MyPage/GiftRoomView/ProgressGiftView/ProgressGiftView.tsx
@@ -2,20 +2,27 @@ import CardRoom from '../../../../components/CardRoom/ProgressCardRoom';
 import EditCardRoom from '../../../../components/CardRoom/EditCardRoom';
 import GiftRoomHeader from '../GiftRoomHeader/GiftRoomHeader';
 import * as S from './ProgressGiftView.style';
+import { ActiveRoomType } from '../../../../types/user';
 
-const ProgressGiftView = () => {
-  const giftData: boolean = true;
+interface ProgressGiftViewType {
+  data: ActiveRoomType | undefined;
+}
+
+const ProgressGiftView = ({ data }: ProgressGiftViewType) => {
+  //const giftData: boolean = true;
   return (
     <S.ProgressGiftViewWrapper>
       <GiftRoomHeader name='진행중인 선물방' page='detail-progress' />
-      {giftData ? (
-        <S.RoomWrapper>
-          <CardRoom />
-          <EditCardRoom />
-        </S.RoomWrapper>
-      ) : (
-        <S.NoneRoom>지금 진행 중인 선물방이 없어요</S.NoneRoom>
-      )}
+      <S.RoomWrapper>
+        {!data ? (
+          <>
+            <CardRoom />
+            <EditCardRoom />
+          </>
+        ) : (
+          <S.NoneRoom>지금 진행 중인 선물방이 없어요</S.NoneRoom>
+        )}
+      </S.RoomWrapper>
     </S.ProgressGiftViewWrapper>
   );
 };
