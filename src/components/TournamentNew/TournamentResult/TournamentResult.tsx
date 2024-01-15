75121423574033cc6456ab4decc06b97ea5f5331
imeureka <lky062@naver.com>
2024-01-15 04:06:22 -0800

    feat: 결과 제출 프롭 설정

--- src/components/TournamentNew/TournamentResult/TournamentResult.tsx
+++ src/components/TournamentNew/TournamentResult/TournamentResult.tsx
@@ -1,13 +1,20 @@
-import TournamentResultCard from './TournamentResultCard/TournamentResultCard';
-import TournamentResultFooter from './TournamentResultFooter/TournamentResultFooter';
-import TournamentResultUser from './TournamentResultUser/TournamentResultUser';
+// TournamentResult.tsx
+import React from 'react';
 import TournamentTitle from './TournamentTitle/TournamentTitle';
+import TournamentResultCard from './TournamentResultCard/TournamentResultCard';
+import TournamentResultUser from './TournamentResultUser/TournamentResultUser';
+import TournamentResultFooter from './TournamentResultFooter/TournamentResultFooter';
+import { GiftData } from '../../../core/mockupData';
 
-const TournamentResult = () => {
+interface TournamentResultProps {
+  winners: GiftData[];
+}
+
+const TournamentResult: React.FC<TournamentResultProps> = ({ winners }) => {
   return (
     <>
       <TournamentTitle />
-      <TournamentResultCard />
+      <TournamentResultCard  />
       <TournamentResultUser />
       <TournamentResultFooter />
     </>
