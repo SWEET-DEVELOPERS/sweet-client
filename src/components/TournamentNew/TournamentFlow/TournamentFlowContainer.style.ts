c39f7e5688b993f7f2e8ce192afc1a15afe4c958
imeureka <lky062@naver.com>
2024-01-15 05:33:38 -0800

    chore: 배경 추가

--- src/components/TournamentNew/TournamentFlow/TournamentFlowContainer.style.ts
+++ src/components/TournamentNew/TournamentFlow/TournamentFlowContainer.style.ts
@@ -1,7 +1,14 @@
 import styled from 'styled-components';
-
+import bgGround from '../../../assets/img/tournament_bg_03.png';
 export const TournamentCardWrapper = styled.div`
   display: flex;
   justify-content: center;
   gap: 1.5rem;
+`;
+export const TournamentFlowContainerWrapper = styled.section`
+  background-image: url(${bgGround});
+  background-size: cover;
+  background-repeat: no-repeat;
+
+  height: 100vh;
 `;
