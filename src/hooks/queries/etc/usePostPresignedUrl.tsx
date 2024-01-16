4c721ef2a1b9bb7dc66e332f74202a06586d7a4b
장정안 <wjddks96@naver.com>
2024-01-16 16:27:41 +0900

    feat: presignedUrl post 커스텀 훅 작성

---
+++ src/hooks/queries/etc/usePostPresignedUrl.tsx
@@ -0,0 +1,27 @@
+import { AxiosError, AxiosResponse } from 'axios';
+import { post } from '../../../apis/client';
+import { useMutation } from '@tanstack/react-query';
+
+const postPresignedUrl = async (url: string) => {
+  try {
+    const response: AxiosResponse = await post('/presigned-url', url);
+    return response.data;
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      throw new Error(error.response?.data?.message ?? error.message);
+    } else {
+      throw error;
+    }
+  }
+};
+
+const usePostPresignedUrl = () => {
+  return useMutation({
+    mutationFn: postPresignedUrl,
+    onSuccess: (data) => {
+      console.log('preSignedUrl 포스트 성공', data);
+    },
+  });
+};
+
+export default usePostPresignedUrl;
