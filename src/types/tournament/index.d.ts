ca8252d59c05497531460f19cd2867bc3c0f5e37
imeureka <lky062@naver.com>
2024-01-16 05:44:16 -0800

    feat: 토너먼트 선물 리스트 조회 API

--- src/types/tournament/index.d.ts
+++ src/types/tournament/index.d.ts
@@ -1,9 +1,7 @@
 export type GiftData = {
-  data: {
-    giftId: number;
-    imageUrl: string;
-    name: string;
-    cost: number;
-    url: string;
-  };
+  giftId: number;
+  imageUrl: string;
+  name: string;
+  cost: number;
+  url: string;
 };
