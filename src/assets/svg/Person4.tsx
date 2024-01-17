b635179958a85839dd6582af740cd2f17b470227
Wang HoEun <hoeunwang@gmail.com>
2024-01-18 00:38:58 +0900

    fix: 안 쓰는 import 문 삭제

--- src/assets/svg/Person4.tsx
+++ src/assets/svg/Person4.tsx
@@ -1,4 +1,3 @@
-import * as React from 'react';
 import type { SVGProps } from 'react';
 const SvgPerson4 = (props: SVGProps<SVGSVGElement>) => (
   <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 225 66' {...props}>
