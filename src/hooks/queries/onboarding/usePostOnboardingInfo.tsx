0a39c4336a6192b26eb4ed585b11885420ad0bd0
장정안 <wjddks96@naver.com>
2024-01-18 07:57:13 +0900

    chore: 필요없는 코드 제거

--- src/hooks/queries/onboarding/usePostOnboardingInfo.tsx
+++ src/hooks/queries/onboarding/usePostOnboardingInfo.tsx
@@ -2,7 +2,6 @@ import { useMutation } from '@tanstack/react-query';
 import { post } from '../../../apis/client';
 import { OnboardingInfo } from '../../../types/Onboarding';
 import { AxiosResponse } from 'axios';
-import { useState } from 'react';
 
 const postOnboardingInfo = async (onboardingInfo: OnboardingInfo) => {
   const response: AxiosResponse = await post(`/room`, onboardingInfo);
@@ -10,17 +9,12 @@ const postOnboardingInfo = async (onboardingInfo: OnboardingInfo) => {
 };
 
 const usePostOnboardingInfo = () => {
-  // const [invitationCode, setInvitationCode] = useState<string>('');
-  //   const queryClient = useQueryClient();
   const mutation = useMutation({
     mutationFn: postOnboardingInfo,
     onSuccess: (data) => {
       console.log('포스트 성공?', data);
       const code = (data as any).invitationCode;
       console.log('커스텀 훅 내 초대코드', code);
-
-      // setInvitationCode(code);
-      // queryClient.invalidateQueries('rooms');
     },
   });
 
