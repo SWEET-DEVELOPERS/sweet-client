364acef34f2c9045b74be922c368cd5324d96846
장정안 <wjddks96@naver.com>
2024-01-18 01:14:35 +0900

    feat: POST 커스텀 훅 내에 useState 추가

--- src/hooks/queries/onboarding/usePostOnboardingInfo.tsx
+++ src/hooks/queries/onboarding/usePostOnboardingInfo.tsx
@@ -2,6 +2,7 @@ import { useMutation } from '@tanstack/react-query';
 import { post } from '../../../apis/client';
 import { OnboardingInfo } from '../../../types/Onboarding';
 import { AxiosError, AxiosResponse } from 'axios';
+import { useState } from 'react';
 const postOnboardingInfo = async (onboardingInfo: OnboardingInfo): Promise<AxiosResponse> => {
   try {
     const response: AxiosResponse = await post(`/room`, onboardingInfo);
@@ -15,16 +16,20 @@ const postOnboardingInfo = async (onboardingInfo: OnboardingInfo): Promise<Axios
   }
 };
 const usePostOnboardingInfo = () => {
+  const [invitationCode, setInvitationCode] = useState<string>('');
   //   const queryClient = useQueryClient();
-  return useMutation({
+  const mutation = useMutation({
     mutationFn: postOnboardingInfo,
     onSuccess: (data) => {
       console.log('포스트 성공?', data);
-      const invitationCode = (data as any).invitationCode;
-      console.log('초대코드', invitationCode);
+      const code = (data as any).invitationCode;
+      console.log('커스텀 훅 내 초대코드', code);
 
+      setInvitationCode(code);
       // queryClient.invalidateQueries('rooms');
     },
   });
+
+  return { ...mutation, invitationCode };
 };
 export default usePostOnboardingInfo;
