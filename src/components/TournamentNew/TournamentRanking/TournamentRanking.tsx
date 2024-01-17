40f4f35ab08bbbcd3f044340a13ae6f281bc6ebd
imeureka <lky062@naver.com>
2024-01-17 10:44:25 -0800

    feat: API GET 메달 랭킹에 따라 변경

--- src/components/TournamentNew/TournamentRanking/TournamentRanking.tsx
+++ src/components/TournamentNew/TournamentRanking/TournamentRanking.tsx
@@ -1,25 +1,46 @@
 import TournamentRankingCard from './TournamentRankingCard/TournamentRankingCard';
 import TournamentRankingTitle from './TournamentRankingTitle/TournamentRankingTitle';
 import { RankingWrapper } from './TournamentRanking.style';
-import { IcMedal1 } from '../../../assets/svg';
+import { IcMedal1, IcMedal2, IcMedal3, IcMedal4 } from '../../../assets/svg';
+import styled from 'styled-components';
 import useGetRanking from '../../../hooks/queries/tournament/useGetRanking';
 
-
 const TournamentRanking = () => {
+  const rankingData = useGetRanking({ roomId: 2 });
+  const tournamentRankingData = rankingData?.data || [];
 
-  const RankingData = useGetRanking({ roomId: 2 });
-  console.log(RankingData);
-
- const tournamentRankingData = RankingData?.data || [];
   return (
     <>
       <TournamentRankingTitle />
       <RankingWrapper>
-        <IcMedal1 style={{ width: '2.5rem' }} />
-        <TournamentRankingCard RankingData={tournamentRankingData} />
+        <Wrapper>
+          {tournamentRankingData.map((data, index) => (
+            <WrapperItem key={index}>
+              {data.ranking === 1 && <IcMedal1 style={{ width: '2.5rem' }} />}
+              {data.ranking === 2 && <IcMedal2 style={{ width: '2.5rem' }} />}
+              {data.ranking === 3 && <IcMedal3 style={{ width: '2.5rem' }} />}
+              {data.ranking !== 1 && data.ranking !== 2 && data.ranking !== 3 && (
+                <IcMedal4 style={{ width: '2.5rem' }} />
+              )}
+              <TournamentRankingCard name={data.name} cost={data.cost} imageUrl={data.imageUrl} />
+            </WrapperItem>
+          ))}
+        </Wrapper>
       </RankingWrapper>
     </>
   );
 };
 
 export default TournamentRanking;
+
+const Wrapper = styled.div`
+  display: inline-flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 2.4rem;
+`;
+
+const WrapperItem = styled.div`
+  display: flex;
+  gap: 2.4rem;
+`;
