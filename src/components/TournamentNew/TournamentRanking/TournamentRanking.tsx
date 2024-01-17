0613d254c0414bdccc2ad94d84450a078200ff43
imeureka <lky062@naver.com>
2024-01-17 11:02:19 -0800

    feat: 메달 인덱스 추가

--- src/components/TournamentNew/TournamentRanking/TournamentRanking.tsx
+++ src/components/TournamentNew/TournamentRanking/TournamentRanking.tsx
@@ -20,7 +20,10 @@ const TournamentRanking = () => {
               {data.ranking === 2 && <IcMedal2 style={{ width: '2.5rem' }} />}
               {data.ranking === 3 && <IcMedal3 style={{ width: '2.5rem' }} />}
               {data.ranking !== 1 && data.ranking !== 2 && data.ranking !== 3 && (
-                <IcMedal4 style={{ width: '2.5rem' }} />
+                <IconWrapper>
+                  <IcMedal4 style={{ width: '2.5rem' }} />
+                  <IcTitle>{data.ranking}</IcTitle>
+                </IconWrapper>
               )}
               <TournamentRankingCard name={data.name} cost={data.cost} imageUrl={data.imageUrl} />
             </WrapperItem>
@@ -38,6 +41,25 @@ const Wrapper = styled.div`
   flex-direction: column;
   align-items: flex-start;
   gap: 2.4rem;
+`;
+
+const IcTitle = styled.p`
+  text-align: center;
+  position: absolute;
+  font-family: 'SUIT';
+  color: #ffffff;
+  text-align: center;
+  font-family: SUIT;
+  font-size: 14px;
+  left: 50%;
+  font-weight: 700;
+  top: 9%;
+  transform: translateX(-50%);
+`;
+
+const IconWrapper = styled.div`
+  position: relative;
+  top: 30%;
 `;
 
 const WrapperItem = styled.div`
