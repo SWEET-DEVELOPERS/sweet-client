8a4a0c1b8df4c338b8bb6f331d94b4ca57923646
imeureka <lky062@naver.com>
2024-01-17 01:10:13 -0800

    feat: modal 오픈

--- src/components/TournamentNew/TournamentResult/TournamentResult.tsx
+++ src/components/TournamentNew/TournamentResult/TournamentResult.tsx
@@ -6,6 +6,8 @@ import * as S from './TournamentResult.sytle';
 import { GiftData } from '../../../core/mockupData';
 import usePostScore from '../../../hooks/queries/tournament/usePostScore';
 import useGetTournamentUser from '../../../hooks/queries/tournament/useGetTournamentUser';
+import { useState } from 'react';
+import Modal from '../../common/Modal/Modal';
 
 interface TournamentResultProps {
   winners: GiftData | null;
@@ -22,11 +24,25 @@ const TournamentResult: React.FC<TournamentResultProps> = ({
 }) => {
   const memberData = useGetTournamentUser({ roomId: 2 });
   const tournamentData = memberData?.data || [];
-  console.log('뷰', memberData);
-  console.log('2', tournamentData);
+
+  //버튼 ture
+  const [isButton, setIsButton] = useState(true);
+  //modat state
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   const { mutation } = usePostScore({ body: { firstGiftId, secondGiftId, finalGiftId } });
+
   const footerClickHandler = () => {
+    setIsModalOpen(true);
     mutation.mutate({ firstGiftId, secondGiftId, finalGiftId });
+
+    console.log('모달열림', isModalOpen);
+  };
+
+  const handleConfirmClick = () => {
+    setIsModalOpen(false);
+    setIsButton(false);
+    console.log('모달닫힘', isModalOpen);
   };
 
   return (
@@ -34,7 +50,14 @@ const TournamentResult: React.FC<TournamentResultProps> = ({
       <TournamentTitle />
       <TournamentResultCard item={winners} />
       <TournamentResultUser memberData={tournamentData} />
-      <TournamentResultFooter onClick={footerClickHandler} />
+      {isButton && <TournamentResultFooter onClick={footerClickHandler} />}
+      {isModalOpen && (
+        <Modal onConfirmClick={handleConfirmClick}>
+          선물토너먼트
+          <br />
+          결과가제출되었어요!
+        </Modal>
+      )}
     </S.TournamentResult>
   );
 };
