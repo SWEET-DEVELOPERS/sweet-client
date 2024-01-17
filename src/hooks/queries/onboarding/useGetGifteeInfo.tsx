c97df1382364cfdd05a3fb64df473553f3d0df74
장정안 <wjddks96@naver.com>
2024-01-17 12:50:30 +0900

    feat: Get 커스텀 훅 작성

---
+++ src/hooks/queries/onboarding/useGetGifteeInfo.tsx
@@ -0,0 +1,21 @@
+import { useQuery } from '@tanstack/react-query';
+import { get } from '../../../apis/client';
+import { AxiosResponse } from 'axios';
+
+export const ONBOARDING_INFO_QUERY_KEY: string[] = ['onboardingData'];
+
+export const fetchOnboarding = async (invitationCode: string) => {
+  const response: AxiosResponse = await get(`/room/invitations/${invitationCode}`);
+  return response.data;
+};
+
+const useGetGifteeInfo = (invitationCode: string) => {
+  const { data, isLoading, isError } = useQuery<any, Error>({
+    queryKey: ONBOARDING_INFO_QUERY_KEY,
+    queryFn: () => fetchOnboarding(invitationCode),
+  });
+
+  return { data, isLoading, isError };
+};
+
+export default useGetGifteeInfo;
