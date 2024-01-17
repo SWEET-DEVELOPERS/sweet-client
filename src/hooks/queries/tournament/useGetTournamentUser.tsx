54837a239e1e7d0a5da0b47149cee6347e867a84
imeureka <lky062@naver.com>
2024-01-17 07:40:46 -0800

    fix: useQuery 안쓰는 거 제거

--- src/hooks/queries/tournament/useGetTournamentUser.tsx
+++ src/hooks/queries/tournament/useGetTournamentUser.tsx
@@ -1,4 +1,4 @@
-import { useQuery, useSuspenseQuery } from '@tanstack/react-query';
+import { useSuspenseQuery } from '@tanstack/react-query';
 import { get } from '../../../apis/client';
 import { TournamentUser } from '../../../types/tournament';
 
