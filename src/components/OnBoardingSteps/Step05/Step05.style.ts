ca7b3afd6921cf2ae999f0545a29ef58c04b9f1e
장정안 <126966681+ExceptAnyone@users.noreply.github.com>
2024-01-18 22:03:37 +0900

    [온보딩] 링크 복사 & 선물전달일과 토너먼트 시작 날짜 계산 (#156)
    * feat: step06 링크복사 구현
    
    * feat: useQuery를 useSuspense로 변경
    
    * feat: 선물 전달일 당일인지 아닌지에 따라 다르게 로직 구현(미완)
    
    * style: 라디오 버튼 props 추가
    
    * style: 공통 Title customStyle제거
    
    * chore: 빌드 에러 해결

--- src/components/OnBoardingSteps/Step05/Step05.style.ts
+++ src/components/OnBoardingSteps/Step05/Step05.style.ts
@@ -30,3 +30,8 @@ export const TimeOptionsWrapper = styled.div`
   border-radius: 1.2rem;
   border: 1px solid ${({ theme }) => theme.colors.G_02};
 `;
+
+export const Period = styled.p<{ isAfterDelivery: boolean }>`
+  color: ${({ theme, isAfterDelivery }) =>
+    isAfterDelivery ? theme.colors.R_02 : theme.colors.B_02};
+`;
--- src/components/common/Button/Radio/BtnRadio.tsx
+++ src/components/common/Button/Radio/BtnRadio.tsx
@@ -1,6 +1,7 @@
 import { ButtonHTMLAttributes, useState } from 'react';
 import * as S from './BtnRadio.style';
 import { IcCircle, IcCirclePink } from '../../../../assets/svg';
+import { Period } from '../../../OnBoardingSteps/Step05/Step05.style';
 type BtnRadioProps = ButtonHTMLAttributes<HTMLButtonElement> & {
   disabled?: boolean;
   time: string;
@@ -10,6 +11,7 @@ type BtnRadioProps = ButtonHTMLAttributes<HTMLButtonElement> & {
   isSelected?: () => void;
   onTimeSelect?: (time: string) => void;
   setSelectedTime?: (time: string) => VoidFunction;
+  isAfterDelivery: boolean;
 };
 const BtnRadio = ({
   disabled,
@@ -19,6 +21,7 @@ const BtnRadio = ({
   onClick,
   onTimeSelect,
   setSelectedTime,
+  isAfterDelivery,
 }: BtnRadioProps) => {
   const [focused, setFocused] = useState(false);
 
@@ -41,12 +44,12 @@ const BtnRadio = ({
       onBlur={() => setFocused(false)}
     >
       {focused ? (
-        <IcCirclePink style={{ width: '5.4rem' }} />
+        <IcCirclePink style={{ width: '2.4rem' }} />
       ) : (
-        <IcCircle style={{ width: '5.4rem' }} />
+        <IcCircle style={{ width: '2.4rem' }} />
       )}
       <S.Time>{time}</S.Time>
-      <S.Period>{period}</S.Period>
+      <Period isAfterDelivery={isAfterDelivery}>{period}</Period>
     </S.Wrapper>
   );
 };
