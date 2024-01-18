6b3cf50e834d83a03310dae777fa168b9a76eb3d
urjimyu <92876819+urjimyu@users.noreply.github.com>
2024-01-18 23:50:05 +0900

    feat: POST 성공 후 navigate 에러 해결

--- src/components/GiftAdd/AddGiftFooter/AddGiftFooter.tsx
+++ src/components/GiftAdd/AddGiftFooter/AddGiftFooter.tsx
@@ -47,17 +47,29 @@ const AddGiftFooter = ({
     url: link,
   };
   const navigate = useNavigate();
-  const { mutation } = usePostGift();
+  const { mutation } = usePostGift(itemInfo.roomId, targetDate);
 
   const onClick = async () => {
     const { presignedUrl } = await fetchPresignedUrl(fileName);
-    console.log('푸터 안에서 fileName', fileName);
+    // console.log('푸터 안에서 fileName', fileName);
     await saveImageUrl(presignedUrl);
     setImageUrl(presignedUrl);
     if (isActivated) {
-      mutation.mutate(updatedItemInfo);
-      navigate(`/add-gift?roomId=${itemInfo.roomId}&targetTime=${targetDate}`);
-      navigate(`/add-gift`);
+      mutation.mutate(
+        {
+          roomId: itemInfo.roomId,
+          name,
+          cost,
+          imageUrl,
+          url: link,
+        },
+        {
+          onSuccess: () => {
+            navigate(`/add-gift?roomId=${itemInfo.roomId}&targetTime=${targetDate}`);
+            setStep(0);
+          },
+        },
+      );
     }
   };
 
