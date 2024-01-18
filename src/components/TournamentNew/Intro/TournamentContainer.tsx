f642a5904047193eaa6e4c0318f5888495ed12af
Wang HoEun <hoeunwang@gmail.com>
2024-01-18 23:50:41 +0900

    feat: useLocation 코드 작성

--- src/components/TournamentNew/Intro/TournamentContainer.tsx
+++ src/components/TournamentNew/Intro/TournamentContainer.tsx
@@ -6,11 +6,21 @@ import useTournament from '../../../hooks/tournament/useTournament';
 import TournamentFlowContainer from '../TournamentFlow/TournamentFlowContainer';
 import useGetItem from '../../../hooks/queries/tournament/useGetItem';
 import trophy from '../../../assets/img/3dic_trophy3.png';
+//import { useLocation } from 'react-router';
 
 const TournamentContainer = () => {
   const { showTournamentContainer, handleStartClick } = useTournament();
   const memberData = useGetItem({ roomId: 2 });
   console.log(memberData);
+  // const location = useLocation();
+  // const searchParams = new URLSearchParams(location.search);
+
+  // const roomIdString = searchParams.get('roomId');
+  // const roomId = parseInt(roomIdString || '', 10);
+  // console.log('추출된 초대 코드', roomId);
+  //const roomMemberWholeData = useGetRoomMember(roomId)?.data;
+  // 여기다가 query api 불러오기
+  //console.log(roomMemberWholeData);
 
   // Use an empty array as a fallback if memberData is undefined
   const tournamentData = memberData?.data || [];
