37bc57bdf47dc5315cf2bac42809f2cb78aa45f3
Wang HoEun <hoeunwang@gmail.com>
2024-01-18 23:56:16 +0900

    fix: 로그인 버튼 보이게 함

--- src/pages/Start/Start.tsx
+++ src/pages/Start/Start.tsx
@@ -22,7 +22,7 @@ const Start = () => {
   const REST_API_KEY: string = import.meta.env.VITE_REST_API_KEY;
   const REDIRECT_URI: string = import.meta.env.VITE_REDIRECT_URI || '';
   const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
-  const isLogin: boolean = !localStorage.getItem('EXIT_LOGIN_TOKEN');
+  const isLogin: boolean = !!localStorage.getItem('EXIT_LOGIN_TOKEN');
   //const navigate = useNavigate();
 
   const handleLogin = () => {
