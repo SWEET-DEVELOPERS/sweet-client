ce05767330b84a2355cd2f6950a84a86f69ef112
Wang HoEun <hoeunwang@gmail.com>
2024-01-19 00:14:04 +0900

    fix: null 값 처리 재 배치

--- src/pages/TournamentPage.tsx
+++ src/pages/TournamentPage.tsx
@@ -5,7 +5,7 @@ const TournamentPage = () => {
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
 
-  const giftee = searchParams.get('giftee' || '');
+  const giftee = searchParams.get('giftee') || '';
   const roomIdString = searchParams.get('roomId');
   const roomId = parseInt(roomIdString || '', 10);
   console.log('추출된 초대 코드', roomId);
