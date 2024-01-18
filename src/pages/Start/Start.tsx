bc102a92eeba44c67124a699f0867dced054f57d
Wang HoEun <hoeunwang@gmail.com>
2024-01-18 16:47:59 +0900

    fix: import 삭제

--- src/pages/Start/Start.tsx
+++ src/pages/Start/Start.tsx
@@ -1,4 +1,4 @@
-import { useNavigate } from 'react-router';
+// import { useNavigate } from 'react-router';
 import { IcKakoLarge, Main03, Main04, Person1, Person2, Person3, Person4 } from '../../assets/svg';
 import * as S from './Start.style';
 // import LottieAnimation from '../../hooks/lottie-animation/LottieAnimation';
@@ -6,42 +6,42 @@ import * as S from './Start.style';
 // import { useEffect, useRef, useState } from 'react';
 import { ScrollAnimationContainer } from '../../components/ScrollAnimationContainer/ScrollAnimationContainer';
 import StartHeader from './StartHeader/StartHeader';
-import { post } from '../../apis/client';
+// import { post } from '../../apis/client';
 import Footer from '../../components/Footer/Footer';
 
-interface TokenResponseType {
-  data: {
-    accessToken: string;
-    refreshToken: string;
-  };
-}
+// interface TokenResponseType {
+//   data: {
+//     accessToken: string;
+//     refreshToken: string;
+//   };
+// }
 
 const Start = () => {
   const REST_API_KEY: string = import.meta.env.VITE_REST_API_KEY;
   const REDIRECT_URI: string = import.meta.env.VITE_REDIRECT_URI || '';
   const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
-  const navigate = useNavigate();
+  //const navigate = useNavigate();
 
-  // const handleLogin = () => {
-  //   window.location.replace(kakaoURL);
-  // };
-  const handleClick = () => {
-    const fetchExAuth = (): Promise<TokenResponseType> => post('/member/token/2');
-    fetchExAuth().then((response: TokenResponseType) => {
-      const data = response;
-      const JWT = data.data.accessToken;
-      if (data) {
-        localStorage.setItem('EXIT_LOGIN_TOKEN', JWT);
-        console.log(data);
-      }
-    });
-
-    navigate('/mypage');
+  const handleLogin = () => {
+    window.location.replace(kakaoURL);
   };
+  // const handleClick = () => {
+  //   const fetchExAuth = (): Promise<TokenResponseType> => post('/member/token/2');
+  //   fetchExAuth().then((response: TokenResponseType) => {
+  //     const data = response;
+  //     const JWT = data.data.accessToken;
+  //     if (data) {
+  //       localStorage.setItem('EXIT_LOGIN_TOKEN', JWT);
+  //       console.log(data);
+  //     }
+  //   });
+
+  //   navigate('/mypage');
+  // };
   return (
     <S.Wrapper>
       <StartHeader />
-      <IcKakoLarge onClick={handleClick} />
+      <IcKakoLarge onClick={handleLogin} />
       {/* <S.Animation>
         <LottieAnimation animation={SweetHomeAnimation} customStyle={{ width: '37.5rem' }} />
       </S.Animation> */}
