4ac900bc9298267a0840256ea4022a6a372522e0
Wang HoEun <hoeunwang@gmail.com>
2024-01-18 23:37:09 +0900

    fix: 안쓰는 import 문 삭제

--- src/hooks/queries/room/useGetRoomMember.tsx
+++ src/hooks/queries/room/useGetRoomMember.tsx
@@ -1,4 +1,4 @@
-import { useQueryClient, useSuspenseQuery } from '@tanstack/react-query';
+import { useSuspenseQuery } from '@tanstack/react-query';
 import { get } from '../../../apis/client';
 import { RoomMemberType } from '../../../types/member';
 
