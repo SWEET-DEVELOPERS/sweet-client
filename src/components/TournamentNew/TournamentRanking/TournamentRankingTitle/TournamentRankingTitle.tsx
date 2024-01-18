d773d412b4f535d8a888c177f42b163516d796dc
LEE KAYOUNG / KATIE <104339899+imeureka@users.noreply.github.com>
2024-01-17 21:49:55 -0800

    [토너먼트] 랭킹 화면 API 데이터 패칭 (#140)
    * chore: 아이콘 추가
    
    * feat: API GET 메달 랭킹에 따라 변경
    
    * feat: 메달 인덱스 추가
    
    * feat: router 설정
    
    * feat: 링크 복사 구현
    
    * feat: 이미지 공유하기 (url png 안됨)

--- src/components/TournamentNew/TournamentRanking/TournamentRankingTitle/TournamentRankingTitle.tsx
+++ src/components/TournamentNew/TournamentRanking/TournamentRankingTitle/TournamentRankingTitle.tsx
@@ -1,7 +1,25 @@
 import * as S from './TournamentRankingTitle.style';
 import { SubTitle } from '../../Intro/TournamentStartText/TournamentStartText.style';
 import RankingImg from '../../../../assets/img/3dic_podium2.png';
-const TournamentRenameTitle = () => {
+import { useLocation } from 'react-router-dom';
+
+interface TournamentRankingTitleProps {
+  onClick: () => void;
+}
+
+const TournamentRankingTitle: React.FC<TournamentRankingTitleProps> = ({ onClick }) => {
+  const location = useLocation();
+  console.log(location);
+  const baseURL = import.meta.env.VITE_APP_BASE_URL;
+  const handleCopyClipBoard = async (text: string) => {
+    try {
+      await navigator.clipboard.writeText(text);
+      alert('클립보드에 링크가 복사되었어요.');
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div>
       <S.TournamentRenameTitleWrapper>
@@ -15,11 +33,11 @@ const TournamentRenameTitle = () => {
           최종 선물 순위를 확인하세요
         </SubTitle>
         <S.ButtonWrapper>
-          <S.LinkButton>
+          <S.LinkButton onClick={() => handleCopyClipBoard(`${baseURL}${location.pathname}`)}>
             {/* <IcLinkShare /> */}
             링크로 공유
           </S.LinkButton>
-          <S.ImageButton>
+          <S.ImageButton onClick={onClick}>
             {/* <IcImgShare style={{ width: '1.8rem', height: '1.8rem' }} /> */}
             이미지로 공유
           </S.ImageButton>
@@ -30,4 +48,4 @@ const TournamentRenameTitle = () => {
   );
 };
 
-export default TournamentRenameTitle;
+export default TournamentRankingTitle;
