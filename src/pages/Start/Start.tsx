920a30c421aaf24ce351f6fee765f5daa7725569
Wang HoEun <hoeunwang@gmail.com>
2024-01-20 04:58:04 +0900

    refactor: 시작 페이지 수정

--- src/pages/Start/Start.tsx
+++ src/pages/Start/Start.tsx
@@ -1,6 +1,6 @@
 import { Person1, Person2, Person3, Person4 } from '../../assets/svg';
 import * as S from './Start.style';
-//import { post } from '../../apis/client';
+import { post } from '../../apis/client';
 import StartAnimation from '../../../public/motions/start_motion.json';
 // import { useEffect, useRef, useState } from 'react';
 import { ScrollAnimationContainer } from '../../components/ScrollAnimationContainer/ScrollAnimationContainer';
@@ -12,12 +12,12 @@ import LottieAnimation from '../../hooks/lottie-animation/LottieAnimation';
 import BtnFill from '../../components/common/Button/Cta/fill/BtnFill';
 import { kakaoURL } from '../../utils/login';
 
-// interface TokenResponseType {
-//   data: {
-//     accessToken: string;
-//     refreshToken: string;
-//   };
-// }
+interface TokenResponseType {
+  data: {
+    accessToken: string;
+    refreshToken: string;
+  };
+}
 
 export const isTokenExist = (): boolean => {
   const token = localStorage.getItem('EXIT_LOGIN_TOKEN'); // 'yourTokenKey'는 실제 사용하는 토큰의 키로 대체하세요
@@ -27,23 +27,23 @@ export const isTokenExist = (): boolean => {
 const Start = () => {
   const navigate = useNavigate();
 
-  const handleLogin = () => {
-    window.location.replace(kakaoURL);
-  };
-  // const handleClick = () => {
-  //   const fetchExAuth = (): Promise<TokenResponseType> => post('/member/token/2');
-  //   fetchExAuth().then((response: TokenResponseType) => {
-  //     const data = response;
-  //     const JWT = data.data.accessToken;
-  //     if (data) {
-  //       localStorage.setItem('EXIT_LOGIN_TOKEN', JWT);
-  //       console.log(data);
-  //       console.log(localStorage.getItem('EXIT_LOGIN_TOKEN'));
-  //     }
-  //   });
-
-  //   navigate('/mypage');
+  // const handleLogin = () => {
+  //   window.location.replace(kakaoURL);
   // };
+  const handleClick = () => {
+    const fetchExAuth = (): Promise<TokenResponseType> => post('/member/token/2');
+    fetchExAuth().then((response: TokenResponseType) => {
+      const data = response;
+      const JWT = data.data.accessToken;
+      if (data) {
+        localStorage.setItem('EXIT_LOGIN_TOKEN', JWT);
+        console.log(data);
+        console.log(localStorage.getItem('EXIT_LOGIN_TOKEN'));
+      }
+    });
+
+    navigate('/mypage');
+  };
 
   const goGiftRoom = () => {
     console.log('선물방으로 이동');
@@ -61,7 +61,7 @@ const Start = () => {
       </S.TextWrapper>
       <LottieAnimation
         animation={StartAnimation}
-        customStyle={{ position: 'absolute', top: '-15rem' }}
+        customStyle={{ position: 'absolute', top: '-13rem' }}
       />
       <S.Gradient />
 
@@ -75,12 +75,13 @@ const Start = () => {
             position: 'fixed',
             bottom: '2rem',
             zIndex: '3',
+            width: '33.5rem',
           }}
         >
           새로운 선물 준비하기
         </BtnFill>
       ) : (
-        <S.KakaoLogin onClick={handleLogin} />
+        <S.KakaoLogin onClick={handleClick} />
       )}
       <S.BtnGradient />
 
@@ -97,7 +98,7 @@ const Start = () => {
       </S.PersonWrapper>
       <S.Main3 />
       <S.Main4 />
-      <Footer customStyle={{ position: 'absolute', top: '290rem' }} />
+      <Footer customStyle={{ position: 'absolute', top: '283rem' }} />
     </S.Wrapper>
   );
 };
