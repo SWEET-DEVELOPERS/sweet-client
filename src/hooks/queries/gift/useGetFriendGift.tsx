cd34a64a9dd75fec74613b32f11fd19047bb1014
urjimyu <92876819+urjimyu@users.noreply.github.com>
2024-01-19 22:40:18 +0900

    fix: 빌드 에러 해결

--- src/hooks/queries/gift/useGetFriendGift.tsx
+++ src/hooks/queries/gift/useGetFriendGift.tsx
@@ -1,4 +1,4 @@
-import { useQuery, useSuspenseQuery } from '@tanstack/react-query';
+import { useSuspenseQuery } from '@tanstack/react-query';
 import { get } from '../../../apis/client';
 import { FriendsGiftsInfoType } from '../../../types/room';
 
