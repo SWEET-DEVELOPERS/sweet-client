cb0f976888234ecda9c8ddd7672d085f44221afd
urjimyu <92876819+urjimyu@users.noreply.github.com>
2024-01-19 23:18:09 +0900

    feat: 불필요한 조건문

--- src/components/GiftHome/CountDownTimer/CountDownTimer.tsx
+++ src/components/GiftHome/CountDownTimer/CountDownTimer.tsx
@@ -9,12 +9,6 @@ interface CountdownTimerProps {
 }
 
 const CountdownTimer: React.FC<CountdownTimerProps> = ({ targetDate }) => {
-  const today = new Date();
-  // const navigate = useNavigate();
-  if (targetDate < today) {
-    // 이따가 토너먼트 뷰로 연결!
-    console.log('시간 지남~!');
-  }
   const [days, hours, minutes, seconds] = useCountDown(targetDate.toLocaleString());
   console.log('시간이 어떻게 됐어??? targetDate', targetDate);
   console.log('시간이 어떻게 됐어??? targetDate to localestring', targetDate.toLocaleString);
