b08b8df7606621af44b0ecae0771214b717d4621
장정안 <wjddks96@naver.com>
2024-01-20 05:40:39 +0900

    style: 카카오 링크 zindex 추가

--- src/components/OnBoardingSteps/Step06/Step06.style.ts
+++ src/components/OnBoardingSteps/Step06/Step06.style.ts
@@ -67,7 +67,7 @@ export const InfoDetail = styled.p`
   ${({ theme }) => theme.fonts.body_07};
 `;
 
-export const KakaoLinkCopyBtn = styled.button`
+export const KakaoLinkCopyBtn = styled.div`
   ${({ theme }) =>
     theme.mixin.flexCenter({ direction: 'row', align: 'center', justify: 'center' })};
   width: 16.3rem;
@@ -76,13 +76,15 @@ export const KakaoLinkCopyBtn = styled.button`
   border-radius: 10rem;
   border: none;
   background: #fee500;
-  color: (0, 0, 0, 0.85);
+  color: ${({ theme }) => theme.colors.black};
   ${({ theme }) => theme.fonts.body_09};
+  z-index: 1;
 `;
 
 export const BtnWrapper = styled.div`
   ${({ theme }) =>
     theme.mixin.flexCenter({ direction: 'row', align: 'center', justify: 'center' })};
+  color: ${({ theme }) => theme.colors.black};
   gap: 1.2rem;
   width: 100%;
   margin-top: 1.7rem;
