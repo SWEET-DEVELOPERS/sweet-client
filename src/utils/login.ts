d6df1896e06874059512000e7e676adc15a27466
Wang HoEun <hoeunwang@gmail.com>
2024-01-19 18:36:35 +0900

    fix: type부분 분류 + refresh 토큰

---
+++ src/utils/login.ts
@@ -0,0 +1,3 @@
+export const REST_API_KEY: string = import.meta.env.VITE_REST_API_KEY;
+export const REDIRECT_URI: string = import.meta.env.VITE_REDIRECT_URI || '';
+export const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
