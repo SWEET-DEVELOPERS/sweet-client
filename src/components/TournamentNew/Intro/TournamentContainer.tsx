8b1a5ff4b2eff23edaa89345c05e71657cefdb71
imeureka <lky062@naver.com>
2024-01-19 07:30:14 -0800

    fix: 토너먼트 예외처리

--- src/components/TournamentNew/Intro/TournamentContainer.tsx
+++ src/components/TournamentNew/Intro/TournamentContainer.tsx
@@ -7,14 +7,14 @@ import TournamentFlowContainer from '../TournamentFlow/TournamentFlowContainer';
 import useGetItem from '../../../hooks/queries/tournament/useGetItem';
 import trophy from '../../../assets/img/3dic_trophy3.png';
 import { useParams } from 'react-router';
+import Header from '../../common/Header';
+import { TrophyNone } from '../../../assets/svg';
+import TournamentNoneText from './TournamentNoneText/TournamentNoneText';
 
 const TournamentContainer = () => {
   const params = useParams();
-
-
   const giftee = params.giftee;
-
-  const roomIdString = params.roomId;
+  const roomIdString = params.roomId || '';
   const roomId = parseInt(roomIdString || '', 10);
   console.log('추출된 초대 코드', roomId);
 
@@ -27,15 +27,31 @@ const TournamentContainer = () => {
     <>
       {showTournamentContainer ? (
         <>
-          <TournamentStartText giftee={giftee} />
-          <TournamentItemCount memberData={[]} />
-          <S.TournamentImg>
-            <img src={trophy} alt='트로피' />
-          </S.TournamentImg>
-          <TournamentStartButton onClick={handleStartClick} />
+          <Header />
+          <S.TournamentFlowWrapper>
+            <TournamentStartText giftee={giftee} />
+            <TournamentItemCount memberData={tournamentData} />
+            <S.TournamentImg>
+              <img src={trophy} alt='트로피' />
+            </S.TournamentImg>
+            <TournamentStartButton onClick={handleStartClick} />
+          </S.TournamentFlowWrapper>
+        </>
+      ) : memberData ? (
+        <>
+          <Header />
+          <TournamentFlowContainer memberData={tournamentData} />
         </>
       ) : (
-        <TournamentFlowContainer memberData={tournamentData} />
+        <>
+          <Header />
+          <S.TournamentFlowWrapper>
+            <TournamentNoneText />
+            <S.TournamentImg>
+              <TrophyNone />
+            </S.TournamentImg>
+          </S.TournamentFlowWrapper>
+        </>
       )}
     </>
   );
