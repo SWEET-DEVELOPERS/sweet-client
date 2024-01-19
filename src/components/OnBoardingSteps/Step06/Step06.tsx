3ae8fd22524c2670aa5a51e1627bad0ac01edd47
장정안 <126966681+ExceptAnyone@users.noreply.github.com>
2024-01-19 23:05:02 +0900

    [온보딩] qa 중  (#239)
    * feat: 카카오 공유하기 시 초대코드로 이동
    
    * feat: imageUrl 빈 문자열로 변경해서 POST
    
    * chore: 빌드에러 해결

--- src/components/OnBoardingSteps/Step06/Step06.tsx
+++ src/components/OnBoardingSteps/Step06/Step06.tsx
@@ -163,7 +163,7 @@ const OnboardingFinal = (props: OnboardingFinalProps) => {
           <IcLink style={{ width: '1.8rem', height: '1.8rem' }} />
           링크 복사
         </S.LinkCopyBtn>
-        <S.KakaoLinkCopyBtn onClick={() => useKakaoShare()}>
+        <S.KakaoLinkCopyBtn onClick={() => useKakaoShare(invitationCode)}>
           <IcKakaoShare style={{ width: '1.8rem', height: '1.8rem' }} />
           카카오톡 공유
         </S.KakaoLinkCopyBtn>
