ec71769990edc5125fee83e5f09609f54053d1ac
imeureka <lky062@naver.com>
2024-01-20 07:49:35 +0900

    fix: 빌드 오류 해결

--- src/pages/Start/Start.tsx
+++ src/pages/Start/Start.tsx
@@ -1,8 +1,8 @@
 import { Person1, Person2, Person3, Person4 } from '../../assets/svg';
 import * as S from './Start.style';
-import { post } from '../../apis/client';
+//import { post } from '../../apis/client';
 import StartAnimation from '../../../public/motions/start_motion.json';
-import { useEffect, useRef, useState } from 'react';
+// import { useEffect, useRef, useState } from 'react';
 import { ScrollAnimationContainer } from '../../components/ScrollAnimationContainer/ScrollAnimationContainer';
 import { ScrollAnimationLeftContainer } from '../../components/ScrollAnimationContainer/ScrollAnimationContainerLeft';
 import StartHeader from './StartHeader/StartHeader';
@@ -12,40 +12,38 @@ import LottieAnimation from '../../hooks/lottie-animation/LottieAnimation';
 import BtnFill from '../../components/common/Button/Cta/fill/BtnFill';
 import { kakaoURL } from '../../utils/login';
 
-interface TokenResponseType {
-  data: {
-    accessToken: string;
-    refreshToken: string;
-  };
-}
+// interface TokenResponseType {
+//   data: {
+//     accessToken: string;
+//     refreshToken: string;
+//   };
+// }
 
 export const isTokenExist = (): boolean => {
-  const token = localStorage.getItem(
-    'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxIiwiaWF0IjoxNzA1NjQ5NjkxLCJleHAiOjE3MDYyNTQ0OTF9.LfkqgbqIYgD1gc6SyNBUUkL4VA_oxpVcEzj0xGqvwec',
-  ); // 'yourTokenKey'는 실제 사용하는 토큰의 키로 대체하세요
+  const token = localStorage.getItem('EXIT_LOGIN_TOKEN'); // 'yourTokenKey'는 실제 사용하는 토큰의 키로 대체하세요
   return !!token;
 };
 
 const Start = () => {
   const navigate = useNavigate();
 
-  // const handleLogin = () => {
-  //   window.location.replace(kakaoURL);
-  // };
-  const handleClick = () => {
-    const fetchExAuth = (): Promise<TokenResponseType> => post('/member/token/2');
-    fetchExAuth().then((response: TokenResponseType) => {
-      const data = response;
-      const JWT = data.data.accessToken;
-      if (data) {
-        localStorage.setItem('EXIT_LOGIN_TOKEN', JWT);
-        console.log(data);
-        console.log(localStorage.getItem('EXIT_LOGIN_TOKEN'));
-      }
-    });
-
-    navigate('/mypage');
+  const handleLogin = () => {
+    window.location.replace(kakaoURL);
   };
+  // const handleClick = () => {
+  //   const fetchExAuth = (): Promise<TokenResponseType> => post('/member/token/2');
+  //   fetchExAuth().then((response: TokenResponseType) => {
+  //     const data = response;
+  //     const JWT = data.data.accessToken;
+  //     if (data) {
+  //       localStorage.setItem('EXIT_LOGIN_TOKEN', JWT);
+  //       console.log(data);
+  //       console.log(localStorage.getItem('EXIT_LOGIN_TOKEN'));
+  //     }
+  //   });
+
+  //   navigate('/mypage');
+  // };
 
   const goGiftRoom = () => {
     console.log('선물방으로 이동');
@@ -82,7 +80,7 @@ const Start = () => {
           새로운 선물 준비하기
         </BtnFill>
       ) : (
-        <S.KakaoLogin onClick={handleClick} />
+        <S.KakaoLogin onClick={handleLogin} />
       )}
       <S.BtnGradient />
 
