c56988ca146055ee631737a18b7d4d301a14e3b2
imeureka <lky062@naver.com>
2024-01-18 18:28:14 -0800

    chore: 구문 분리

--- src/components/TournamentNew/Intro/TournamentNoneText/TournamentNoneText.tsx
+++ src/components/TournamentNew/Intro/TournamentNoneText/TournamentNoneText.tsx
@@ -4,7 +4,10 @@ const TournamentNoneText = () => {
   return (
     <S.TournamentStartTextWrapper>
       <S.Title>토너먼트를 시작할 수 없어요</S.Title>
-      <S.SubTitle>선물이 등록되어야 토너먼트를 시작할 수 있어요</S.SubTitle>
+      <S.SubTitle>
+        선물이 등록되어야 <br />
+        토너먼트를 시작할 수 있어요
+      </S.SubTitle>
     </S.TournamentStartTextWrapper>
   );
 };
