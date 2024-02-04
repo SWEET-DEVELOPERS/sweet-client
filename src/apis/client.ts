1bbd0f7a1329dc331927e2c7f859d2b109867803
Wang HoEun <hoeunwang@gmail.com>
2024-02-04 22:21:48 +0900

    refactor: 에러 코드 확인을 위해 response 관련 코드 추가

--- src/apis/client.ts
+++ src/apis/client.ts
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios, { AxiosError, AxiosResponse } from 'axios';
 
 export const getAccessTokenLocalStorage = () => {
   const accessToken = localStorage.getItem('EXIT_LOGIN_TOKEN');
@@ -53,7 +53,6 @@ instance.interceptors.response.use(
       if (msg == '액세스 토큰이 만료되었습니다. 재발급 받아주세요.') {
         console.log('엑세스 토큰 만료 메세지 인식 했다.');
 
-
         // access 토큰 재발급 api
         const response = await postRefreshToken();
         //access 토큰 요청이 성공할 때
@@ -73,7 +72,6 @@ instance.interceptors.response.use(
       else if (msg == '리프레시 토큰이 만료되었습니다. 다시 로그인해 주세요.') {
         console.log('리프레쉬 토큰 만료 메세지 인식 했다.');
 
-
         localStorage.clear();
         window.location.replace('/');
         window.alert('토큰이 만료되어 자동으로 로그아웃 되었습니다.');
@@ -82,6 +80,17 @@ instance.interceptors.response.use(
     return Promise.reject(error);
   },
 );
+
+function interceptorResponseFulfilled(res: AxiosResponse) {
+  return res.status >= 200 && res.status < 300 ? res.data : Promise.reject(res.data);
+}
+
+function interceptorResponseRejected(error: AxiosError) {
+  // @ts-ignore
+  return Promise.reject(new Error(error.response?.data?.message ?? error));
+}
+
+instance.interceptors.response.use(interceptorResponseFulfilled, interceptorResponseRejected);
 
 export function get<T>(...args: Parameters<typeof instance.get>) {
   return instance.get<T, T>(...args);
