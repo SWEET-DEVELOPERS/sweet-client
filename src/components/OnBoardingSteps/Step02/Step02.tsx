4156c0dfb178eb3475afc46724f2881551423a19
장정안 <wjddks96@naver.com>
2024-02-06 15:44:35 +0900

    feat: step02 관심사 분리

--- src/components/OnBoardingSteps/Step02/Step02.tsx
+++ src/components/OnBoardingSteps/Step02/Step02.tsx
@@ -1,35 +1,18 @@
 import Title from '../../common/title/Title';
 import { IcEmptyThumbnail } from '../../../assets/svg';
 import * as S from './Step02.style';
-import { useState } from 'react';
 import OnBoardingBtn from '../onboardingBtn/OnBoardingBtn';
+import usePreviewImage from '../../../hooks/common/usePreviewImage';
 
 /** @TODO 추후 presigned URL 진행 */
 interface ThumbnailInputProps {
   onNext: VoidFunction;
-  setFileName: React.Dispatch<React.SetStateAction<string>>;
-  setImageFile: React.Dispatch<React.SetStateAction<File | null>>;
 }
 
 const ThumbnailInput = (props: ThumbnailInputProps) => {
-  const { onNext, setFileName, setImageFile } = props;
-  const [isImageUploaded, setIsImageUploaded] = useState<boolean>(false);
-  const [previewImage, setPreviewImage] = useState<string | null>(null);
+  const { onNext } = props;
 
-  const handleImageUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const { files } = event.target;
-    if (files && files.length > 0) {
-      const selectedFiles = files as FileList;
-      setPreviewImage(URL.createObjectURL(selectedFiles[0]));
-      setIsImageUploaded(!!selectedFiles?.[0]);
-
-      const imageFile = selectedFiles[0];
-      setImageFile(imageFile);
-
-      const imageName = files[0].name.trim();
-      setFileName(imageName);
-    }
-  };
+  const { isImageUploaded, previewImage, handleImageUpload } = usePreviewImage();
 
   return (
     <>
