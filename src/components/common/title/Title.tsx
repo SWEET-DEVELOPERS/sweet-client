71f9202f5acfc15e79b8864297f8d5298cd46c31
장정안 <wjddks96@naver.com>
2024-02-07 23:59:07 +0900

    chore: 공통 타이틀 주석 제거

--- src/components/common/title/Title.tsx
+++ src/components/common/title/Title.tsx
@@ -10,7 +10,6 @@ const Title = ({ children, userName }: PropsWithChildren<TitleProps>) => {
   return (
     <div style={{ display: 'flex', whiteSpace: 'nowrap' }}>
       {userName && <S.Username>{userName}</S.Username>}
-      {/* <S.Title>{title}</S.Title> */}
       <S.Title>{children}</S.Title>
     </div>
   );
