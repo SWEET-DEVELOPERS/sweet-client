b4c3dd99152754d81e979de56997ed0ba0e4656d
urjimyu <92876819+urjimyu@users.noreply.github.com>
2024-02-10 00:44:38 +0900

    fix: invalidated queries to refetch fresh data

--- src/hooks/queries/gift/usePostGift.tsx
+++ src/hooks/queries/gift/usePostGift.tsx
@@ -1,28 +1,36 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { post } from '../../../apis/client';
 import { GiftPostRequestType } from '../../../types/gift';
 import { useNavigate } from 'react-router-dom';
+import { MY_GIFT_QUERY_KEY } from './useGetMyGift';
 
-export const postNewGift = async (body: GiftPostRequestType) => {
-  post(`/gift`, body);
-};
+export async function postNewGift(body: GiftPostRequestType) {
+  await post(`/gift`, body);
+}
 
-export const usePostGift = (roomId: number, targetDate: string) => {
+export function usePostGift(
+  roomId: number,
+  targetDate: string,
+  setStep: React.Dispatch<React.SetStateAction<number>>,
+) {
   const navigate = useNavigate();
+
+  const queryClient = useQueryClient();
 
   const mutation = useMutation({
     mutationFn: postNewGift,
     onSuccess: () => {
-      console.log('roomId, targetDate', roomId, targetDate);
+      console.log('선물 등록 성공!!');
+      queryClient.invalidateQueries({ queryKey: [MY_GIFT_QUERY_KEY[0], roomId] });
       navigate(`/add-gift/${roomId}/${targetDate}`);
+      setStep(0);
     },
-
-    onError: () => {
-      console.log('선물 등록 에러!!');
+    onError: (error) => {
+      console.log('선물 등록 에러!!', error.message);
     },
   });
 
   return { mutation };
-};
+}
 
 export default usePostGift;
