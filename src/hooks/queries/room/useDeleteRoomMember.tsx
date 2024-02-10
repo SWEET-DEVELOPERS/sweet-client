2f7ad8c9cd89a7a55080b41307b68b0ef2906e83
urjimyu <92876819+urjimyu@users.noreply.github.com>
2024-02-10 18:44:57 +0900

    refactor: 에러 메시지 출력되도록 수정

--- src/hooks/queries/room/useDeleteRoomMember.tsx
+++ src/hooks/queries/room/useDeleteRoomMember.tsx
@@ -13,15 +13,14 @@ export const deleteRoomMember = async ({ roomId, memberId }: DeleteRoomMember) =
 
 const useDeleteRoomMember = ({ roomId }: DeleteRoomMember) => {
   const queryClient = useQueryClient();
-
   const mutation = useMutation({
     mutationFn: deleteRoomMember,
     onSuccess() {
       console.log('삭제 성공');
       queryClient.invalidateQueries({ queryKey: [...ROOM_MEMBER_QUERY_KEY, roomId] });
     },
-    onError: () => {
-      console.log('선물 삭제 중 에러가 발생했습니다.');
+    onError: (error) => {
+      console.log('선물 삭제 중 에러가 발생했습니다.', error.message);
     },
   });
   return { mutation };
