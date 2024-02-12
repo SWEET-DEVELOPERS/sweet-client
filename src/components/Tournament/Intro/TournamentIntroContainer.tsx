274fcbfef5525f2da48cbe445bbb1511bad583dd
imeureka <lky062@naver.com>
2024-02-12 17:10:26 +0900

    feat: 토너먼트 선물 0개 일 때 예외 처리

--- src/components/Tournament/Intro/TournamentIntroContainer.tsx
+++ src/components/Tournament/Intro/TournamentIntroContainer.tsx
@@ -6,44 +6,54 @@ import useTournament from '../../../hooks/tournament/useTournament';
 import TournamentFlowContainer from '../TournamentFlow/TournamentFlowContainer';
 import useGetItem from '../../../hooks/queries/tournament/useGetItem';
 import trophy from '../../../assets/img/3dic_trophy3.png';
+import { TrophyNone } from '../../../assets/svg';
 import { useParams } from 'react-router';
 import Header from '../../common/Header';
+import TournamentNoneText from './TournamentNoneText/TournamentNoneText'; // TournamentStartNone 컴포넌트를 import합니다.
 
 const TournamentIntroContainer = () => {
   const params = useParams();
   const giftee = params.giftee;
-  console.log('울랄라', giftee);
+
   const roomIdString = params.roomId || '';
   const roomId = parseInt(roomIdString || '', 10);
-  console.log('추출된 초대 코드', roomId);
 
   const { showTournamentContainer, handleStartClick } = useTournament();
   const memberData = useGetItem({ roomId: Number(roomId) });
   console.log(memberData);
 
   const tournamentData = memberData?.data || [];
+
   return (
     <>
       {showTournamentContainer ? (
         <>
           <Header />
           <S.TournamentFlowWrapper>
-            <TournamentStartText giftee={giftee} />
-            <TournamentItemCount memberData={tournamentData} />
-            <S.TournamentImg>
-              <img src={trophy} alt='트로피' />
-            </S.TournamentImg>
-            <TournamentStartButton onClick={handleStartClick} />
+            {tournamentData.length === 0 ? (
+              <>
+                <TournamentNoneText />
+                <S.TournamentImg>
+                  <TrophyNone />
+                </S.TournamentImg>
+              </>
+            ) : (
+              <>
+                <TournamentStartText giftee={giftee} />
+                <TournamentItemCount memberData={tournamentData} />
+                <S.TournamentImg>
+                  <img src={trophy} alt='트로피' />
+                </S.TournamentImg>
+                <TournamentStartButton onClick={handleStartClick} />
+              </>
+            )}
           </S.TournamentFlowWrapper>
         </>
-      ) : memberData ? (
+      ) : (
         <>
           <Header />
-
           <TournamentFlowContainer memberData={tournamentData} roomId={roomId} />
         </>
-      ) : (
-        <TournamentFlowContainer memberData={tournamentData} roomId={roomId} />
       )}
     </>
   );
