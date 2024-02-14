a40cc48ef7966e1e3c86c5e8ff39fe8fe548f15a
urjimyu <92876819+urjimyu@users.noreply.github.com>
2024-02-14 18:25:25 +0900

    refactor: 폼데이터 전송으로 수정

--- src/hooks/queries/onboarding/usePutPresignedUrl.tsx
+++ src/hooks/queries/onboarding/usePutPresignedUrl.tsx
@@ -1,26 +1,40 @@
-import axios, { AxiosError, AxiosResponse } from 'axios';
+// import axios, { AxiosError, AxiosResponse } from 'axios';
 import { useMutation } from '@tanstack/react-query';
+import { put } from '../../../apis/client';
+import { AxiosRequestConfig } from 'axios';
 
-const putPresignedUrl = async (presignedUrl: string): Promise<AxiosResponse> => {
-  try {
-    const response: AxiosResponse = await axios.put(presignedUrl);
-    return response.data;
-  } catch (error) {
-    if (error instanceof AxiosError) {
-      throw new Error(error.response?.data?.message ?? error.message);
-    } else {
-      throw error;
-    }
-  }
-};
+interface putPresignedUrlProps {
+  presignedUrl: string;
+  formData: FormData;
+}
 
-const usePutPresignedUrl = () => {
-  return useMutation({
-    mutationFn: putPresignedUrl,
-    onSuccess: (data) => {
-      console.log('풋 성공', data);
+export function putFormData<T>(url: string, data: FormData, config?: AxiosRequestConfig) {
+  return put<T>(url, data, {
+    ...config,
+    headers: {
+      ...config?.headers,
+      'Content-Type': 'multipart/form-data',
     },
   });
+}
+
+export const putPresignedUrl = async ({ presignedUrl, formData }: putPresignedUrlProps) => {
+  console.log('put직전!');
+  putFormData(presignedUrl, formData);
+};
+
+export const usePutPresignedUrl = () => {
+  const mutation = useMutation({
+    mutationFn: putPresignedUrl,
+    onSuccess: (data) => {
+      console.log('PUT 성공', data);
+    },
+    onError: (error) => {
+      console.log('이미지 PUT 에러!', error.message);
+    },
+  });
+
+  return { mutation };
 };
 
 export default usePutPresignedUrl;
