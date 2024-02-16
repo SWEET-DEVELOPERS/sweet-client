cddfca49a5d31008461e7e116392e872cee26a7e
imeureka <lky062@naver.com>
2024-02-17 01:08:47 +0900

    fix: useGetItem 이름 변경

--- src/hooks/queries/tournament/useGetItem.tsx
+++ src/hooks/queries/tournament/useGetItem.tsx
@@ -11,14 +11,19 @@ export const TOURNAMENT_BY_ID_QUERY_KEY: string[] = ['tournamentData'];
 export const fetchTournamentItem = async (roomId: number): Promise<TournamentResponse> =>
   get(`/gift/tournament/${roomId}`);
 
-const useGetMyPage = ({ roomId }: { roomId: number }) => {
+const useGetItem = ({ roomId }: { roomId: number }) => {
   const { data } = useQuery({
     queryKey: [TOURNAMENT_BY_ID_QUERY_KEY, roomId],
     queryFn: () => fetchTournamentItem(roomId),
   });
 
+  if (Error && (Error as any).response?.data?.message === '이미 참가한 토너먼트입니다.') {
+    return 400;
+    console.log('400 중복 참여입니다');
+  }
+
   console.log(data);
   return data;
 };
 
-export default useGetMyPage;
+export default useGetItem;
