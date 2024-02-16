8cdc214033f8ea1076d2802e0be70672cd6b6446
장정안 <wjddks96@naver.com>
2024-02-17 02:59:59 +0900

    style: 온보딩 이미지와 참여자 뷰 이미지 그라데이션 수치 일치화

--- src/components/OnBoardingSteps/Step06/Step06.style.ts
+++ src/components/OnBoardingSteps/Step06/Step06.style.ts
@@ -28,7 +28,7 @@ export const GradientImg = styled.div`
     left: 0;
     width: 100%;
     height: 100%;
-    background: linear-gradient(180deg, rgba(255, 255, 255, 0.7) 0%, #fff 100%);
+    background: linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, #fff 100%);
     z-index: 1;
   }
 `;
