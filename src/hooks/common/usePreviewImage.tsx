7c67d642741fb19cec082bf23b6946354662c392
장정안 <wjddks96@naver.com>
2024-02-16 21:50:58 +0900

    feat: filename & file자체 저장 useState 추가

--- src/hooks/common/usePreviewImage.tsx
+++ src/hooks/common/usePreviewImage.tsx
@@ -3,14 +3,27 @@ import { useState } from 'react';
 const usePreviewImage = () => {
   const [isImageUploaded, setIsImageUploaded] = useState<boolean>(false);
   const [previewImage, setPreviewImage] = useState<string | null>(null);
+  const [imageName, setImageName] = useState<string>('');
+  const [file, setFile] = useState<File | null>(null);
 
   const handleImageUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { files } = event.target;
 
     if (files && files.length > 0) {
       const selectedFiles = files as FileList;
+      const imageName = files[0].name.trim();
+      // const selectedFile = files[0];
+      // const newFormData = new FormData();
+
+      // newFormData.append('file', selectedFile);
+
+      setFile(selectedFiles[0]);
+
       setPreviewImage(URL.createObjectURL(selectedFiles[0]));
       setIsImageUploaded(!!selectedFiles?.[0]);
+
+      setImageName(imageName);
+      // console.log('handleImageUpload newformData', newFormData);
     }
   };
 
@@ -18,6 +31,8 @@ const usePreviewImage = () => {
     isImageUploaded,
     previewImage,
     handleImageUpload,
+    imageName,
+    file,
   };
 };
 
