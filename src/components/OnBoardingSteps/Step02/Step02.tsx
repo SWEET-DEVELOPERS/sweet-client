81f8667eb5a40b48393d011b784db6d9165e2749
장정안 <wjddks96@naver.com>
2024-02-16 21:53:16 +0900

    feat: presignedUrl 로직 추가
    POST로 받아온 presignedUrl, binary형식으로 파일 자체를 담아서 PUT

--- src/components/OnBoardingSteps/Step02/Step02.tsx
+++ src/components/OnBoardingSteps/Step02/Step02.tsx
@@ -3,6 +3,9 @@ import { IcEmptyThumbnail } from '../../../assets/svg';
 import * as S from './Step02.style';
 import OnBoardingBtn from '../onboardingBtn/OnBoardingBtn';
 import usePreviewImage from '../../../hooks/common/usePreviewImage';
+import usePostPresignedUrl from '../../../hooks/queries/etc/usePostPresignedUrl';
+import usePutPresignedUrl from '../../../hooks/queries/onboarding/usePutPresignedUrl';
+import useBinarizeAndPutImage from '../../../hooks/queries/etc/useBinarizeAndPutImage';
 
 /** @TODO 추후 presigned URL 진행 */
 interface ThumbnailInputProps {
@@ -11,7 +14,43 @@ interface ThumbnailInputProps {
 
 const ThumbnailInput = (props: ThumbnailInputProps) => {
   const { onNext } = props;
-  const { isImageUploaded, previewImage, handleImageUpload } = usePreviewImage();
+  const { isImageUploaded, previewImage, handleImageUpload, imageName, file } = usePreviewImage();
+
+  const { binarizeAndPutImage } = useBinarizeAndPutImage();
+  // const { presignedUrl, mutate } = usePostPresignedUrl();
+  // const { presignedUrl, ...mutation } = usePostPresignedUrl();
+
+  const postPresignedUrl = usePostPresignedUrl();
+  const putPresignedUrlAndImageFile = usePutPresignedUrl();
+
+  const fetchPresignedUrl = async (filename: string) => {
+    try {
+      postPresignedUrl.mutate(filename, {
+        onSuccess: async (data) => {
+          /** @todo 파일네임 파싱하는 함수 유틸로 처리 */
+          const presignedUrl = data.presignedUrl.split('?')[0];
+          console.log('data.presignedUrl', data.presignedUrl);
+          console.log('parsingpresignedUrl', presignedUrl);
+
+          if (file) {
+            await binarizeAndPutImage({ presignedUrl, file });
+          }
+
+          // putPresignedUrlAndImageFile.mutate({ presignedUrl: parsingPresignedUrl, binaryData });
+
+          // /** @see formData를 콘솔에 찍는 법 */
+          // for (const [key, value] of formData?.entries()) {
+          //   console.log(key, value);
+          // }
+
+          // onNext();
+          // return data.presignedUrl;
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <>
@@ -29,7 +68,14 @@ const ThumbnailInput = (props: ThumbnailInputProps) => {
         </label>
         {previewImage && <S.PreviewImg src={previewImage} />}
       </S.IcEmptyThumbnailWrapper>
-      <OnBoardingBtn isActivated={isImageUploaded} setStep={onNext}>
+      <OnBoardingBtn
+        isActivated={isImageUploaded}
+        // setStep={onNext}>
+        setStep={() => {
+          fetchPresignedUrl(imageName);
+          // fetchPresignedUrl();
+        }}
+      >
         다음
       </OnBoardingBtn>
     </>
