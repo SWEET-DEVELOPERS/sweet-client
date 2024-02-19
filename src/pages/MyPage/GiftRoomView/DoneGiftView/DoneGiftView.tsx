606b8d9153a9aa7c021fb93d08ddec9cbd58f043
Wang HoEun <hoeunwang@gmail.com>
2024-02-19 23:54:40 +0900

    refactor: view 코드 component 분리 map을 더 간단하고 가독성 있게

--- src/pages/MyPage/GiftRoomView/DoneGiftView/DoneGiftView.tsx
+++ src/pages/MyPage/GiftRoomView/DoneGiftView/DoneGiftView.tsx
@@ -1,8 +1,9 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { ClosedRoomArrayType, ClosedRoomType } from '../../../../types/member';
 import DoneCardRoom from '../../../../components/CardRoom/DoneCardRoom';
 import GiftRoomHeader from '../GiftRoomHeader/GiftRoomHeader';
-import { ClosedRoomType } from '../../../../types/member';
 import * as S from './DoneGiftView.style';
-import { useNavigate } from 'react-router-dom';
 
 interface DoneGiftViewProps {
   data: ClosedRoomType | undefined;
@@ -14,24 +15,25 @@ const DoneGiftView: React.FC<DoneGiftViewProps> = ({ data }) => {
 
   const [doneData1, doneData2] = Array.isArray(data) ? data : [undefined, undefined];
 
+  const renderRoom = (doneData: ClosedRoomArrayType | undefined, index: number) => {
+    if (!doneData) return null;
+
+    return (
+      <DoneCardRoom
+        key={index}
+        user={doneData.gifteeName || ''}
+        srcImage={doneData.imageUrl || ''}
+        userCount={doneData.gifterNumber || 0}
+        onClick={() => navigate(`/tournament-ranking/${doneData.gifteeName}/${doneData.roomId}`)}
+      />
+    );
+  };
+
   return (
     <S.DoneGiftViewWrapper>
       <GiftRoomHeader name='종료된 선물방' page='detail-done' doneData={data} />
       <S.RoomWrapper>
-        {[doneData1, doneData2].map(
-          (doneData, index) =>
-            doneData && (
-              <DoneCardRoom
-                key={index}
-                user={doneData.gifteeName || ''}
-                srcImage={doneData.imageUrl || ''}
-                userCount={doneData.gifterNumber || 0}
-                onClick={() =>
-                  navigate(`/tournament-ranking/${doneData.gifteeName}/${doneData.roomId}`)
-                }
-              />
-            ),
-        )}
+        {[doneData1, doneData2].map(renderRoom)}
         {!doneData1 && !doneData2 && <S.NoneRoom>지금 진행 중인 선물방이 없어요</S.NoneRoom>}
       </S.RoomWrapper>
     </S.DoneGiftViewWrapper>
