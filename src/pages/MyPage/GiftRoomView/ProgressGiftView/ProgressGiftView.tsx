5548eef0cf41195ba6d584c74431deea9bb37677
Wang HoEun <hoeunwang@gmail.com>
2024-02-19 23:49:40 +0900

    refactor: 반복되는 컴포넌트 새로운 const로 생성

--- src/pages/MyPage/GiftRoomView/ProgressGiftView/ProgressGiftView.tsx
+++ src/pages/MyPage/GiftRoomView/ProgressGiftView/ProgressGiftView.tsx
@@ -1,10 +1,10 @@
 import EditCardRoom from '../../../../components/CardRoom/EditCardRoom';
 import GiftRoomHeader from '../GiftRoomHeader/GiftRoomHeader';
-import { ActiveRoomArrayType, ActiveRoomType } from '../../../../types/member';
 import ProgressCardRoom from '../../../../components/CardRoom/ProgressCardRoom';
 import { useNavigate } from 'react-router-dom';
 import DateCheck from '../../../../components/DateCheck/DateCheck';
 import * as S from './ProgressGiftView.style';
+import { ActiveRoomType, ActiveRoomArrayType } from '../../../../types/member';
 
 interface ProgressGiftViewProps {
   data: ActiveRoomType | undefined;
@@ -23,37 +23,30 @@ const ProgressGiftView: React.FC<ProgressGiftViewProps> = ({ data }) => {
       : `/tournament/${progressData.gifteeName}/${progressData.roomId}`;
   };
 
+  const renderRoom = (progressData: ActiveRoomArrayType | undefined, index: number) => {
+    if (!progressData) return null;
+
+    const CardComponent = progressData.isOwner ? EditCardRoom : ProgressCardRoom;
+
+    return (
+      <CardComponent
+        user={progressData.gifteeName || ''}
+        userCount={progressData.gifterNumber || 0}
+        srcImage={progressData.imageUrl || ''}
+        roomId={progressData.roomId}
+        date={progressData.tournamentStartDate}
+        onClick={() => navigate(getNavigateLink(progressData))}
+        key={index}
+      />
+    );
+  };
+
   return (
     <S.ProgressGiftViewWrapper>
       <GiftRoomHeader name='진행중인 선물방' page='detail-progress' activeData={data} />
       <S.RoomWrapper>
-        {[progressData1, progressData2].map(
-          (progressData, index) =>
-            progressData && (
-              <>
-                {progressData.isOwner ? (
-                  <EditCardRoom
-                    user={progressData.gifteeName}
-                    userCount={progressData.gifterNumber}
-                    srcImage={progressData.imageUrl}
-                    roomId={progressData.roomId}
-                    date={progressData.tournamentStartDate}
-                    onClick={() => navigate(getNavigateLink(progressData))}
-                    key={index}
-                  />
-                ) : (
-                  <ProgressCardRoom
-                    user={progressData.gifteeName || ''}
-                    userCount={progressData.gifterNumber || 0}
-                    srcImage={progressData.imageUrl || ''}
-                    roomId={progressData.roomId}
-                    date={progressData.tournamentStartDate}
-                    onClick={() => navigate(getNavigateLink(progressData))}
-                    key={index}
-                  />
-                )}
-              </>
-            ),
+        {[progressData1, progressData2].map((progressData, index) =>
+          renderRoom(progressData, index),
         )}
         {!progressData1 && !progressData2 && (
           <S.NoneRoom>지금 진행 중인 선물방이 없어요</S.NoneRoom>
