3766e04ef70a44cab5c56ed4abddd7ebeeaeed32
Wang HoEun <hoeunwang@gmail.com>
2024-02-19 21:19:36 +0900

    refactor: mypage view 코드 분리

--- src/pages/MyPage/MyPage.tsx
+++ src/pages/MyPage/MyPage.tsx
@@ -11,22 +11,37 @@ interface MyPage {
   memberData: MyPageType;
 }
 
+const MAX_USERNAME_LENGTH = 5;
+
 const MyPage = () => {
   const navigate = useNavigate();
 
   const memberData = useGetMyPage();
 
-  const userImage = memberData?.data?.memberInfo.profileImage;
-  const userName = memberData?.data?.memberInfo.nickname;
-  const translatedUserName = userName && userName.length > 5 ? userName.substring(0, 5) : userName;
-
-  const progressRoomData = memberData?.data?.activeRooms;
-  const doneMemberRoomData = memberData?.data?.closedRooms;
-
-  const giftData: boolean = progressRoomData !== null && doneMemberRoomData !== null;
-
   const goOnboarding = () => {
     navigate('/onboarding');
+  };
+
+  const renderGiftRoom = () => {
+    const progressRoomData = memberData?.data?.activeRooms;
+    const doneMemberRoomData = memberData?.data?.closedRooms;
+    return progressRoomData && doneMemberRoomData ? (
+      <S.GiftRoomWrapper>
+        <ProgressGiftView data={progressRoomData} />
+        <DoneGiftView data={doneMemberRoomData} />
+      </S.GiftRoomWrapper>
+    ) : (
+      <S.NoneText> 아직 선물방이 없어요</S.NoneText>
+    );
+  };
+
+  const renderUserName = () => {
+    const userName = memberData?.data?.memberInfo.nickname;
+    const translatedUserName =
+      userName && userName.length > MAX_USERNAME_LENGTH
+        ? `${userName.substring(0, MAX_USERNAME_LENGTH)}...`
+        : userName;
+    return <S.UserName>{translatedUserName}님</S.UserName>;
   };
 
   return (
@@ -35,32 +50,16 @@ const MyPage = () => {
       <S.ProfileWrapper>
         <S.UserButtonWrapper>
           <S.UserWrapper>
-            <ProfileImage image={userImage} />
-            <S.UserName>
-              <S.User>{translatedUserName}</S.User>님
-            </S.UserName>
+            <ProfileImage image={memberData?.data?.memberInfo.profileImage} />
+            {renderUserName()}
           </S.UserWrapper>
           <BtnLogout customStyle={{ width: '8.4rem', height: '2.6rem' }}>로그아웃</BtnLogout>
         </S.UserButtonWrapper>
-        <BtnFill
-          onClick={goOnboarding}
-          customStyle={{
-            width: '30.3rem',
-            height: '5.2rem',
-          }}
-        >
+        <BtnFill onClick={goOnboarding} customStyle={{ width: '30.3rem', height: '5.2rem' }}>
           새로운 선물 준비하기
         </BtnFill>
       </S.ProfileWrapper>
-
-      {giftData ? (
-        <S.GiftRoomWrapper>
-          <ProgressGiftView data={progressRoomData} />
-          <DoneGiftView data={doneMemberRoomData} />
-        </S.GiftRoomWrapper>
-      ) : (
-        <S.NoneText> 아직 선물방이 없어요</S.NoneText>
-      )}
+      {renderGiftRoom()}
     </S.MyPageWrapper>
   );
 };
