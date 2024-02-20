6de45b93e28d15418db394f3612d3a36935b4364
장정안 <wjddks96@naver.com>
2024-02-20 21:48:18 +0900

    feat: 뒤로가기 아이콘 추가 및 로직 추가

--- src/components/OnBoardingSteps/onboardingHeader/OnBoardingHeader.tsx
+++ src/components/OnBoardingSteps/onboardingHeader/OnBoardingHeader.tsx
@@ -1,13 +1,19 @@
 import styled from 'styled-components';
+import { IcLeft } from '../../../assets/svg';
 
 interface OnBoardingHeaderProps {
   step?: number;
   onClick?: () => void;
+  setStep: React.Dispatch<React.SetStateAction<number>>;
 }
 
-const OnBoardingHeader = ({ step }: OnBoardingHeaderProps) => {
+const OnBoardingHeader = ({ step, setStep }: OnBoardingHeaderProps) => {
+  const onClick = () => {
+    setStep((prev) => prev - 1);
+  };
   return (
     <OnBoardingHeaderWrapper>
+      <IcLeft width='4rem' height='4rem' onClick={onClick} style={{ marginLeft: '0.7rem' }} />
       <StepText>{step}/5</StepText>
     </OnBoardingHeaderWrapper>
   );
@@ -16,7 +22,8 @@ const OnBoardingHeader = ({ step }: OnBoardingHeaderProps) => {
 export default OnBoardingHeader;
 
 const OnBoardingHeaderWrapper = styled.div`
-  ${({ theme }) => theme.mixin.flexBox({ direction: 'row', align: 'center', justify: 'end' })};
+  ${({ theme }) =>
+    theme.mixin.flexBox({ direction: 'row', align: 'center', justify: 'space-between' })};
   width: 100%;
   height: 5.6rem;
 `;
@@ -24,4 +31,5 @@ const OnBoardingHeaderWrapper = styled.div`
 const StepText = styled.p`
   color: ${({ theme }) => theme.colors.G_07};
   ${({ theme }) => theme.fonts.body_02};
+  margin-right: 2rem;
 `;
