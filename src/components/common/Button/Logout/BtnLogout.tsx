34055260de6bb71753222d996b9213121cfaf5e6
Wang Hoeun <38005874+hoeun0723@users.noreply.github.com>
2024-02-20 19:37:11 +0900

    [마이페이지] 코드 리펙토링 (#370)
    * style: console 삭제
    
    * refactor: logout 버튼 logic 코드 이동
    
    * style: 마이페이지 data 순서 정리
    
    * style: 마이페이지 header 코드 가독성 높이기
    
    * refactor: progressgiftview 코드 정리
    
    * refactor: donegiftview 코드 정리
    
    * style: console 창 삭제
    
    * style: 기존으로 다시 원상복귀..
    
    * fix: logout instance 수정
    
    * fix: loggout 시 스토리지 clear로 변경
    
    * refactor: giftroomview 코드 클린하게 변동
    
    * refactor: detail room 코드 정리
    
    * refactor: mypage view 코드 분리
    
    * style: 안 쓰는 interface 삭제
    
    * refactor: 반복되는 컴포넌트 새로운 const로 생성
    
    * refactor: view 코드 component 분리 map을 더 간단하고 가독성 있게
    
    * style: props 변수 명 수정 및 옵셔널로 반영
    
    * style: 좀 더 정확한 data 네이밍 수정
    
    * style: data 옵셔널이 아닌, undefined로 예외처리 코드로 작성하기 수정
    
    * refactor: React.FC 삭제
    
    ---------
    
    Co-authored-by: Wang HoEun <hoeunwang@gmail.com>

--- src/components/common/Button/Logout/BtnLogout.tsx
+++ src/components/common/Button/Logout/BtnLogout.tsx
@@ -1,16 +1,26 @@
 import { ButtonHTMLAttributes } from 'react';
 import * as S from './BtnLogout.style';
+import { logOutInstance } from '../../../../apis/client';
+import { useNavigate } from 'react-router';
 
 type BtnLogoutProps = ButtonHTMLAttributes<HTMLButtonElement> & {
   disabled?: boolean;
   children: React.ReactNode;
   customStyle?: React.CSSProperties;
-  onClick?: () => void;
 };
 
-const BtnLogout = ({ disabled, children, customStyle, onClick }: BtnLogoutProps) => {
+const BtnLogout = ({ disabled, children, customStyle }: BtnLogoutProps) => {
+  const navigate = useNavigate();
+  const fetchAuth = async () => logOutInstance.post(`/oauth/logout`);
+  const handleClick = () => {
+    fetchAuth().then((response: any) => {
+      console.log(response);
+    });
+    localStorage.clear();
+    navigate('/');
+  };
   return (
-    <S.Wrapper disabled={disabled} style={customStyle} onClick={onClick}>
+    <S.Wrapper disabled={disabled} style={customStyle} onClick={handleClick}>
       {children}
     </S.Wrapper>
   );
