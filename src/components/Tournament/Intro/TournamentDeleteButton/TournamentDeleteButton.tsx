deb67445353e66704dbc027a5f61e055e4596b76
imeureka <lky062@naver.com>
2024-02-20 21:40:26 +0900

    chore: React.FC 지양

--- src/components/Tournament/Intro/TournamentDeleteButton/TournamentDeleteButton.tsx
+++ src/components/Tournament/Intro/TournamentDeleteButton/TournamentDeleteButton.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import BtnFill from '../../../common/Button/Cta/fill/BtnFill';
 import * as S from './TournamentDeleteButton.style';
 
@@ -6,7 +5,7 @@ interface TournamentStartButtonProps {
   onClick: () => void;
 }
 
-const TournamentDeleteButton: React.FC<TournamentStartButtonProps> = ({ onClick }) => {
+function TournamentDeleteButton({ onClick }: TournamentStartButtonProps) {
   return (
     <S.TournamentStartButtonWrapper>
       <BtnFill
@@ -22,6 +21,6 @@ const TournamentDeleteButton: React.FC<TournamentStartButtonProps> = ({ onClick 
       </BtnFill>
     </S.TournamentStartButtonWrapper>
   );
-};
+}
 
 export default TournamentDeleteButton;
