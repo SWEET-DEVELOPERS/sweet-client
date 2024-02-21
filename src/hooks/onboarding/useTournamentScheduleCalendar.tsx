48a32c0ef32bea095a8b6bfdd2e97b7a6113d9c1
장정안 <wjddks96@naver.com>
2024-02-21 22:42:04 +0900

    feat: 토너먼트 스케줄 캘린더 담당 커스텀 훅 작성

---
+++ src/hooks/onboarding/useTournamentScheduleCalendar.tsx
@@ -0,0 +1,52 @@
+import { format } from 'date-fns';
+import { useEffect, useState } from 'react';
+import { useOnboardingContext } from '../../context/Onboarding/OnboardingContext';
+
+const useTournamentScheduleCalendar = () => {
+  const { onboardingInfo, updateOnboardingInfo } = useOnboardingContext();
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [selectedDate, setSelectedDate] = useState<Date | null>(null);
+  const [isActivated, setIsActivated] = useState<boolean>(false);
+  const disabledDays = { before: new Date() };
+  const time = new Date();
+
+  const openCalendar = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleDateSelect = (date: Date) => {
+    const padTwoDigits = (value: number) => String(value).padStart(2, '0');
+
+    const formattedDate =
+      format(date, 'y-MM-dd') +
+      'T' +
+      `${padTwoDigits(time.getHours())}:${padTwoDigits(time.getMinutes())}:${padTwoDigits(
+        time.getSeconds(),
+      )}`;
+
+    updateOnboardingInfo({ tournamentStartDate: formattedDate });
+
+    console.log('날짜 선택 시 context formattedDate update', formattedDate);
+
+    setSelectedDate(date);
+    setIsOpen(false);
+  };
+
+  useEffect(() => {
+    setIsActivated(Boolean(onboardingInfo.deliveryDate));
+  }, [onboardingInfo.deliveryDate]);
+
+  return {
+    isOpen,
+    selectedDate,
+    isActivated,
+    setIsActivated,
+    disabledDays,
+    time,
+    openCalendar,
+    handleDateSelect,
+    onboardingInfo,
+  };
+};
+
+export default useTournamentScheduleCalendar;
