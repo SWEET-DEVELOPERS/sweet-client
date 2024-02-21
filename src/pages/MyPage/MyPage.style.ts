632fc3e057de49796f1471e069bb9fdcd2d40be5
Wang HoEun <hoeunwang@gmail.com>
2024-02-21 17:40:39 +0900

    style: 마이페이지 가로 스크롤 안 생기게 수정 및, userbox 중앙정렬

--- src/pages/MyPage/MyPage.style.ts
+++ src/pages/MyPage/MyPage.style.ts
@@ -2,15 +2,14 @@ import styled from 'styled-components';
 import BackgroundImage from '../../assets/img/mypage_background.png';
 
 export const MyPageWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
+  ${({ theme: { mixin } }) => mixin.flexCenter({})};
   width: 100%;
-  height: 100%;
 `;
 
 export const TopImage = styled.div`
+  ${({ theme: { mixin } }) => mixin.flexBox({})};
   position: relative;
-  width: 37.5rem;
+  width: 100%;
   height: 34rem;
   flex-shrink: 0;
   top: 5rem;
@@ -21,7 +20,7 @@ export const TopImage = styled.div`
   margin: 0;
 `;
 export const UserName = styled.div`
-  ${({ theme: { mixin } }) => mixin.flexBox({ align: 'center' })};
+  ${({ theme: { mixin } }) => mixin.flexBox({ align: 'center', justify: 'center' })};
 
   ${({ theme: { fonts } }) => fonts.heading_03};
 `;
@@ -51,7 +50,6 @@ export const ProfileWrapper = styled.div`
   padding: 0 1.6rem;
   gap: 1.6rem;
   top: 19rem;
-  left: 1.7rem;
 
   z-index: 8;
 
@@ -62,7 +60,6 @@ export const GiftRoomWrapper = styled.div`
   ${({ theme: { mixin } }) =>
     mixin.flexBox({ direction: 'column', align: 'center', justify: 'center' })};
   background-color: white;
-  width: 100%;
   position: relative;
   top: -5rem;
   padding-top: 4rem;
