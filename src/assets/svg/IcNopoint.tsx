eb2fdcc4b5589b5f1bf7923f2130efc7aeb9c1b9
장정안 <wjddks96@naver.com>
2024-02-23 04:58:15 +0900

    chore: svg 파일 추가

---
+++ src/assets/svg/IcNopoint.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import type { SVGProps } from 'react';
+const SvgIcNopoint = (props: SVGProps<SVGSVGElement>) => (
+  <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 16 16' {...props}>
+    <circle cx={8} cy={8} r={6} fill='#fff' stroke='#FF99C0' strokeWidth={4} />
+  </svg>
+);
+export default SvgIcNopoint;
