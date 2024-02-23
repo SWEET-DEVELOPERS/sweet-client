22073d771c1dffca345e2ddf57761fa35e1e6a09
장정안 <wjddks96@naver.com>
2024-02-23 15:52:27 +0900

    feat: 이미지 최소 height 변경 (추후 논의 필요)

--- src/hooks/common/usePreviewImage.tsx
+++ src/hooks/common/usePreviewImage.tsx
@@ -7,7 +7,7 @@ const usePreviewImage = () => {
   const [file, setFile] = useState<File | null>(null);
 
   /**@todo 이미지 최소 height 상의 필요*/
-  const MIN_IMAGE_HEIGHT = 400;
+  const MIN_IMAGE_HEIGHT = 300;
 
   const handleImageUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { files } = event.target;
@@ -21,7 +21,7 @@ const usePreviewImage = () => {
         if (img.height <= MIN_IMAGE_HEIGHT) {
           //  이미지 너비가  어느 수준  이하일 때 업로드 x
           alert(
-            `이미지 세로길이가 너무 작습니다. ${MIN_IMAGE_HEIGHT} 이상인 이미지를 선택해주세요.`,
+            `이미지 세로길이가 너무 작습니다. ${MIN_IMAGE_HEIGHT}px 이상인 이미지를 선택해주세요.`,
           );
           setIsImageUploaded(false);
           setFile(null);
