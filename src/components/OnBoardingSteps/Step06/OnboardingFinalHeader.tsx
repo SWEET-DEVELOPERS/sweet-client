3eb12b076018c0cceff4c0ac85e3e2fa2c4907f8
장정안 <wjddks96@naver.com>
2024-02-23 22:23:11 +0900

    fix: 빌드에러 수정

--- src/components/OnBoardingSteps/Step06/OnboardingFinalHeader.tsx
+++ src/components/OnBoardingSteps/Step06/OnboardingFinalHeader.tsx
@@ -1,11 +1,9 @@
 import { useNavigate } from 'react-router';
 import * as S from './OnboardingFinalHeader.style';
-import { useState } from 'react';
 import { IcHome, IcProfile } from '../../../assets/svg';
 
 const OnboardingFinalHeader = () => {
   const navigate = useNavigate();
-  const [isTop, setIsTop] = useState(true);
 
   const handleMyPageIcon = () => {
     navigate(`/mypage`);
@@ -16,7 +14,7 @@ const OnboardingFinalHeader = () => {
   };
 
   return (
-    <S.OnboardingFinalHeaderWrapper style={{ backgroundColor: isTop ? 'transparent' : 'white' }}>
+    <S.OnboardingFinalHeaderWrapper style={{ backgroundColor: 'white' }}>
       <IcHome style={{ width: '4.3rem' }} onClick={handleSettingHomeIcon} />
 
       <IcProfile style={{ width: '4.3rem' }} onClick={handleMyPageIcon} />
