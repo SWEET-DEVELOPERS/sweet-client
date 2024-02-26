11ea434a81e3092aa199efd0992599d92d9593fe
장정안 <wjddks96@naver.com>
2024-02-27 00:01:02 +0900

    feat: 기존 OnboardingContext에서 previewImage 관련 로직 삭제

--- src/context/Onboarding/OnboardingContext.tsx
+++ src/context/Onboarding/OnboardingContext.tsx
@@ -4,8 +4,6 @@ import { OnboardingInfo } from '../../types/Onboarding';
 interface OnboardingInfoContext {
   onboardingInfo: OnboardingInfo;
   updateOnboardingInfo: (newInfo: Partial<OnboardingInfo>) => void;
-  previewImage: string;
-  setPreviewImage: (previewImage: string) => void;
 }
 
 const initialOnboardingInfo: OnboardingInfo = {
@@ -19,15 +17,12 @@ const initialOnboardingInfo: OnboardingInfo = {
 const OnboardingContext = createContext<OnboardingInfoContext>({
   onboardingInfo: initialOnboardingInfo,
   updateOnboardingInfo: () => {},
-  previewImage: '',
-  setPreviewImage: () => {},
 });
 
 export const useOnboardingContext = () => useContext(OnboardingContext);
 
 export const OnboardingProvider = ({ children }: PropsWithChildren) => {
   const [onboardingInfo, setOnboardingInfo] = useState<OnboardingInfo>(initialOnboardingInfo);
-  const [previewImage, setPreviewImage] = useState<string>('');
 
   const updateOnboardingInfo = (newInfo: Partial<OnboardingInfo>) => {
     setOnboardingInfo((prev) => ({ ...prev, ...newInfo }));
@@ -38,18 +33,12 @@ export const OnboardingProvider = ({ children }: PropsWithChildren) => {
     console.log('전체 값 확인:', onboardingInfo);
   }, [onboardingInfo]);
 
-  useEffect(() => {
-    console.log(' 컨텍스트 내 previewImage 확인', previewImage);
-  }, [previewImage]);
-
   const OnboardingInfoContextValue = useMemo(
     () => ({
       onboardingInfo,
       updateOnboardingInfo,
-      previewImage,
-      setPreviewImage,
     }),
-    [onboardingInfo, previewImage],
+    [onboardingInfo],
   );
 
   return (
