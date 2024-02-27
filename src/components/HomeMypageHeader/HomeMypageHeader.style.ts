8488e57e675d4c4762a5765a4b5effbf7838bd7c
Wang HoEun <hoeunwang@gmail.com>
2024-02-27 19:48:43 +0900

    style: mypage header 요소들 수정

--- src/components/HomeMypageHeader/HomeMypageHeader.style.ts
+++ src/components/HomeMypageHeader/HomeMypageHeader.style.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const HomeMypageHeaderWrapper = styled.div`
   ${({ theme: { mixin } }) => mixin.flexBox({ justify: 'flex-end', align: 'center' })};
   position: fixed;
-  width: 100%;
+  width: 37.5rem;
   height: 5.6rem;
   padding: 0 1.8rem;
   z-index: 999;
@@ -12,7 +12,8 @@ export const HomeMypageHeaderWrapper = styled.div`
 
 export const UserProfileImg = styled.img`
   ${({ theme: { mixin } }) => mixin.flexBox({})};
-  width: 3rem;
-  height: 3rem;
+  width: 2.4rem;
+  height: 2.4rem;
   border-radius: 9rem;
+  margin-left: 0.8rem;
 `;
