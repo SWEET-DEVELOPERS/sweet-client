4c90c166aa68ee189cd88589c32dba98e512ab06
Wang HoEun <hoeunwang@gmail.com>
2024-02-27 20:59:15 +0900

    refactor: 아래아이콘 스크롤에 따라서 애니메이션 적용

--- src/pages/Start/StartAnimation/DownIconAnimation/StartDownIconAnimation.style.ts
+++ src/pages/Start/StartAnimation/DownIconAnimation/StartDownIconAnimation.style.ts
@@ -1,13 +1,28 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import { IcDown } from '../../../../assets/svg';
 
 export const DownIconWrapper = styled.div`
-  display: flex;
+  ${({ theme: { mixin } }) => mixin.flexCenter({})};
+  position: relative;
 `;
 
-export const DownIcon = styled(IcDown)`
+const frameInAnimationUp = keyframes`
+  0% {
+    opacity: 1;
+    transform: translateY(0%);
+  }
+
+  100%{
+    opacity: 0;
+    transform: translateY(-100%);
+  }
+`;
+
+export const DownIcon = styled(IcDown)<{ $isVisible: boolean }>`
   position: absolute;
   top: 4rem;
   width: 4rem;
   color: ${({ theme: { colors } }) => colors.G_07};
+  opacity: ${({ $isVisible }) => ($isVisible ? 1 : 0)}; /* 스크롤에 따라 투명도 조절 */
+  animation: ${({ $isVisible }) => ($isVisible ? 'none' : frameInAnimationUp)} 2s forwards; /* isVisible에 따라 애니메이션 적용 */
 `;
