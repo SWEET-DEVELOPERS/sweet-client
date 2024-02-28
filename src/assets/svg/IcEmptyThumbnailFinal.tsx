d400ff4bf2a7a55d901b4b7e8f7a6c92fa1c45f2
장정안 <wjddks96@naver.com>
2024-02-29 00:50:26 +0900

    chore: stroke 포함된 empty thumbnail svg 파일 추가

---
+++ src/assets/svg/IcEmptyThumbnailFinal.tsx
@@ -0,0 +1,41 @@
+import type { SVGProps } from 'react';
+const SvgIcEmptyThumbnailFinal = (props: SVGProps<SVGSVGElement>) => (
+  <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 242 242' {...props}>
+    <g filter='url(#ic_empty_thumbnail_final_svg__a)'>
+      <rect width={240} height={240} x={1} y={1} fill='#F8F8F8' rx={12} />
+      <rect width={241} height={241} x={0.5} y={0.5} stroke='#EBE9EA' rx={12.5} />
+    </g>
+    <path
+      fill='#CAC6C8'
+      d='M103.026 135.059a1.501 1.501 0 0 0 2.122 2.121zm10.191-8.07 1.061-1.061a1.5 1.5 0 0 0-2.121 0zm4.566 4.565-1.061 1.061a1.5 1.5 0 0 0 2.121 0zm10.271-10.271 1.061-1.061a1.5 1.5 0 0 0-2.121 0zm8.07 10.191a1.501 1.501 0 0 0 2.121-2.122zm-30.976 5.706 9.13-9.13-2.121-2.122-9.131 9.131zm7.009-9.13 4.565 4.565 2.121-2.121-4.565-4.566zm6.686 4.565 10.272-10.272-2.121-2.121-10.272 10.272zm8.151-10.272 9.13 9.131 2.121-2.122-9.13-9.13zm-18.342-17.821h22.826v-3h-22.826zm28.174 5.348v22.826h3V109.87zm-5.348 28.173h-22.826v3h22.826zm-28.174-5.347V109.87h-3v22.826zm5.348 5.347a5.35 5.35 0 0 1-5.348-5.347h-3a8.35 8.35 0 0 0 8.348 8.347zm28.174-5.347a5.347 5.347 0 0 1-5.348 5.347v3a8.347 8.347 0 0 0 8.348-8.347zm-5.348-28.174a5.35 5.35 0 0 1 5.348 5.348h3a8.347 8.347 0 0 0-8.348-8.348zm-22.826-3a8.35 8.35 0 0 0-8.348 8.348h3a5.35 5.35 0 0 1 5.348-5.348zM114 113.293a1.923 1.923 0 0 1-1.924 1.924v3a4.923 4.923 0 0 0 4.924-4.924zm-1.924 1.924a1.924 1.924 0 0 1-1.924-1.924h-3a4.924 4.924 0 0 0 4.924 4.924zm-1.924-1.924c0-1.062.862-1.923 1.924-1.923v-3a4.924 4.924 0 0 0-4.924 4.923zm1.924-1.923c1.063 0 1.924.861 1.924 1.923h3a4.923 4.923 0 0 0-4.924-4.923z'
+    />
+    <circle cx={138.652} cy={103.348} r={9.13} fill='#CAC6C8' />
+    <rect width={1.826} height={9.13} x={137.739} y={98.783} fill='#fff' rx={0.913} />
+    <rect
+      width={1.826}
+      height={9.13}
+      x={134.087}
+      y={104.261}
+      fill='#fff'
+      rx={0.913}
+      transform='rotate(-90 134.087 104.261)'
+    />
+    <defs>
+      <filter
+        id='ic_empty_thumbnail_final_svg__a'
+        width={282}
+        height={282}
+        x={-20}
+        y={-20}
+        colorInterpolationFilters='sRGB'
+        filterUnits='userSpaceOnUse'
+      >
+        <feFlood floodOpacity={0} result='BackgroundImageFix' />
+        <feGaussianBlur in='BackgroundImageFix' stdDeviation={10} />
+        <feComposite in2='SourceAlpha' operator='in' result='effect1_backgroundBlur_3247_15133' />
+        <feBlend in='SourceGraphic' in2='effect1_backgroundBlur_3247_15133' result='shape' />
+      </filter>
+    </defs>
+  </svg>
+);
+export default SvgIcEmptyThumbnailFinal;
