02a3f61d291795fdc3ac2c836e82e712f31590e9
장정안 <wjddks96@naver.com>
2024-02-28 23:33:10 +0900

    feat: 시간 선택 중 input 색상 P-06으로 변경 &  바깥 클릭 시 or 시간 선택 시 원래대로

--- src/components/OnBoardingSteps/Step04/TimePicker.tsx
+++ src/components/OnBoardingSteps/Step04/TimePicker.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { IcUnselectedClock } from '../../../assets/svg';
 import * as S from './Step04.style';
 
@@ -12,7 +12,25 @@ const TimePicker = ({ onSelect }: TimePickerProps) => {
 
   const clockRef = useRef<HTMLInputElement>(null);
 
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent | TouchEvent) => {
+      if (clockRef.current && !clockRef.current.contains(event.target as Node)) {
+        setIsPickerOpen(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    document.addEventListener('touchstart', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+      document.removeEventListener('touchstart', handleClickOutside);
+    };
+  }, []);
+
   const handleIconClick = () => {
+    setIsPickerOpen(!isPickerOpen);
+    console.log('isPickerOpen', isPickerOpen);
     clockRef.current?.showPicker();
   };
 
@@ -23,33 +41,34 @@ const TimePicker = ({ onSelect }: TimePickerProps) => {
 
     /**@todo 사용하는 곳 없는 것을 방지하기 위한 콘솔 */
     console.log(selectedTime);
-    console.log(isPickerOpen);
   };
 
   return (
     <>
-      <S.TextField onClick={handleIconClick}>
-        <S.Input
-          placeholder='시작 시간을 선택해주세요'
-          value={selectedTime}
-          onChange={(e) => setSelectedTime(e.target.value)}
-        />
-      </S.TextField>
-      <S.IconField>
-        <input
-          ref={clockRef}
-          type='time'
-          id='clock'
-          onChange={(e) => handleTimeSelect(e.target.value)}
-          style={{ display: 'flex', opacity: 0 }}
-        />
-        <label htmlFor='clock'>
-          <IcUnselectedClock
-            style={{ width: '2.4rem', height: '2.4rem' }}
-            onClick={handleIconClick}
+      <S.Container $hasContent={isPickerOpen}>
+        <S.TextField onClick={handleIconClick}>
+          <S.Input
+            placeholder='시작 시간을 선택해주세요'
+            value={selectedTime}
+            onChange={(e) => setSelectedTime(e.target.value)}
           />
-        </label>
-      </S.IconField>
+        </S.TextField>
+        <S.IconField>
+          <input
+            ref={clockRef}
+            type='time'
+            id='clock'
+            onChange={(e) => handleTimeSelect(e.target.value)}
+            style={{ display: 'flex', opacity: 0 }}
+          />
+          <label htmlFor='clock'>
+            <IcUnselectedClock
+              style={{ width: '2.4rem', height: '2.4rem' }}
+              onClick={handleIconClick}
+            />
+          </label>
+        </S.IconField>
+      </S.Container>
     </>
   );
 };
