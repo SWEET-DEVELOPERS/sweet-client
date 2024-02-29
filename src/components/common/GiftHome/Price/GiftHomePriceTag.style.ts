fdb2a3c3b768dbab67926a77f4e4c72f56c4b10d
urjimyu <92876819+urjimyu@users.noreply.github.com>
2024-02-29 12:31:22 +0900

    fix: 스타일 수정

--- src/components/common/GiftHome/Price/GiftHomePriceTag.style.ts
+++ src/components/common/GiftHome/Price/GiftHomePriceTag.style.ts
@@ -2,7 +2,9 @@ import styled from 'styled-components';
 
 export const Price = styled.p<{ $fonts: string; $colors: string; $gap: number }>`
   display: flex;
-  column-gap: ${({ $gap }) => $gap};
-  ${({ theme, $fonts }) => `${theme}.fonts.${$fonts}`};
-  color: ${({ theme, $colors }) => `${theme}.colors.${$colors}`};
+  column-gap: ${({ $gap }) => $gap}px; /* Ensure unit for column-gap */
+  ${({ theme, $fonts, $colors }) => `
+    ${theme.fonts[$fonts]};
+    color: ${theme.colors[$colors]};
+ `}
 `;
