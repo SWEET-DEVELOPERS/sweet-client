b40efc1e3b65554974a58c52a3a2057efe71fafb
urjimyu <92876819+urjimyu@users.noreply.github.com>
2024-02-29 21:03:35 +0900

    style: 햄버거 바 제거

--- src/components/common/GiftDetail/GiftDetailHeader.tsx
+++ src/components/common/GiftDetail/GiftDetailHeader.tsx
@@ -1,11 +1,9 @@
 import { useNavigate } from 'react-router-dom';
-import { IcLeft, IcMenu } from '../../../assets/svg';
 import * as S from './GiftDetailHeader.style';
 
 interface GiftDetailHeaderProps {
   title: string;
   roomId: string;
-  // targetDate: string;
 }
 
 const GiftDetailHeader = ({ title, roomId }: GiftDetailHeaderProps) => {
@@ -13,17 +11,12 @@ const GiftDetailHeader = ({ title, roomId }: GiftDetailHeaderProps) => {
 
   const handleClickBtn = () => {
     navigate(`/gift-home/${roomId}`);
-    // navigate(-1);
   };
 
   return (
     <S.GiftDetailHeaderWrapper>
-      <IcLeft
-        style={{ width: '3.6rem', height: '3.6rem', cursor: 'pointer' }}
-        onClick={handleClickBtn}
-      />
+      <S.IcLeftSvg onClick={handleClickBtn} />
       {title}
-      <IcMenu style={{ width: '2.8rem', height: '2.8rem', cursor: 'pointer' }} />
     </S.GiftDetailHeaderWrapper>
   );
 };
