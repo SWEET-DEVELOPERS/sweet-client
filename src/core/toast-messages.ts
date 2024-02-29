792615bfbe3533648b89cd37c817c9489fd32145
장정안 <wjddks96@naver.com>
2024-02-29 20:16:37 +0900

    feat: 토스트 메세지 추가

--- src/core/toast-messages.ts
+++ src/core/toast-messages.ts
@@ -8,4 +8,5 @@ export const MESSAGE = Object.freeze({
   HEIGHT_SMALL: `이미지 세로길이가 너무 작습니다. ${IMAGE_HEIGHT.MIN}px 이상 이미지를 선택해주세요.`,
   HEIGHT_BIG: `이미지 세로길이가 너무 큽니다. ${IMAGE_HEIGHT.MAX}px 이하 이미지를 선택해주세요. `,
   UNSELECT_DATE: '날짜를 먼저 선택해주세요.',
+  INVALID_INVITATION_CODE: '유효하지 않은 초대코드입니다.',
 });
