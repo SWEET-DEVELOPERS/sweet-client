821ce98bbe0c4ebb0e3cc251d04a9d033379e8a3
urjimyu <92876819+urjimyu@users.noreply.github.com>
2024-02-29 20:43:04 +0900

    style: gap 통일 반영

--- src/components/common/GiftHome/Price/GiftHomePriceTag.style.ts
+++ src/components/common/GiftHome/Price/GiftHomePriceTag.style.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Price = styled.p<{ $fonts: string; $colors: string; $gap: number }>`
   display: flex;
-  column-gap: ${({ $gap }) => $gap}px; /* Ensure unit for column-gap */
+  column-gap: ${({ $gap }) => $gap}rem;
   ${({ theme, $fonts, $colors }) => `
     ${theme.fonts[$fonts]};
     color: ${theme.colors[$colors]};
