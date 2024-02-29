1bab066f736f7835e1cc1c45e2cc26e2666f0363
urjimyu <92876819+urjimyu@users.noreply.github.com>
2024-02-29 21:16:38 +0900

    style: 스타일 수정

--- src/pages/GiftHome/GiftHomeMyGifts/GiftHomeMyGifts.style.ts
+++ src/pages/GiftHome/GiftHomeMyGifts/GiftHomeMyGifts.style.ts
@@ -2,12 +2,12 @@ import styled from 'styled-components';
 
 export const GiftHomeMyGiftsWrapper = styled.article`
   width: 100%;
+
   margin-bottom: 1.8rem;
 
   padding: 0.6rem 0.5rem 0 2rem;
 `;
 
 export const GiftsWrapper = styled.div`
-  height: 7rem;
   gap: 1.7rem;
 `;
