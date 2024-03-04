52a5eaca1d78efa391e43ac48c40ec2c809c7959
Wang HoEun <hoeunwang@gmail.com>
2024-03-04 14:00:07 +0900

    feat: 토너먼트 애니메이션 직접 구현 위해 폴더 및 파일 생성

---
+++ src/pages/Start/StartAnimation/TournamentAnimation/StartTournamentAnimation.tsx
@@ -0,0 +1,11 @@
+import * as S from './StartTournamentAnimation.style';
+
+const StartTournamentAnimation = () => {
+  return (
+    <S.TournamentWrapper>
+      <p>토너먼트 애니메이션</p>
+    </S.TournamentWrapper>
+  );
+};
+
+export default StartTournamentAnimation;
