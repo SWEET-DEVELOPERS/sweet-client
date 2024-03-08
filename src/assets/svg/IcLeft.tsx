692a491f259a4d49b8a07fe44157c62bb815719c
urjimyu <92876819+urjimyu@users.noreply.github.com>
2024-03-08 09:59:04 +0900

    style: 뒤로가기 아이콘 업데이트

--- src/assets/svg/IcLeft.tsx
+++ src/assets/svg/IcLeft.tsx
@@ -1,12 +1,12 @@
 import type { SVGProps } from 'react';
 const SvgIcLeft = (props: SVGProps<SVGSVGElement>) => (
-  <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' {...props}>
+  <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 36 36' {...props}>
     <path
-      stroke='#000'
+      stroke='#222'
       strokeLinecap='round'
       strokeLinejoin='round'
       strokeWidth={1.5}
-      d='m14 17-5-5 5-5'
+      d='M21 25.5 13.5 18l7.5-7.5'
     />
   </svg>
 );
