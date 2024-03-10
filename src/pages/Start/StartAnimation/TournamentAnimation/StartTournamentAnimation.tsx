41e2ca04de268aacdc5e4cad0eeaf8b4e94d6306
Wang HoEun <hoeunwang@gmail.com>
2024-03-10 15:58:30 +0900

    feat: touranment 애니메이션 직접 구현 후 적용 X view에 안 보이는 이슈

--- src/pages/Start/StartAnimation/TournamentAnimation/StartTournamentAnimation.tsx
+++ src/pages/Start/StartAnimation/TournamentAnimation/StartTournamentAnimation.tsx
@@ -1,9 +1,21 @@
+import { Tournament01, Tournament02, Tournament03, Tournament04 } from '../../../../assets/svg';
 import * as S from './StartTournamentAnimation.style';
 
 const StartTournamentAnimation = () => {
   return (
     <S.TournamentWrapper>
-      <p>토너먼트 애니메이션</p>
+      <S.AnimatedTournament $animationDelay='0s' $style='0rem'>
+        <Tournament04 />
+      </S.AnimatedTournament>
+      <S.AnimatedTournament $animationDelay='0.25s' $style='20rem'>
+        <Tournament03 />
+      </S.AnimatedTournament>
+      <S.AnimatedTournament $animationDelay='0.5s' $style='40rem'>
+        <Tournament02 />
+      </S.AnimatedTournament>
+      <S.AnimatedTournament $animationDelay='0.75s' $style='60rem'>
+        <Tournament01 />
+      </S.AnimatedTournament>
     </S.TournamentWrapper>
   );
 };
