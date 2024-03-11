059e66f2cb1208bdb093eb04e4e879ba32774b7a
장정안 <wjddks96@naver.com>
2024-03-11 20:20:44 +0900

    feat: resizedFile 함수 분리 및 확장자 강제 WEBP로 변경

--- src/hooks/common/usePreviewImage.tsx
+++ src/hooks/common/usePreviewImage.tsx
@@ -18,37 +18,43 @@ const usePreviewImage = () => {
       });
     };
 
+    const resizedFile = (file: File, previewImage: string) => {
+      new Promise((resolve) => {
+        Resizer.imageFileResizer(
+          file,
+          384,
+          384,
+          'WEBP',
+          100,
+          0,
+          (uri) => {
+            updatePreviewImageInfo({
+              isImageUploaded: true,
+              imageName: (uri as File).name,
+              file: uri as File,
+              previewImage: previewImage,
+            });
+            resolve(uri);
+            console.log('됐다?');
+          },
+          'file',
+        );
+      });
+    };
+
     if (files && files.length > 0) {
       const selectedFiles = files as FileList;
-      const imageName = files[0].name.trim();
-
-      Resizer.imageFileResizer(
-        selectedFiles[0],
-        300, //리사이즈할 가로 크기
-        300, //리사이즈할 세로 크기
-        'WebP', //리사이즈된 이미지 형식포맷
-        100, //이미지 품질
-        0, //회전 각도
-        (resizedFile) => {
-          const previewImageUrl = URL.createObjectURL(selectedFiles[0]);
-          console.log('typeof resizedFile', typeof resizedFile);
-          updatePreviewImageInfo({
-            isImageUploaded: true,
-            file: resizedFile,
-            previewImage: previewImageUrl,
-            imageName: finalImageName,
-          });
-        },
-        'file', //리사이즈 된 이미지 반환 형식
-      );
+      /**@see 추후 유니크한 이미지 네임 필요할 수 있으니 일단 주석처리 */
+      // const imageName = files[0].name.trim();
 
       /**@todo 파싱 유틸 함수 공용으로 따로 작성 */
-      const uploadTime = new Date().toISOString();
+      // const uploadTime = new Date().toISOString();
 
-      const uniqueName = `${uploadTime}${imageName}`;
-      const finalImageName = uniqueName
-        .replace(/\//g, '') // 폴더링 방지를 위해 '/' 제거
-        .replace(/\s/g, ''); // 공백 제거
+      // const uniqueName = `${uploadTime}${imageName}`;
+      // const finalImageName = uniqueName
+      //   .replace(/\//g, '') // 폴더링 방지를 위해 '/' 제거
+      //   .replace(/\s/g, ''); // 공백 제거
+      resizedFile(selectedFiles[0], URL.createObjectURL(selectedFiles[0]));
 
       const img = new Image();
       img.onload = function () {
@@ -60,17 +66,6 @@ const usePreviewImage = () => {
           toast(MESSAGE.HEIGHT_BIG);
           uploadFalse();
         }
-        // else {
-        //   //  이미지 너비가 허용된 범위 내에 있을 때
-        //   updatePreviewImageInfo({
-        //     isImageUploaded: true,
-        //     file: selectedFiles[0],
-        //     previewImage: URL.createObjectURL(selectedFiles[0]),
-        //     imageName: finalImageName,
-        //   });
-        //   console.log('imageName', imageName);
-        //   console.log('finalImageName', finalImageName);
-        // }
       };
       img.src = URL.createObjectURL(selectedFiles[0]);
     }
