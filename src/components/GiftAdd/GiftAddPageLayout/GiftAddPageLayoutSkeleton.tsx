a9cbb63ae3f6db3ffda7dcc06b525ad8cc094cd9
urjimyu <92876819+urjimyu@users.noreply.github.com>
2024-03-11 23:29:01 +0900

    refactor: 스타일 명시

--- src/components/GiftAdd/GiftAddPageLayout/GiftAddPageLayoutSkeleton.tsx
+++ src/components/GiftAdd/GiftAddPageLayout/GiftAddPageLayoutSkeleton.tsx
@@ -4,9 +4,38 @@ import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
 const GiftsItemWrapper = () => (
   <>
     <S.GiftsItemWrapper>
-      <Skeleton wrapper={S.GiftsItemImage} containerClassName='flex: 1' />
-      <Skeleton wrapper={S.GiftsItemTitle} containerClassName='flex: 1' />
-      <Skeleton wrapper={S.GiftsItemTitle2} containerClassName='flex: 1' />
+      <Skeleton
+        count={1}
+        style={{
+          width: '15.8rem',
+          height: '15.8rem',
+          borderRadius: '0.4rem',
+          backgroundColor: '#EBE9EA',
+        }}
+        containerClassName='flex: 1'
+      />
+      <Skeleton
+        count={1}
+        style={{
+          width: '14.6rem',
+          height: '2rem',
+          marginTop: '1.4rem',
+          marginBottom: '0.5rem',
+          borderRadius: '0.4rem',
+          backgroundColor: '#EBE9EA',
+        }}
+        containerClassName='flex: 1'
+      />
+      <Skeleton
+        count={1}
+        style={{
+          width: '9.4rem',
+          height: '2rem',
+          borderRadius: '0.4rem',
+          backgroundColor: '#EBE9EA',
+        }}
+        containerClassName='flex: 1'
+      />
     </S.GiftsItemWrapper>
   </>
 );
@@ -14,12 +43,21 @@ const GiftsItemWrapper = () => (
 const GiftAddPageLayoutSkeleton = () => {
   console.log('SKELETON');
   return (
-    <SkeletonTheme>
+    <SkeletonTheme highlightColor='#f5f5f5' duration={1}>
       <S.GiftAddPageWrapper>
         <S.GiftAddPageSkeletonHeader>
           <S.HeaderText>내가 등록한 선물</S.HeaderText>
         </S.GiftAddPageSkeletonHeader>
-        <Skeleton wrapper={S.MiniTimerWrapper} containerClassName='flex: 1' />
+        <Skeleton
+          count={1}
+          style={{
+            width: '10.3rem',
+            height: '2.7rem',
+            borderRadius: '1.4rem',
+            backgroundColor: '#F8F8F8',
+          }}
+          containerClassName='flex: 1'
+        />
         <S.AddButtonsWrapper>
           <GiftsItemWrapper />
           <GiftsItemWrapper />
