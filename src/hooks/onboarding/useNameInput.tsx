ee8dcc30dc65dca1e7161ebf2013bd69fd7e1501
장정안 <wjddks96@naver.com>
2024-03-12 01:30:54 +0900

    feat: 일반 특수문자 & 아이폰 특수문자 입력 안되게 유효성 체크

--- src/hooks/onboarding/useNameInput.tsx
+++ src/hooks/onboarding/useNameInput.tsx
@@ -1,15 +1,34 @@
+import { toast } from 'react-toastify';
 import { useOnboardingContext } from '../../context/Onboarding/OnboardingContext';
 
 const useNameInput = () => {
   const { onboardingInfo, updateOnboardingInfo } = useOnboardingContext();
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const inputValue = e.target.value;
-    const unicodeChars = [...inputValue].filter((char) => /[\ud800-\udfff]/.test(char)).length;
+    let inputValue = e.target.value;
 
-    inputValue.length + unicodeChars <= 10
-      ? updateOnboardingInfo({ gifteeName: inputValue })
-      : e.preventDefault();
+    const hasSpecialCharacter = /[~`!@#$%\^&*+=\-\[\]\\';,/{}|\\":<>\?]/.test(inputValue);
+    const hasIphoneSpecialCharacter = /[\ud800-\udfff]/.test(inputValue);
+
+    if (hasSpecialCharacter || hasIphoneSpecialCharacter) {
+      toast.info('특수문자는 입력하실 수 없습니다.', {
+        position: 'top-right',
+        autoClose: 2000,
+        hideProgressBar: true,
+        closeOnClick: false,
+        pauseOnHover: false,
+        draggable: false,
+        progress: undefined,
+        theme: 'colored',
+        icon: false,
+      });
+
+      inputValue = inputValue.replace(/[~`!@#$%\^&*+=\-\[\]\\';,/{}|\\":<>\?]/g, ''); // 일반 특수문자 제거
+      inputValue = inputValue.replace(/[\ud800-\udfff]/g, ''); // 아이폰 특수문자 제거
+      updateOnboardingInfo({ gifteeName: inputValue });
+    } else {
+      updateOnboardingInfo({ gifteeName: inputValue });
+    }
   };
 
   const handleBtnClick = () => {
