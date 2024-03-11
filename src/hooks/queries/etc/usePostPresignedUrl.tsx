71f213eb93d63e288c0ccbc352f12233c7c890c5
장정안 <wjddks96@naver.com>
2024-03-11 20:55:06 +0900

    chore: 필요없는 주석 및 코드 제거

--- src/hooks/queries/etc/usePostPresignedUrl.tsx
+++ src/hooks/queries/etc/usePostPresignedUrl.tsx
@@ -3,14 +3,8 @@ import { post } from '../../../apis/client';
 import { useMutation } from '@tanstack/react-query';
 import { useState } from 'react';
 
-// interface PostPresignedUrlArgs {
-//   // url: string;
-//   filename: string;
-// }
-
 const usePostPresignedUrl = () => {
   const [presignedUrl, setPresignedUrl] = useState<string>(''); // 상태 추가
-
   const postPresignedUrl = async (filename: string) => {
     try {
       const queryString = filename ? `presigned-url?fileName=${filename}` : '';
@@ -32,8 +26,6 @@ const usePostPresignedUrl = () => {
   const mutation = useMutation({
     mutationFn: postPresignedUrl,
     onSuccess: (data) => {
-      // console.log('usePostPresignedUrl onSuccess', data);
-      // console.log('data.presignedUrl', data.presignedUrl);
       setPresignedUrl(data.presignedUrl);
     },
   });
