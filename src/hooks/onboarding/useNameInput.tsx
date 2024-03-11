85909c921621f23aa0d68e6f7106eb2624e56302
장정안 <wjddks96@naver.com>
2024-03-12 01:41:31 +0900

    feat: 아이폰 이모티콘도 입력안되게 유효성 체크 추가

--- src/hooks/onboarding/useNameInput.tsx
+++ src/hooks/onboarding/useNameInput.tsx
@@ -9,8 +9,12 @@ const useNameInput = () => {
 
     const hasSpecialCharacter = /[~`!@#$%\^&*+=\-\[\]\\';,/{}|\\":<>\?]/.test(inputValue);
     const hasIphoneSpecialCharacter = /[\ud800-\udfff]/.test(inputValue);
+    const hasEmoji =
+      /[\u{1F300}-\u{1F6FF}\u{1F900}-\u{1F9FF}\u{2600}-\u{26FF}\u{2700}-\u{27BF}]/u.test(
+        inputValue,
+      );
 
-    if (hasSpecialCharacter || hasIphoneSpecialCharacter) {
+    if (hasSpecialCharacter || hasIphoneSpecialCharacter || hasEmoji) {
       toast.info('특수문자는 입력하실 수 없습니다.', {
         position: 'top-right',
         autoClose: 2000,
@@ -25,6 +29,10 @@ const useNameInput = () => {
 
       inputValue = inputValue.replace(/[~`!@#$%\^&*+=\-\[\]\\';,/{}|\\":<>\?]/g, ''); // 일반 특수문자 제거
       inputValue = inputValue.replace(/[\ud800-\udfff]/g, ''); // 아이폰 특수문자 제거
+      inputValue = inputValue.replace(
+        /[\u{1F300}-\u{1F6FF}\u{1F900}-\u{1F9FF}\u{2600}-\u{26FF}\u{2700}-\u{27BF}]/gu,
+        '',
+      ); //아이폰 이모티콘 제거
       updateOnboardingInfo({ gifteeName: inputValue });
     } else {
       updateOnboardingInfo({ gifteeName: inputValue });
