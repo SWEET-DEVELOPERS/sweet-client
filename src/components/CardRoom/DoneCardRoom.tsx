0eac3c8bee5e0da88f190d8bf7ac37b87654dd20
Wang HoEun <hoeunwang@gmail.com>
2024-03-12 18:07:27 +0900

    feat: suspense로 skeleton 라이브러리 적용

--- src/components/CardRoom/DoneCardRoom.tsx
+++ src/components/CardRoom/DoneCardRoom.tsx
@@ -1,6 +1,8 @@
 import * as S from './CardRoom.style';
 import { IcUser } from '../../assets/svg';
 import Type3Tag from '../IcTag/Type3/Type3';
+import Skeleton from 'react-loading-skeleton';
+import { Suspense } from 'react';
 
 interface DoneCardRoomType {
   user: string;
@@ -13,25 +15,27 @@ const DoneCardRoom = ({ user, srcImage, userCount, onClick }: DoneCardRoomType) 
   const multiline = user.length > 3 || (/[a-zA-Z]/.test(user) && user.length > 5);
   return (
     <S.CardRoomWrapper onClick={onClick}>
-      <div>
-        <S.RoomImgWrapper src={srcImage} />
-        <S.Text>
-          {multiline ? (
-            <>
-              <S.User>{user}님</S.User>을 위한 선물방
-            </>
-          ) : (
-            <S.User>{user}님을 위한 선물방</S.User>
-          )}
-        </S.Text>
-        <S.CountUser>
-          <IcUser style={{ width: '1.6rem', height: '1.6rem', color: '#ACA7A9' }} />
-          {userCount}
-        </S.CountUser>
-      </div>
-      <S.TagWrapper>
-        <Type3Tag tag='토너먼트 완료' />
-      </S.TagWrapper>
+      <Suspense fallback={<Skeleton />}>
+        <div>
+          <S.RoomImgWrapper src={srcImage} />
+          <S.Text>
+            {multiline ? (
+              <>
+                <S.User>{user}님</S.User>을 위한 선물방
+              </>
+            ) : (
+              <S.User>{user}님을 위한 선물방</S.User>
+            )}
+          </S.Text>
+          <S.CountUser>
+            <IcUser style={{ width: '1.6rem', height: '1.6rem', color: '#ACA7A9' }} />
+            {userCount}
+          </S.CountUser>
+        </div>
+        <S.TagWrapper>
+          <Type3Tag tag='토너먼트 완료' />
+        </S.TagWrapper>
+      </Suspense>
     </S.CardRoomWrapper>
   );
 };
--- src/components/CardRoom/EditCardRoom.tsx
+++ src/components/CardRoom/EditCardRoom.tsx
@@ -4,6 +4,8 @@ import Type2Tag from '../IcTag/Type2/Type2';
 import Type1Tag from '../IcTag/Type1/Type1';
 import { useNavigate } from 'react-router';
 import DateCheck from '../DateCheck/DateCheck';
+import Skeleton from 'react-loading-skeleton';
+import { Suspense } from 'react';
 
 interface EditCardRoomType {
   user: string;
@@ -24,39 +26,41 @@ const EditCardRoom = ({ user, srcImage, userCount, roomId, date, onClick }: Edit
   const multiline = user.length > 3 || (/[a-zA-Z]/.test(user) && user.length > 5);
   return (
     <S.WholeWrapper>
-      <S.CardRoomWrapper onClick={onClick}>
-        <div>
-          <S.RoomImgWrapper src={srcImage} />
-          <S.Text>
-            {multiline ? (
-              <>
-                <S.User>{user}님</S.User>을 위한 선물방
-              </>
-            ) : (
-              <S.User>{user}님을 위한 선물방</S.User>
-            )}
-          </S.Text>
-          <S.CountUser>
-            <IcUser
-              style={{
-                width: '1.6rem',
-                height: '1.6rem',
-                color: '#ACA7A9',
-                position: 'relative',
-                top: '-0.1rem',
-              }}
-            />
-            {userCount}
-          </S.CountUser>
-        </div>
-        <S.TagWrapper>
-          <Type1Tag tag='개설자' />
-          {!isFuture ? <Type2Tag tag='토너먼트 진행 중' /> : <Type2Tag tag='선물 등록 중' />}
-        </S.TagWrapper>
-      </S.CardRoomWrapper>
-      <S.SettingWrapper onClick={handleSettingIcon}>
-        <IcSettings style={{ width: '2.4rem', height: '2.4rem', color: 'white' }} />
-      </S.SettingWrapper>
+      <Suspense fallback={<Skeleton />}>
+        <S.CardRoomWrapper onClick={onClick}>
+          <div>
+            <S.RoomImgWrapper src={srcImage} />
+            <S.Text>
+              {multiline ? (
+                <>
+                  <S.User>{user}님</S.User>을 위한 선물방
+                </>
+              ) : (
+                <S.User>{user}님을 위한 선물방</S.User>
+              )}
+            </S.Text>
+            <S.CountUser>
+              <IcUser
+                style={{
+                  width: '1.6rem',
+                  height: '1.6rem',
+                  color: '#ACA7A9',
+                  position: 'relative',
+                  top: '-0.1rem',
+                }}
+              />
+              {userCount}
+            </S.CountUser>
+          </div>
+          <S.TagWrapper>
+            <Type1Tag tag='개설자' />
+            {!isFuture ? <Type2Tag tag='토너먼트 진행 중' /> : <Type2Tag tag='선물 등록 중' />}
+          </S.TagWrapper>
+        </S.CardRoomWrapper>
+        <S.SettingWrapper onClick={handleSettingIcon}>
+          <IcSettings style={{ width: '2.4rem', height: '2.4rem', color: 'white' }} />
+        </S.SettingWrapper>
+      </Suspense>
     </S.WholeWrapper>
   );
 };
