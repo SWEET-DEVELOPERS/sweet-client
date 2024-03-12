33734068e85fb3d4adbac9f4e60ef7a2f0ec750b
Wang HoEun <hoeunwang@gmail.com>
2024-03-12 18:09:32 +0900

    fix: suspense 개별적으로 작성한 코드 삭제

--- src/components/CardRoom/ProgressCardRoom.tsx
+++ src/components/CardRoom/ProgressCardRoom.tsx
@@ -2,8 +2,6 @@ import * as S from './CardRoom.style';
 import { IcUser } from '../../assets/svg';
 import Type2Tag from '../IcTag/Type2/Type2';
 import DateCheck from '../DateCheck/DateCheck';
-import Skeleton from 'react-loading-skeleton';
-import { Suspense } from 'react';
 
 interface ProgressCardRoomType {
   user: string;
@@ -20,35 +18,33 @@ const ProgressCardRoom = ({ user, srcImage, userCount, date, onClick }: Progress
 
   return (
     <S.CardRoomWrapper onClick={onClick}>
-      <Suspense fallback={<Skeleton />}>
-        <div>
-          <S.RoomImgWrapper src={srcImage} />
-          <S.Text>
-            {multiline ? (
-              <>
-                <S.User>{user}님</S.User>을 위한 선물방
-              </>
-            ) : (
-              <S.User>{user}님을 위한 선물방</S.User>
-            )}
-          </S.Text>
-          <S.CountUser>
-            <IcUser
-              style={{
-                width: '1.6rem',
-                height: '1.6rem',
-                color: '#ACA7A9',
-                position: 'relative',
-                top: '0.2rem',
-              }}
-            />
-            {userCount}
-          </S.CountUser>
-        </div>
-        <S.TagWrapper>
-          {!isFuture ? <Type2Tag tag='토너먼트 진행 중' /> : <Type2Tag tag='선물 등록 중' />}
-        </S.TagWrapper>
-      </Suspense>
+      <div>
+        <S.RoomImgWrapper src={srcImage} />
+        <S.Text>
+          {multiline ? (
+            <>
+              <S.User>{user}님</S.User>을 위한 선물방
+            </>
+          ) : (
+            <S.User>{user}님을 위한 선물방</S.User>
+          )}
+        </S.Text>
+        <S.CountUser>
+          <IcUser
+            style={{
+              width: '1.6rem',
+              height: '1.6rem',
+              color: '#ACA7A9',
+              position: 'relative',
+              top: '0.2rem',
+            }}
+          />
+          {userCount}
+        </S.CountUser>
+      </div>
+      <S.TagWrapper>
+        {!isFuture ? <Type2Tag tag='토너먼트 진행 중' /> : <Type2Tag tag='선물 등록 중' />}
+      </S.TagWrapper>
     </S.CardRoomWrapper>
   );
 };
