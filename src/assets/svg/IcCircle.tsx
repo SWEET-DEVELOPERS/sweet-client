886a87e044d869c5e0ba9ca44216c24d0bb4f78f
장정안 <wjddks96@naver.com>
2024-03-13 18:59:31 +0900

    style: 라디오 버튼 svg 파일 위치 값 수치 조정

--- src/assets/svg/IcCircle.tsx
+++ src/assets/svg/IcCircle.tsx
@@ -1,6 +1,6 @@
 import type { SVGProps } from 'react';
 const SvgIcCircle = (props: SVGProps<SVGSVGElement>) => (
-  <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' {...props}>
+  <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 25 25' {...props}>
     <rect width={23} height={23} x={0.5} y={0.5} stroke='#EBE9EA' rx={11.5} />
   </svg>
 );
