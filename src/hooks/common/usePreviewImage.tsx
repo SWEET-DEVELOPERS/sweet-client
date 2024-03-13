58b6d4483e01d99cec2281d88e9f4847b8f7e018
장정안 <wjddks96@naver.com>
2024-03-13 22:21:57 +0900

    refactor: 함수 분리 & 고용량 이미지 이슈 예외처리
    이미지 업로드가 되는 것을 진행해놓은 상태에서
    다시 고용량 이미지를 추가하면 이미지 업로드가 되는 현상이 있어
    이를 수정하였습니다.

--- src/hooks/common/usePreviewImage.tsx
+++ src/hooks/common/usePreviewImage.tsx
@@ -6,68 +6,80 @@ import Resizer from 'react-image-file-resizer';
 const usePreviewImage = () => {
   const { previewImageInfo, updatePreviewImageInfo } = usePreviewImageContext();
 
+  const uploadFalse = () => {
+    updatePreviewImageInfo({
+      isImageUploaded: false,
+      file: null,
+      previewImage: null,
+      imageName: '',
+    });
+  };
+
+  const resizedFile = (file: File, previewImage: string) => {
+    new Promise((resolve) => {
+      Resizer.imageFileResizer(
+        file,
+        480,
+        480,
+        'WEBP',
+        75,
+        0,
+        (uri) => {
+          updatePreviewImageInfo({
+            isImageUploaded: true,
+            imageName: (uri as File).name,
+            file: uri as File,
+            previewImage: previewImage,
+          });
+          resolve(uri);
+          console.log('됐다?');
+        },
+        'file',
+      );
+    });
+  };
+
+  const failUploadImageToast = (selectedFile: Blob | MediaSource) => {
+    const img = new Image();
+
+    img.onload = function () {
+      if (img.height <= IMAGE_HEIGHT.MIN) {
+        //  이미지 너비가  어느 수준  이하일 때 업로드 x
+        toast(MESSAGE.HEIGHT_SMALL);
+        uploadFalse();
+      } else if (img.height > IMAGE_HEIGHT.MAX) {
+        toast(MESSAGE.HEIGHT_BIG);
+        uploadFalse();
+      }
+    };
+
+    img.src = URL.createObjectURL(selectedFile);
+  };
+
   const handleImageUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { files } = event.target;
 
-    const uploadFalse = () => {
-      updatePreviewImageInfo({
-        isImageUploaded: false,
-        file: null,
-        previewImage: null,
-        imageName: '',
-      });
-    };
+    try {
+      if (files && files.length > 0) {
+        const selectedFiles = files as FileList;
+        console.log('typeof selectedFiles', typeof selectedFiles);
+        /**@see 추후 유니크한 이미지 네임 필요할 수 있으니 일단 주석처리 */
+        // const imageName = files[0].name.trim();
 
-    const resizedFile = (file: File, previewImage: string) => {
-      new Promise((resolve) => {
-        Resizer.imageFileResizer(
-          file,
-          480,
-          480,
-          'WEBP',
-          75,
-          0,
-          (uri) => {
-            updatePreviewImageInfo({
-              isImageUploaded: true,
-              imageName: (uri as File).name,
-              file: uri as File,
-              previewImage: previewImage,
-            });
-            resolve(uri);
-            console.log('됐다?');
-          },
-          'file',
-        );
-      });
-    };
+        /**@todo 파싱 유틸 함수 공용으로 따로 작성 */
+        // const uploadTime = new Date().toISOString();
 
-    if (files && files.length > 0) {
-      const selectedFiles = files as FileList;
-      /**@see 추후 유니크한 이미지 네임 필요할 수 있으니 일단 주석처리 */
-      // const imageName = files[0].name.trim();
+        // const uniqueName = `${uploadTime}${imageName}`;
+        // const finalImageName = uniqueName
+        //   .replace(/\//g, '') // 폴더링 방지를 위해 '/' 제거
+        //   .replace(/\s/g, ''); // 공백 제거
 
-      /**@todo 파싱 유틸 함수 공용으로 따로 작성 */
-      // const uploadTime = new Date().toISOString();
+        failUploadImageToast(selectedFiles[0]);
 
-      // const uniqueName = `${uploadTime}${imageName}`;
-      // const finalImageName = uniqueName
-      //   .replace(/\//g, '') // 폴더링 방지를 위해 '/' 제거
-      //   .replace(/\s/g, ''); // 공백 제거
-      resizedFile(selectedFiles[0], URL.createObjectURL(selectedFiles[0]));
-
-      const img = new Image();
-      img.onload = function () {
-        if (img.height <= IMAGE_HEIGHT.MIN) {
-          //  이미지 너비가  어느 수준  이하일 때 업로드 x
-          toast(MESSAGE.HEIGHT_SMALL);
-          uploadFalse();
-        } else if (img.height > IMAGE_HEIGHT.MAX) {
-          toast(MESSAGE.HEIGHT_BIG);
-          uploadFalse();
-        }
-      };
-      img.src = URL.createObjectURL(selectedFiles[0]);
+        resizedFile(selectedFiles[0], URL.createObjectURL(selectedFiles[0]));
+      }
+    } catch (err) {
+      console.log('error:', err);
     }
   };
 
