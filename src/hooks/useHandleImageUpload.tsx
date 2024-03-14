8f8476476770ef299c61e4a730411eee5d6557bd
Jimin Yu <92876819+urjimyu@users.noreply.github.com>
2024-03-14 11:21:20 +0900

    refactor: heic 변환 처리 추가 (#492)

--- src/hooks/useHandleImageUpload.tsx
+++ src/hooks/useHandleImageUpload.tsx
@@ -1,6 +1,7 @@
 import { OpenGraphResponseType } from '../types/etc';
 import useParseFileName from './useParseFileName';
 import Resizer from 'react-image-file-resizer';
+import heic2any from 'heic2any';
 
 interface HandleImageUploadProps {
   openGraph: OpenGraphResponseType | null;
@@ -21,48 +22,52 @@ const useHandleImageUpload = ({
 }: HandleImageUploadProps) => {
   const handleImageUpload = async (event: React.ChangeEvent<HTMLInputElement>) => {
     const { files } = event.target;
+    if (files && files.length > 0) {
+      const selectedFile = files[0];
 
-    const resizeFile = (file: File) =>
-      new Promise((resolve) => {
+      setPreviewImage(URL.createObjectURL(selectedFile));
+
+      try {
+        let convertedFile = selectedFile;
+
+        if (selectedFile.type === 'image/heic' || selectedFile.type === 'image/HEIC') {
+          let blob = selectedFile;
+          const resultBlob = await heic2any({ blob, toType: 'image/webp' });
+          convertedFile = new File(
+            [resultBlob as Blob],
+            selectedFile.name.split('.')[0] + '.webp',
+            { type: 'image/webp', lastModified: new Date().getTime() },
+          );
+        }
+
         Resizer.imageFileResizer(
-          file,
+          convertedFile,
           384,
           384,
           'WEBP',
           75,
           0,
           (uri) => {
-            setFileName((uri as File).name);
-            setFile(uri as File);
-            console.log('FILE', uri);
-            resolve(uri);
+            if (uri instanceof File) {
+              setFileName((uri as File).name);
+              setFile(uri as File);
+              console.log('FILE', uri);
+              setImageUrl(URL.createObjectURL(uri as File));
+              setIsImageUploaded(true);
+
+              if (openGraph) {
+                openGraph.image = '';
+              }
+
+              const imageName = uri.name.trim();
+              useParseFileName({ setFileName, imageString: imageName });
+            }
           },
           'file',
         );
-      });
-
-    if (files && files.length > 0) {
-      const selectedFiles = files as FileList;
-
-      if (files) {
-        try {
-          resizeFile(selectedFiles[0]);
-        } catch (err) {
-          console.log('이미지 용량 압축 중 에러 발생', err);
-        }
+      } catch (err) {
+        console.log('Error processing image:', err);
       }
-
-      setPreviewImage(URL.createObjectURL(selectedFiles[0]));
-
-      setImageUrl(URL.createObjectURL(selectedFiles[0]));
-      setIsImageUploaded(!!selectedFiles?.[0]);
-
-      if (openGraph) {
-        openGraph.image = '';
-      }
-
-      const imageName = selectedFiles[0].name.trim();
-      useParseFileName({ setFileName, imageString: imageName });
     }
   };
   return { handleImageUpload };
